a2836ab30a2d1e2d7d662116ad578746a1dbdeb8
Gunnar Morling <gunnar.morling@googlemail.com>
2022-05-11 15:03:42 +0200

    WIP

--- src/main/java/io/strimzi/kproxy/KafkaProxy.java
+++ src/main/java/io/strimzi/kproxy/KafkaProxy.java
@@ -16,13 +16,19 @@
  */
 package io.strimzi.kproxy;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.ServerChannel;
 import io.netty.channel.epoll.Epoll;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
@@ -38,6 +44,7 @@ import io.strimzi.kproxy.internal.InterceptorProviderFactory;
 import io.strimzi.kproxy.internal.KafkaProxyInitializer;
 import io.strimzi.kproxy.internal.interceptor.AdvertisedListenersInterceptor;
 import io.strimzi.kproxy.internal.interceptor.ApiVersionsInterceptor;
+import io.strimzi.kproxy.internal.interceptor.ProduceRecordTransformationInterceptor;
 
 public final class KafkaProxy {
 
@@ -72,9 +79,13 @@ public final class KafkaProxy {
                             public int port(String host, int port) {
                                 return port + 100;
                             }
-                        })))
-                                .startup()
-                                .block();
+                        }),
+                        new ProduceRecordTransformationInterceptor(
+                                buffer -> ByteBuffer.wrap(new String(StandardCharsets.UTF_8.decode(buffer).array()).toLowerCase().getBytes(StandardCharsets.UTF_8))
+                                )
+                        ))
+        .startup()
+        .block();
     }
 
     public KafkaProxy(
