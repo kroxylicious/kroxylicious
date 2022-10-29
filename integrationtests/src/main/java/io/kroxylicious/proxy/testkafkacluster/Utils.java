a486a4050c7fed99f84c2e3797f9be912ad0e032
kwall <kwall@apache.org>
2022-10-29 16:31:15 +0100

    add javadoc to preAllocateListeningPorts

--- integrationtests/src/main/java/io/kroxylicious/proxy/testkafkacluster/Utils.java
+++ integrationtests/src/main/java/io/kroxylicious/proxy/testkafkacluster/Utils.java
@@ -9,18 +9,24 @@ package io.kroxylicious.proxy.testkafkacluster;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Utils {
+    /**
+     * Pre-allocate 1 or more ephemeral ports which are available for use.
+     *
+     * @param num number of ports to pre-allocate
+     * @return list of ephemeral ports
+     */
     public static Stream<Integer> preAllocateListeningPorts(int num) {
+        // Uses recursive algorithm to avoid the risk of returning a duplicate ephemeral port.
         if (num < 1) {
             return Stream.of();
         }
         try (var serverSocket = new ServerSocket(0)) {
             serverSocket.setReuseAddress(true);
             int localPort = serverSocket.getLocalPort();
-            return Stream.concat(List.of(localPort).stream(), preAllocateListeningPorts(num - 1));
+            return Stream.concat(Stream.of(localPort), preAllocateListeningPorts(num - 1));
         }
         catch (IOException e) {
             throw new UncheckedIOException(e);
