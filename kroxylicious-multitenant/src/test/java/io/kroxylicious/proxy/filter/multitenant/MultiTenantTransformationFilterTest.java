6be9c491ceb3b850721e15149c37f0b39523f843
kwall <kwall@apache.org>
2023-03-20 22:38:52 +0000

    Generate code for the JSON/Kafka request/response converters

--- kroxylicious-multitenant/src/test/java/io/kroxylicious/proxy/filter/multitenant/MultiTenantTransformationFilterTest.java
+++ kroxylicious-multitenant/src/test/java/io/kroxylicious/proxy/filter/multitenant/MultiTenantTransformationFilterTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
@@ -18,42 +17,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import org.apache.kafka.common.message.ApiMessageType;
-import org.apache.kafka.common.message.CreateTopicsRequestData;
-import org.apache.kafka.common.message.CreateTopicsRequestDataJsonConverter;
-import org.apache.kafka.common.message.CreateTopicsResponseData;
-import org.apache.kafka.common.message.CreateTopicsResponseDataJsonConverter;
-import org.apache.kafka.common.message.DeleteTopicsRequestData;
-import org.apache.kafka.common.message.DeleteTopicsRequestDataJsonConverter;
-import org.apache.kafka.common.message.DeleteTopicsResponseData;
-import org.apache.kafka.common.message.DeleteTopicsResponseDataJsonConverter;
-import org.apache.kafka.common.message.FetchRequestData;
-import org.apache.kafka.common.message.FetchRequestDataJsonConverter;
-import org.apache.kafka.common.message.FetchResponseData;
-import org.apache.kafka.common.message.FetchResponseDataJsonConverter;
-import org.apache.kafka.common.message.ListOffsetsRequestData;
-import org.apache.kafka.common.message.ListOffsetsRequestDataJsonConverter;
-import org.apache.kafka.common.message.ListOffsetsResponseData;
-import org.apache.kafka.common.message.ListOffsetsResponseDataJsonConverter;
-import org.apache.kafka.common.message.MetadataRequestData;
-import org.apache.kafka.common.message.MetadataRequestDataJsonConverter;
-import org.apache.kafka.common.message.MetadataResponseData;
-import org.apache.kafka.common.message.MetadataResponseDataJsonConverter;
-import org.apache.kafka.common.message.OffsetCommitRequestData;
-import org.apache.kafka.common.message.OffsetCommitRequestDataJsonConverter;
-import org.apache.kafka.common.message.OffsetCommitResponseData;
-import org.apache.kafka.common.message.OffsetCommitResponseDataJsonConverter;
-import org.apache.kafka.common.message.OffsetFetchRequestData;
-import org.apache.kafka.common.message.OffsetFetchRequestDataJsonConverter;
-import org.apache.kafka.common.message.OffsetFetchResponseData;
-import org.apache.kafka.common.message.OffsetFetchResponseDataJsonConverter;
-import org.apache.kafka.common.message.OffsetForLeaderEpochRequestData;
-import org.apache.kafka.common.message.OffsetForLeaderEpochRequestDataJsonConverter;
-import org.apache.kafka.common.message.OffsetForLeaderEpochResponseData;
-import org.apache.kafka.common.message.OffsetForLeaderEpochResponseDataJsonConverter;
-import org.apache.kafka.common.message.ProduceRequestData;
-import org.apache.kafka.common.message.ProduceRequestDataJsonConverter;
-import org.apache.kafka.common.message.ProduceResponseData;
-import org.apache.kafka.common.message.ProduceResponseDataJsonConverter;
 import org.apache.kafka.common.message.RequestHeaderData;
 import org.apache.kafka.common.message.ResponseHeaderData;
 import org.apache.kafka.common.protocol.ApiKeys;
@@ -76,6 +39,8 @@ import io.kroxylicious.proxy.filter.KrpcFilterContext;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
+import static io.kroxylicious.proxy.filter.multitenant.KafkaApiMessageConverter.requestConverterFor;
+import static io.kroxylicious.proxy.filter.multitenant.KafkaApiMessageConverter.responseConverterFor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -91,59 +56,6 @@ class MultiTenantTransformationFilterTest {
 
     private record RequestResponseTestDef(String testName, ApiMessageType apiKey, RequestHeaderData header, ApiMessageTestDef request, ApiMessageTestDef response) {
     }
-
-    private record Converters(BiFunction<JsonNode, Short, ApiMessage> requestReader,
-                              BiFunction<JsonNode, Short, ApiMessage> responseReader,
-                              BiFunction<ApiMessage, Short, JsonNode> requestWriter,
-                              BiFunction<ApiMessage, Short, JsonNode> responseWriters) {
-    }
-
-    private static final Map<ApiMessageType, Converters> converters = Map.of(
-            ApiMessageType.CREATE_TOPICS, new Converters(
-                    CreateTopicsRequestDataJsonConverter::read,
-                    CreateTopicsResponseDataJsonConverter::read,
-                    (o, ver) -> CreateTopicsRequestDataJsonConverter.write(((CreateTopicsRequestData) o), ver),
-                    (o, ver) -> CreateTopicsResponseDataJsonConverter.write(((CreateTopicsResponseData) o), ver)),
-            ApiMessageType.DELETE_TOPICS, new Converters(
-                    DeleteTopicsRequestDataJsonConverter::read,
-                    DeleteTopicsResponseDataJsonConverter::read,
-                    (o, ver) -> DeleteTopicsRequestDataJsonConverter.write(((DeleteTopicsRequestData) o), ver),
-                    (o, ver) -> DeleteTopicsResponseDataJsonConverter.write(((DeleteTopicsResponseData) o), ver)),
-            ApiMessageType.METADATA, new Converters(
-                    MetadataRequestDataJsonConverter::read,
-                    MetadataResponseDataJsonConverter::read,
-                    (o, ver) -> MetadataRequestDataJsonConverter.write(((MetadataRequestData) o), ver),
-                    (o, ver) -> MetadataResponseDataJsonConverter.write(((MetadataResponseData) o), ver)),
-            ApiMessageType.PRODUCE, new Converters(
-                    ProduceRequestDataJsonConverter::read,
-                    ProduceResponseDataJsonConverter::read,
-                    (o, ver) -> ProduceRequestDataJsonConverter.write(((ProduceRequestData) o), ver),
-                    (o, ver) -> ProduceResponseDataJsonConverter.write(((ProduceResponseData) o), ver)),
-            ApiMessageType.LIST_OFFSETS, new Converters(
-                    ListOffsetsRequestDataJsonConverter::read,
-                    ListOffsetsResponseDataJsonConverter::read,
-                    (o, ver) -> ListOffsetsRequestDataJsonConverter.write(((ListOffsetsRequestData) o), ver),
-                    (o, ver) -> ListOffsetsResponseDataJsonConverter.write(((ListOffsetsResponseData) o), ver)),
-            ApiMessageType.OFFSET_FETCH, new Converters(
-                    OffsetFetchRequestDataJsonConverter::read,
-                    OffsetFetchResponseDataJsonConverter::read,
-                    (o, ver) -> OffsetFetchRequestDataJsonConverter.write(((OffsetFetchRequestData) o), ver),
-                    (o, ver) -> OffsetFetchResponseDataJsonConverter.write(((OffsetFetchResponseData) o), ver)),
-            ApiMessageType.OFFSET_FOR_LEADER_EPOCH, new Converters(
-                    OffsetForLeaderEpochRequestDataJsonConverter::read,
-                    OffsetForLeaderEpochResponseDataJsonConverter::read,
-                    (o, ver) -> OffsetForLeaderEpochRequestDataJsonConverter.write(((OffsetForLeaderEpochRequestData) o), ver),
-                    (o, ver) -> OffsetForLeaderEpochResponseDataJsonConverter.write(((OffsetForLeaderEpochResponseData) o), ver)),
-            ApiMessageType.OFFSET_COMMIT, new Converters(
-                    OffsetCommitRequestDataJsonConverter::read,
-                    OffsetCommitResponseDataJsonConverter::read,
-                    (o, ver) -> OffsetCommitRequestDataJsonConverter.write(((OffsetCommitRequestData) o), ver),
-                    (o, ver) -> OffsetCommitResponseDataJsonConverter.write(((OffsetCommitResponseData) o), ver)),
-            ApiMessageType.FETCH, new Converters(
-                    FetchRequestDataJsonConverter::read,
-                    FetchResponseDataJsonConverter::read,
-                    (o, ver) -> FetchRequestDataJsonConverter.write(((FetchRequestData) o), ver),
-                    (o, ver) -> FetchResponseDataJsonConverter.write(((FetchResponseData) o), ver)));
 
     private static Stream<RequestResponseTestDef> requestResponseTestDefinitions() throws Exception {
         var resourceInfoStream = ClassPath.from(MultiTenantTransformationFilterTest.class.getClassLoader()).getResources().stream()
@@ -191,9 +103,10 @@ class MultiTenantTransformationFilterTest {
 
         var testName = String.format("%s,%s,v%d[%s]", source, messageType, versionNode.intValue(),
                 Optional.ofNullable(descriptionNode).map(JsonNode::asText).orElse("-"));
-        var conv = converters.get(messageType);
-        var request = buildApiMessageTestDef(header.requestApiVersion(), jsonNode.get("request"), conv.requestReader());
-        var response = buildApiMessageTestDef(header.requestApiVersion(), jsonNode.get("response"), conv.responseReader());
+        var requestConverter = requestConverterFor(messageType);
+        var responseConverter = responseConverterFor(messageType);
+        var request = buildApiMessageTestDef(header.requestApiVersion(), jsonNode.get("request"), requestConverter.reader());
+        var response = buildApiMessageTestDef(header.requestApiVersion(), jsonNode.get("response"), responseConverter.reader());
         return new RequestResponseTestDef(testName, messageType, header, request, response);
     }
 
@@ -226,7 +139,7 @@ class MultiTenantTransformationFilterTest {
     void requestsTransformed(@SuppressWarnings("unused") String testName, ApiMessageType apiMessageType, RequestHeaderData header, ApiMessageTestDef requestTestDef) {
         var request = requestTestDef.message();
         // marshalled the request object back to json, this is used for the comparison later.
-        var requestWriter = converters.get(apiMessageType).requestWriter();
+        var requestWriter = requestConverterFor(apiMessageType).writer();
         var marshalled = requestWriter.apply(request, header.requestApiVersion());
 
         filter.onRequest(ApiKeys.forId(apiMessageType.apiKey()), header, request, context);
@@ -245,7 +158,7 @@ class MultiTenantTransformationFilterTest {
     void responseTransformed(@SuppressWarnings("unused") String testName, ApiMessageType apiMessageType, RequestHeaderData header, ApiMessageTestDef responseTestDef) {
         var response = responseTestDef.message();
         // marshalled the response object back to json, this is used for comparison later.
-        var responseWriter = converters.get(apiMessageType).responseWriters();
+        var responseWriter = responseConverterFor(apiMessageType).writer();
 
         var marshalled = responseWriter.apply(response, header.requestApiVersion());
 
