d1363e358cdcd33397d2a4293a681615c103d42b
Sam Barker <sbarker@redhat.com>
2023-03-24 11:54:37 +1300

    Make sure the API release actually hits all modules.

--- release-api.sh
+++ release-api.sh
@@ -24,7 +24,8 @@ echo "Validating the build is green"
 mvn clean verify || { echo 'maven build failed' ; exit 1; }
 
 echo "Setting API version to ${RELEASE_API_VERSION}"
-mvn versions:set -DnewVersion="${RELEASE_API_VERSION}" -DprocessAllModules=true -pl ${API_MODULES} -DgenerateBackupPoms=false || { echo 'failed to set the API version' ; exit 1; }
+mvn versions:set -DnewVersion="${RELEASE_API_VERSION}" -DprocessAllModules=true -pl ":kroxylicious-api" -DgenerateBackupPoms=false || { echo 'failed to set the API version' ; exit 1; }
+mvn versions:set -DnewVersion="${RELEASE_API_VERSION}" -DprocessAllModules=true -pl ":kroxylicious-filter-api" -DgenerateBackupPoms=false || { echo 'failed to set the API version' ; exit 1; }
 mvn clean install -Pquick -pl ${API_MODULES}  #quick sanity check to ensure the API modules still build
 mvn versions:set-property -Dproperty=kroxyliciousApi.version -DnewVersion="${RELEASE_API_VERSION}" -DgenerateBackupPoms=false || { echo "failed to depend on API version ${RELEASE_API_VERSION}" ; exit 1; }
 
@@ -35,9 +36,7 @@ echo "Committing release to git"
 git add '**/pom.xml' 'pom.xml'
 git commit --message "Release API version v${RELEASE_API_VERSION}" --signoff
 
-if [[ -n ${RELEASE_API_VERSION}  ]]; then
-  git tag "api-v${RELEASE_API_VERSION}"
-fi
+git tag -f "api-v${RELEASE_API_VERSION}"
 
 git push --tags
 
