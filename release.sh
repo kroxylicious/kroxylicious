8631ca5cfa7217771a09d8fb7137b80edc48176f
Sam Barker <sbarker@redhat.com>
2023-03-24 11:13:49 +1300

    DROP ME: debugging

--- release.sh
+++ release.sh
@@ -45,16 +45,18 @@ git stash --all
 echo "Creating release branch from ${BRANCH_FROM}"
 git fetch -q "${REPOSITORY}"
 release_date=$(date -u '+%Y-%m-%d')
-git checkout -b "prepare-release-${release_date}" #"${REPOSITORY}/${BRANCH_FROM}"
+git checkout -B "prepare-release-${release_date}" #"${REPOSITORY}/${BRANCH_FROM}"
 
 if [[ -n ${RELEASE_API_VERSION} ]]; then
   echo "Releasing Public APIs as ${RELEASE_API_VERSION}"
   ./release-api.sh "${RELEASE_API_VERSION}"
+  echo "Released API"
 fi
 
 if [[ -n ${RELEASE_VERSION} ]]; then
   echo "Releasing Kroxylicious as ${RELEASE_VERSION}"
   ./release-framework.sh "${RELEASE_VERSION}"
+  echo "Released Framework"
 fi
 
 if [[ -z ${RELEASE_API_VERSION} && -z ${RELEASE_VERSION} ]]; then
