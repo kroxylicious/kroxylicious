0aa229f44247e53a16202d5a002280faa04523c2
Robert Young <robeyoun@redhat.com>
2023-03-24 14:38:06 +1300

    Generate BodyDecoder to translate from Readable to ApiMessage
    Why:
    We should generate anything that is boilerplate switching on
    the ApiKey so everything works if a new message is added to kafka.
    
    Closes #219

--- kroxylicious/src/main/java/io/kroxylicious/proxy/internal/codec/KafkaRequestDecoder.java
+++ kroxylicious/src/main/java/io/kroxylicious/proxy/internal/codec/KafkaRequestDecoder.java
@@ -7,37 +7,7 @@ package io.kroxylicious.proxy.internal.codec;
 
 import java.util.List;
 
-import org.apache.kafka.common.message.AddOffsetsToTxnRequestData;
-import org.apache.kafka.common.message.AddPartitionsToTxnRequestData;
-import org.apache.kafka.common.message.ApiVersionsRequestData;
-import org.apache.kafka.common.message.ControlledShutdownRequestData;
-import org.apache.kafka.common.message.CreateTopicsRequestData;
-import org.apache.kafka.common.message.DeleteRecordsRequestData;
-import org.apache.kafka.common.message.DeleteTopicsRequestData;
-import org.apache.kafka.common.message.DescribeGroupsRequestData;
-import org.apache.kafka.common.message.EndTxnRequestData;
-import org.apache.kafka.common.message.FetchRequestData;
-import org.apache.kafka.common.message.FindCoordinatorRequestData;
-import org.apache.kafka.common.message.HeartbeatRequestData;
-import org.apache.kafka.common.message.InitProducerIdRequestData;
-import org.apache.kafka.common.message.JoinGroupRequestData;
-import org.apache.kafka.common.message.LeaderAndIsrRequestData;
-import org.apache.kafka.common.message.LeaveGroupRequestData;
-import org.apache.kafka.common.message.ListGroupsRequestData;
-import org.apache.kafka.common.message.ListOffsetsRequestData;
-import org.apache.kafka.common.message.MetadataRequestData;
-import org.apache.kafka.common.message.OffsetCommitRequestData;
-import org.apache.kafka.common.message.OffsetFetchRequestData;
-import org.apache.kafka.common.message.OffsetForLeaderEpochRequestData;
-import org.apache.kafka.common.message.ProduceRequestData;
 import org.apache.kafka.common.message.RequestHeaderData;
-import org.apache.kafka.common.message.SaslAuthenticateRequestData;
-import org.apache.kafka.common.message.SaslHandshakeRequestData;
-import org.apache.kafka.common.message.StopReplicaRequestData;
-import org.apache.kafka.common.message.SyncGroupRequestData;
-import org.apache.kafka.common.message.TxnOffsetCommitRequestData;
-import org.apache.kafka.common.message.UpdateMetadataRequestData;
-import org.apache.kafka.common.message.WriteTxnMarkersRequestData;
 import org.apache.kafka.common.protocol.ApiKeys;
 import org.apache.kafka.common.protocol.ApiMessage;
 import org.apache.kafka.common.protocol.Readable;
@@ -126,7 +96,7 @@ public class KafkaRequestDecoder extends KafkaMessageDecoder {
         }
         final RequestFrame frame;
         if (decodeRequest) {
-            ApiMessage body = readBody(apiId, apiVersion, accessor);
+            ApiMessage body = BodyDecoder.decodeRequest(apiKey, apiVersion, accessor);
             if (log().isTraceEnabled()) {
                 log().trace("{}: body {}", ctx, body);
             }
@@ -160,70 +130,4 @@ public class KafkaRequestDecoder extends KafkaMessageDecoder {
         return new RequestHeaderData(accessor, headerVersion);
     }
 
-    private ApiMessage readBody(short apiKey, short apiVersion, Readable accessor) {
-        switch (ApiKeys.forId(apiKey)) {
-            case PRODUCE:
-                return new ProduceRequestData(accessor, apiVersion);
-            case FETCH:
-                return new FetchRequestData(accessor, apiVersion);
-            case LIST_OFFSETS:
-                return new ListOffsetsRequestData(accessor, apiVersion);
-            case METADATA:
-                return new MetadataRequestData(accessor, apiVersion);
-            case LEADER_AND_ISR:
-                return new LeaderAndIsrRequestData(accessor, apiVersion);
-            case STOP_REPLICA:
-                return new StopReplicaRequestData(accessor, apiVersion);
-            case UPDATE_METADATA:
-                return new UpdateMetadataRequestData(accessor, apiVersion);
-            case CONTROLLED_SHUTDOWN:
-                return new ControlledShutdownRequestData(accessor, apiVersion);
-            case OFFSET_COMMIT:
-                return new OffsetCommitRequestData(accessor, apiVersion);
-            case OFFSET_FETCH:
-                return new OffsetFetchRequestData(accessor, apiVersion);
-            case FIND_COORDINATOR:
-                return new FindCoordinatorRequestData(accessor, apiVersion);
-            case JOIN_GROUP:
-                return new JoinGroupRequestData(accessor, apiVersion);
-            case HEARTBEAT:
-                return new HeartbeatRequestData(accessor, apiVersion);
-            case LEAVE_GROUP:
-                return new LeaveGroupRequestData(accessor, apiVersion);
-            case SYNC_GROUP:
-                return new SyncGroupRequestData(accessor, apiVersion);
-            case DESCRIBE_GROUPS:
-                return new DescribeGroupsRequestData(accessor, apiVersion);
-            case LIST_GROUPS:
-                return new ListGroupsRequestData(accessor, apiVersion);
-            case SASL_HANDSHAKE:
-                return new SaslHandshakeRequestData(accessor, apiVersion);
-            case API_VERSIONS:
-                return new ApiVersionsRequestData(accessor, apiVersion);
-            case CREATE_TOPICS:
-                return new CreateTopicsRequestData(accessor, apiVersion);
-            case DELETE_TOPICS:
-                return new DeleteTopicsRequestData(accessor, apiVersion);
-            case DELETE_RECORDS:
-                return new DeleteRecordsRequestData(accessor, apiVersion);
-            case INIT_PRODUCER_ID:
-                return new InitProducerIdRequestData(accessor, apiVersion);
-            case OFFSET_FOR_LEADER_EPOCH:
-                return new OffsetForLeaderEpochRequestData(accessor, apiVersion);
-            case ADD_PARTITIONS_TO_TXN:
-                return new AddPartitionsToTxnRequestData(accessor, apiVersion);
-            case ADD_OFFSETS_TO_TXN:
-                return new AddOffsetsToTxnRequestData(accessor, apiVersion);
-            case END_TXN:
-                return new EndTxnRequestData(accessor, apiVersion);
-            case WRITE_TXN_MARKERS:
-                return new WriteTxnMarkersRequestData(accessor, apiVersion);
-            case TXN_OFFSET_COMMIT:
-                return new TxnOffsetCommitRequestData(accessor, apiVersion);
-            case SASL_AUTHENTICATE:
-                return new SaslAuthenticateRequestData(accessor, apiVersion);
-            default:
-                throw new IllegalArgumentException("Unsupported API key " + apiKey);
-        }
-    }
 }
--- kroxylicious/src/main/java/io/kroxylicious/proxy/internal/codec/KafkaResponseDecoder.java
+++ kroxylicious/src/main/java/io/kroxylicious/proxy/internal/codec/KafkaResponseDecoder.java
@@ -7,37 +7,7 @@ package io.kroxylicious.proxy.internal.codec;
 
 import java.util.List;
 
-import org.apache.kafka.common.message.AddOffsetsToTxnResponseData;
-import org.apache.kafka.common.message.AddPartitionsToTxnResponseData;
-import org.apache.kafka.common.message.ApiVersionsResponseData;
-import org.apache.kafka.common.message.ControlledShutdownResponseData;
-import org.apache.kafka.common.message.CreateTopicsResponseData;
-import org.apache.kafka.common.message.DeleteRecordsResponseData;
-import org.apache.kafka.common.message.DeleteTopicsResponseData;
-import org.apache.kafka.common.message.DescribeGroupsResponseData;
-import org.apache.kafka.common.message.EndTxnResponseData;
-import org.apache.kafka.common.message.FetchResponseData;
-import org.apache.kafka.common.message.FindCoordinatorResponseData;
-import org.apache.kafka.common.message.HeartbeatResponseData;
-import org.apache.kafka.common.message.InitProducerIdResponseData;
-import org.apache.kafka.common.message.JoinGroupResponseData;
-import org.apache.kafka.common.message.LeaderAndIsrResponseData;
-import org.apache.kafka.common.message.LeaveGroupResponseData;
-import org.apache.kafka.common.message.ListGroupsResponseData;
-import org.apache.kafka.common.message.ListOffsetsResponseData;
-import org.apache.kafka.common.message.MetadataResponseData;
-import org.apache.kafka.common.message.OffsetCommitResponseData;
-import org.apache.kafka.common.message.OffsetFetchResponseData;
-import org.apache.kafka.common.message.OffsetForLeaderEpochResponseData;
-import org.apache.kafka.common.message.ProduceResponseData;
 import org.apache.kafka.common.message.ResponseHeaderData;
-import org.apache.kafka.common.message.SaslAuthenticateRequestData;
-import org.apache.kafka.common.message.SaslHandshakeResponseData;
-import org.apache.kafka.common.message.StopReplicaRequestData;
-import org.apache.kafka.common.message.SyncGroupResponseData;
-import org.apache.kafka.common.message.TxnOffsetCommitResponseData;
-import org.apache.kafka.common.message.UpdateMetadataResponseData;
-import org.apache.kafka.common.message.WriteTxnMarkersResponseData;
 import org.apache.kafka.common.protocol.ApiKeys;
 import org.apache.kafka.common.protocol.ApiMessage;
 import org.apache.kafka.common.protocol.Readable;
@@ -103,7 +73,7 @@ public class KafkaResponseDecoder extends KafkaMessageDecoder {
             log().trace("{}: Header version: {}", ctx, headerVersion);
             ResponseHeaderData header = readHeader(headerVersion, accessor);
             log().trace("{}: Header: {}", ctx, header);
-            ApiMessage body = readBody(apiKey, apiVersion, accessor);
+            ApiMessage body = BodyDecoder.decodeResponse(apiKey, apiVersion, accessor);
             log().trace("{}: Body: {}", ctx, body);
             KrpcFilter recipient = correlation.recipient();
             final List<Tag> LIST_OF_TAGS = List.of(
@@ -133,70 +103,4 @@ public class KafkaResponseDecoder extends KafkaMessageDecoder {
         return new ResponseHeaderData(accessor, headerVersion);
     }
 
-    private ApiMessage readBody(ApiKeys apiKey, short apiVersion, Readable accessor) {
-        switch (apiKey) {
-            case PRODUCE:
-                return new ProduceResponseData(accessor, apiVersion);
-            case FETCH:
-                return new FetchResponseData(accessor, apiVersion);
-            case LIST_OFFSETS:
-                return new ListOffsetsResponseData(accessor, apiVersion);
-            case METADATA:
-                return new MetadataResponseData(accessor, apiVersion);
-            case LEADER_AND_ISR:
-                return new LeaderAndIsrResponseData(accessor, apiVersion);
-            case STOP_REPLICA:
-                return new StopReplicaRequestData(accessor, apiVersion);
-            case UPDATE_METADATA:
-                return new UpdateMetadataResponseData(accessor, apiVersion);
-            case CONTROLLED_SHUTDOWN:
-                return new ControlledShutdownResponseData(accessor, apiVersion);
-            case OFFSET_COMMIT:
-                return new OffsetCommitResponseData(accessor, apiVersion);
-            case OFFSET_FETCH:
-                return new OffsetFetchResponseData(accessor, apiVersion);
-            case FIND_COORDINATOR:
-                return new FindCoordinatorResponseData(accessor, apiVersion);
-            case JOIN_GROUP:
-                return new JoinGroupResponseData(accessor, apiVersion);
-            case HEARTBEAT:
-                return new HeartbeatResponseData(accessor, apiVersion);
-            case LEAVE_GROUP:
-                return new LeaveGroupResponseData(accessor, apiVersion);
-            case SYNC_GROUP:
-                return new SyncGroupResponseData(accessor, apiVersion);
-            case DESCRIBE_GROUPS:
-                return new DescribeGroupsResponseData(accessor, apiVersion);
-            case LIST_GROUPS:
-                return new ListGroupsResponseData(accessor, apiVersion);
-            case SASL_HANDSHAKE:
-                return new SaslHandshakeResponseData(accessor, apiVersion);
-            case API_VERSIONS:
-                return new ApiVersionsResponseData(accessor, apiVersion);
-            case CREATE_TOPICS:
-                return new CreateTopicsResponseData(accessor, apiVersion);
-            case DELETE_TOPICS:
-                return new DeleteTopicsResponseData(accessor, apiVersion);
-            case DELETE_RECORDS:
-                return new DeleteRecordsResponseData(accessor, apiVersion);
-            case INIT_PRODUCER_ID:
-                return new InitProducerIdResponseData(accessor, apiVersion);
-            case OFFSET_FOR_LEADER_EPOCH:
-                return new OffsetForLeaderEpochResponseData(accessor, apiVersion);
-            case ADD_PARTITIONS_TO_TXN:
-                return new AddPartitionsToTxnResponseData(accessor, apiVersion);
-            case ADD_OFFSETS_TO_TXN:
-                return new AddOffsetsToTxnResponseData(accessor, apiVersion);
-            case END_TXN:
-                return new EndTxnResponseData(accessor, apiVersion);
-            case WRITE_TXN_MARKERS:
-                return new WriteTxnMarkersResponseData(accessor, apiVersion);
-            case TXN_OFFSET_COMMIT: // ???
-                return new TxnOffsetCommitResponseData(accessor, apiVersion);
-            case SASL_AUTHENTICATE:
-                return new SaslAuthenticateRequestData(accessor, apiVersion);
-            default:
-                throw new IllegalArgumentException("Unsupported API key " + apiKey);
-        }
-    }
 }
