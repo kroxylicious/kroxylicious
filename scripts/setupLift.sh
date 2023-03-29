389c02b8b000b8de41c95573f8d5b37200580c56
Sam Barker <sbarker@redhat.com>
2023-03-29 15:02:08 +1300

    Try and script it...

---
+++ scripts/setupLift.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#
+# Copyright Kroxylicious Authors.
+#
+# Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+#
+
+mvn install -DskipTests -pl :kroxylicious-krpc-plugin
