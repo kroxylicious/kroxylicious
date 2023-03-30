98b16114e8f1fde17d17066a88aa69bad74a7d08
Sam Barker <sbarker@redhat.com>
2023-03-31 10:04:11 +1300

    Explicitly configure maven.
    Co-authored-by: Francisco Vila <fvila@redhat.com>

--- scripts/setupLift.sh
+++ scripts/setupLift.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-
 #
 # Copyright Kroxylicious Authors.
 #
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
+set -e
+
+mkdir -p ~/.m2
+
+cat <<EOF > ~/.m2/settings.xml
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                      https://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <localRepository>/home/lift/.m2/repository</localRepository>
+  <interactiveMode>false</interactiveMode>
+  <offline>false</offline>
+</settings>
+EOF
 
 mvn install -DskipTests -pl :kroxylicious-krpc-plugin
