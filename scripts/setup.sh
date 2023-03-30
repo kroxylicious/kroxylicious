11859b22d37ff1bdcac55115e512a35c7f87d512
Francisco Vila <fvila@redhat.com>
2023-03-30 14:16:21 +0200

    test lift

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,19 @@
+#!/usr/bin/env bash
 #
 # Copyright Kroxylicious Authors.
 #
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
-
+mkdir ~/.m2
+cat <<EOF > ~/.m2/settings.xml
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                      https://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <localRepository>${user.home}/.m2/repository</localRepository>
+  <interactiveMode>true</interactiveMode>
+  <offline>false</offline>
+  ...
+</settings>
+EOF
 mvn install -DskipTests -pl :kroxylicious-krpc-plugin
-mvn install -DskipTests -pl :kroxylicious-filter-api
