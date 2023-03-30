0e65307c4a54d28934f06f1cfd376a2db0f08099
Sam Barker <sbarker@redhat.com>
2023-03-31 10:11:42 +1300

    Fix license headers

--- scripts/setupLift.sh
+++ scripts/setupLift.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+
 #
 # Copyright Kroxylicious Authors.
 #
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
+
 set -e
 
 mkdir -p ~/.m2
