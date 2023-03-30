ad01e448e419629b3d2a82f72ab0c0db70be63a1
Sam Barker <sbarker@redhat.com>
2023-03-31 11:09:04 +1300

    don't assume the path to user home.

--- scripts/setupLift.sh
+++ scripts/setupLift.sh
@@ -15,7 +15,7 @@ cat <<EOF > ~/.m2/settings.xml
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                       https://maven.apache.org/xsd/settings-1.0.0.xsd">
-  <localRepository>/home/lift/.m2/repository</localRepository>
+  <localRepository>${user.home}/.m2/repository</localRepository>
   <interactiveMode>false</interactiveMode>
   <offline>false</offline>
 </settings>
