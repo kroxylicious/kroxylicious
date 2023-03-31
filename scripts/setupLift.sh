16c68368262f89114cd49e2f218995f6c418c96e
Francisco Vila <fvila@redhat.com>
2023-03-31 11:28:39 +0200

    test lift

--- scripts/setupLift.sh
+++ scripts/setupLift.sh
@@ -5,15 +5,17 @@
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
 
-mkdir -p ~/.m2
-cat <<EOF > ~/.m2/settings.xml
+M2_HOME=~/.m2
+
+mkdir -p $M2_HOME
+cat <<EOF > $M2_HOME/settings.xml
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                       https://maven.apache.org/xsd/settings-1.0.0.xsd">
-  <localRepository>/home/lift/.m2/repository</localRepository>
-  <interactiveMode>true</interactiveMode>
+  <localRepository>$M2_HOME/repository</localRepository>
+  <interactiveMode>false</interactiveMode>
   <offline>false</offline>
 </settings>
 EOF
-mvn install -DskipTests -pl :kroxylicious-krpc-plugin
+mvn install -DskipTests -pl :kroxylicious-krpc-plugin -Dquick
