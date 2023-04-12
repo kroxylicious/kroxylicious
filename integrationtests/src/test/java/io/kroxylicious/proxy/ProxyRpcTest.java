4ad4a26aee85c70ddeaa3cf1b1135d60a905c27c
Robert Young <robeyoun@redhat.com>
2023-04-13 10:41:56 +1200

    Add integration test for simple proxy and manipulation of all RPCs
    Introduces a FixedClientIdFilter and TestFilterContributor to integrationtests
    so we have a filter available that decodes all requests/responses and modifies
    the header of all requests.
    
    Why:
    We want to validate that Kroxy in a very simple setup can decode all
    Kafka RPCS, at all supported apiVErsions, apply some simple manipulation,
    re-encode the request, proxy it to the physical broker and forward the
    response back to the client.
    
    These tests use a mock kafka broker with a configurable response and
    a test client to send a single RPC at a time to kroxy, and then verify
    the message received at the mock is what we expect and that the response
    recieved by the client is what we expect.

---
+++ integrationtests/src/test/java/io/kroxylicious/proxy/ProxyRpcTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.apache.kafka.common.message.ApiMessageType;
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.ApiMessage;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import io.kroxylicious.test.ApiMessageSampleGenerator;
+import io.kroxylicious.test.ApiMessageSampleGenerator.ApiAndVersion;
+import io.kroxylicious.test.Request;
+import io.kroxylicious.test.Response;
+import io.kroxylicious.test.client.KafkaClient;
+import io.kroxylicious.test.server.MockServer;
+
+import static io.kroxylicious.proxy.Utils.startProxy;
+import static org.apache.kafka.common.protocol.ApiKeys.API_VERSIONS;
+import static org.apache.kafka.common.protocol.ApiKeys.CONTROLLED_SHUTDOWN;
+import static org.apache.kafka.common.protocol.ApiKeys.SASL_AUTHENTICATE;
+import static org.apache.kafka.common.protocol.ApiKeys.SASL_HANDSHAKE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ProxyRpcTest {
+
+    public static final int PROXY_PORT = 9192;
+    public static final String PROXY_ADDRESS = "localhost:" + PROXY_PORT;
+    private static MockServer mockServer;
+    private static KafkaClient kafkaClient;
+    private static KafkaProxy kafkaProxy;
+
+    public record Scenario(String name, Response givenMockResponse, Request whenSendRequest, Request thenMockReceivesRequest, Response thenResponseReceived) {
+    }
+
+    /**
+     * API_VERSIONS is not proxied, kroxy can respond to this itself
+     */
+    private static final Set<ApiKeys> NOT_PROXIED = Set.of(API_VERSIONS);
+
+    @BeforeAll
+    public static void beforeAll() throws InterruptedException {
+        mockServer = MockServer.startOnRandomPort();
+        kafkaClient = new KafkaClient();
+        String config = configure(mockServer);
+        kafkaProxy = startProxy(config);
+    }
+
+    @MethodSource("scenarios")
+    @ParameterizedTest
+    public void testKroxyliciousCanDecodeManipulateAndProxyRPC(Scenario scenario) throws Exception {
+        mockServer.clear();
+        mockServer.setResponse(scenario.givenMockResponse());
+        CompletableFuture<Response> future = kafkaClient.get("localhost", PROXY_PORT, scenario.whenSendRequest());
+        Response response = future.get(10, TimeUnit.SECONDS);
+        assertEquals(scenario.thenMockReceivesRequest(), onlyRequest(mockServer), "unexpected request received at mock for scenario: " + scenario.name());
+        assertEquals(scenario.thenResponseReceived(), response, "unexpected response received from kroxylicious for scenario: " + scenario.name());
+    }
+
+    @AfterAll
+    public static void afterAll() throws Exception {
+        kafkaProxy.close();
+        mockServer.close();
+        kafkaClient.close();
+    }
+
+    @NotNull
+    private static Stream<Scenario> scenarios() {
+        Map<ApiAndVersion, ApiMessage> requestSamples = ApiMessageSampleGenerator.createRequestSamples();
+        Map<ApiAndVersion, ApiMessage> responseSamples = ApiMessageSampleGenerator.createResponseSamples();
+        return Arrays.stream(ApiKeys.values()).filter(apiKeys -> !NOT_PROXIED.contains(apiKeys)).flatMap(apiKeys -> toScenario(requestSamples, responseSamples, apiKeys));
+    }
+
+    private static final ApiAndVersion v0HeaderVersion = new ApiAndVersion(CONTROLLED_SHUTDOWN, (short) 0);
+
+    private static @NotNull Stream<Scenario> toScenario(Map<ApiAndVersion, ApiMessage> requestSamples, Map<ApiAndVersion, ApiMessage> responseSample, ApiKeys apiKeys) {
+        ApiMessageType messageType = apiKeys.messageType;
+        IntStream supported = IntStream.range(messageType.lowestSupportedVersion(), apiKeys.messageType.highestSupportedVersion() + 1);
+        return supported.mapToObj(version -> new ApiAndVersion(apiKeys, (short) version)).map(apiAndVersion -> {
+            ApiMessage request = requestSamples.get(apiAndVersion);
+            ApiMessage response = responseSample.get(apiAndVersion);
+            Request clientRequest = createRequestDefinition(apiAndVersion, "mockClientId", request);
+            String expected;
+            if (v0HeaderVersion.equals(apiAndVersion)) {
+                // controlled shutdown is the only usage of a version 0 header schema which doesn't have clientId
+                expected = "";
+            }
+            else if (apiAndVersion.keys() == SASL_AUTHENTICATE || apiAndVersion.keys() == SASL_HANDSHAKE) {
+                // sasl requests have special logic in kroxy, and they do not get Filtered
+                expected = "mockClientId";
+            }
+            else {
+                expected = "fixed";
+            }
+            Request expectedAtMock = createRequestDefinition(apiAndVersion, expected, request);
+            Response responseJson = createResponseDefinition(apiAndVersion, response);
+            return new Scenario(apiKeys.name, responseJson, clientRequest, expectedAtMock, responseJson);
+        });
+    }
+
+    @NotNull
+    private static Response createResponseDefinition(ApiAndVersion apiAndVersion, ApiMessage message) {
+        return new Response(apiAndVersion.keys(), apiAndVersion.apiVersion(), message);
+    }
+
+    @NotNull
+    private static Request createRequestDefinition(ApiAndVersion apiKeys, String clientId, ApiMessage requestBody) {
+        return new Request(apiKeys.keys(), apiKeys.apiVersion(), clientId, requestBody);
+    }
+
+    private static String configure(MockServer mockServer) {
+        return KroxyConfig.builder().withNewProxy().withAddress(ProxyRpcTest.PROXY_ADDRESS).endProxy()
+                .addToClusters("demo", new ClusterBuilder().withBootstrapServers("localhost:" + mockServer.port()).build())
+                .addNewFilter().withType("ApiVersions").endFilter()
+                .addNewFilter().withType("FixedClientId").withConfig(Map.of("clientId", "fixed")).endFilter()
+                .build().toYaml();
+    }
+
+    private static Request onlyRequest(MockServer mockServer) {
+        List<Request> requests = mockServer.getReceivedRequests();
+        if (requests.size() != 1) {
+            fail("mock server does not have exactly one request recorded");
+        }
+        return requests.get(0);
+    }
+
+}
---
+++ integrationtests/src/test/java/io/kroxylicious/proxy/filter/FixedClientIdFilter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.filter;
+
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.message.ResponseHeaderData;
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.ApiMessage;
+
+import io.kroxylicious.proxy.config.BaseConfig;
+
+public class FixedClientIdFilter implements KrpcFilter {
+
+    private final String clientId;
+
+    public static class FixedClientIdFilterConfig extends BaseConfig {
+
+        private final String clientId;
+
+        public FixedClientIdFilterConfig(String clientId) {
+            this.clientId = clientId;
+        }
+
+        public String getClientId() {
+            return clientId;
+        }
+    }
+
+    FixedClientIdFilter(FixedClientIdFilterConfig config) {
+        this.clientId = config.getClientId();
+    }
+
+    @Override
+    public boolean shouldDeserializeRequest(ApiKeys apiKey, short apiVersion) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldDeserializeResponse(ApiKeys apiKey, short apiVersion) {
+        return true;
+    }
+
+    @Override
+    public void onRequest(ApiKeys apiKey, RequestHeaderData header, ApiMessage body, KrpcFilterContext filterContext) {
+        header.setClientId(clientId);
+        filterContext.forwardRequest(body);
+    }
+
+    @Override
+    public void onResponse(ApiKeys apiKey, ResponseHeaderData header, ApiMessage body, KrpcFilterContext filterContext) {
+        filterContext.forwardResponse(body);
+    }
+}
