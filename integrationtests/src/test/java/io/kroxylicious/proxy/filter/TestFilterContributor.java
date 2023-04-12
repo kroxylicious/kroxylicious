548e4455b7012c851ba62bf91ebc6cb146e31617
Robert Young <robeyoun@redhat.com>
2023-04-13 10:41:56 +1200

    Add integration test for simple proxy and manipulation of all RPCs
    Introduces a FixedClientIdFilter and TestFilterContributor to integrationtests
    so we have a filter available that decodes all requests/responses and modifies
    the header of all requests.
    
    Why:
    We want to validate that Kroxy in a very simple setup can decode all
    Kafka RPCS, at all supported apiVErsions, apply some simple manipulation,
    re-encode the request, proxy it to the physical broker and forward the
    response back to the client.
    
    These tests use a mock kafka broker with a configurable response and
    a test client to send a single RPC at a time to kroxy, and then verify
    the message received at the mock is what we expect and that the response
    recieved by the client is what we expect.

---
+++ integrationtests/src/test/java/io/kroxylicious/proxy/filter/TestFilterContributor.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.kroxylicious.proxy.filter;
+
+import io.kroxylicious.proxy.service.BaseContributor;
+
+public class TestFilterContributor extends BaseContributor<KrpcFilter> implements FilterContributor {
+
+    public static final BaseContributorBuilder<KrpcFilter> FILTERS = BaseContributor.<KrpcFilter> builder()
+            .add("FixedClientId", FixedClientIdFilter.FixedClientIdFilterConfig.class, FixedClientIdFilter::new);
+
+    public TestFilterContributor() {
+        super(FILTERS);
+    }
+}
