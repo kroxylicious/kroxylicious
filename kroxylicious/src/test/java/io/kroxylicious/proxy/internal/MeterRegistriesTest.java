30c13fb50917b40f0c809830da8bec1169b3c45f
Robert Young <robeyoun@redhat.com>
2023-04-13 15:42:45 +1200

    Explode if we attempt to register meter with same name but different tags
    If we register meters with the same name and different tags with prometheus as
    the backing registry, currently it is effectively like the second registration
    is ignored and the data will be not emitted in the scraped data over HTTP.

---
+++ kroxylicious/src/test/java/io/kroxylicious/proxy/internal/MeterRegistriesTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.internal;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+
+class MeterRegistriesTest {
+
+    @Test
+    public void testPreventingRegistrationOfMetersWithSameNameButDifferentTags() {
+        CompositeMeterRegistry registry = new CompositeMeterRegistry();
+        MeterRegistries.preventDifferentTagNameRegistration(registry);
+        registry.counter("abc", List.of(Tag.of("a", "b")));
+        registry.counter("abc", List.of(Tag.of("a", "c")));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            registry.counter("abc", List.of(Tag.of("c", "d")));
+        });
+    }
+
+}
