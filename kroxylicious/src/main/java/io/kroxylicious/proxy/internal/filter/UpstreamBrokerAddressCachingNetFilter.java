de6f1fe19ac249eda6090c3b49ddd517febb8a5b
kwall <kwall@apache.org>
2023-04-17 13:46:39 +0100

    addressing review comments

--- kroxylicious/src/main/java/io/kroxylicious/proxy/internal/filter/UpstreamBrokerAddressCachingNetFilter.java
+++ kroxylicious/src/main/java/io/kroxylicious/proxy/internal/filter/UpstreamBrokerAddressCachingNetFilter.java
@@ -11,6 +11,9 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.kroxylicious.proxy.bootstrap.FilterChainFactory;
 import io.kroxylicious.proxy.filter.KrpcFilter;
 import io.kroxylicious.proxy.filter.MetadataResponseFilter;
@@ -24,6 +27,8 @@ import io.kroxylicious.proxy.service.HostPort;
  * the upstream broker address from a cache built from a previous connection's metadata response.
  */
 public class UpstreamBrokerAddressCachingNetFilter implements NetFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UpstreamBrokerAddressCachingNetFilter.class);
 
     private final HostPort targetClusterBootstrap;
     private final FilterChainFactory filterChainFactory;
@@ -44,19 +49,39 @@ public class UpstreamBrokerAddressCachingNetFilter implements NetFilter {
 
         // Add a filter to the *end of the chain* that gathers the true nodeId/upstream broker mapping.
         filters.add((MetadataResponseFilter) (header, response, filterContext) -> {
-            response.brokers().forEach(b -> upstreamBrokers.put(b.nodeId(), new HostPort(b.host(), b.port())));
+            response.brokers().forEach(b -> {
+                var replacement = new HostPort(b.host(), b.port());
+                var existing = upstreamBrokers.put(b.nodeId(), replacement);
+                if (!replacement.equals(existing)) {
+                    LOGGER.info("Got upstream for broker {} : {}", b.nodeId(), replacement);
+                }
+            });
             filterContext.forwardResponse(response);
         });
 
-        var endpointMatchResult = endpointProvider.hasMatchingEndpoint(context.sniHostname(),
-                ((InetSocketAddress) context.localAddress()).getPort());
+        var targetPort = ((InetSocketAddress) context.localAddress()).getPort();
+        var endpointMatchResult = endpointProvider.hasMatchingEndpoint(context.sniHostname(), targetPort);
 
         HostPort target;
         if (endpointMatchResult.matched() && endpointMatchResult.nodeId() != null) {
-            target = upstreamBrokers.getOrDefault(endpointMatchResult.nodeId(), targetClusterBootstrap);
+            var upstreamBroker = upstreamBrokers.get(endpointMatchResult.nodeId());
+            if (upstreamBroker != null) {
+                target = upstreamBroker;
+            }
+            else {
+                // TODO: this behaviour is sub-optimal as it means a client will proceed with a connection to the wrong broker.
+                // This will lead to difficult to diagnose failure cases later (produces going to the wrong broker, metadata refresh cycles, etc).
+                LOGGER.warn("An upstream address for broker {} is not yet known, connecting the client to bootstrap instead.", endpointMatchResult.nodeId());
+                target = targetClusterBootstrap;
+            }
+        }
+        else if (endpointMatchResult.matched()) {
+            target = targetClusterBootstrap;
         }
         else {
-            target = targetClusterBootstrap;
+            throw new RuntimeException(
+                    "Connection to %s:%d cannot be routed to an upstream endpoint".formatted(context.sniHostname() == null ? "" : context.sniHostname(),
+                            targetPort));
         }
 
         context.initiateConnect(target.host(), target.port(), filters.toArray(new KrpcFilter[0]));
