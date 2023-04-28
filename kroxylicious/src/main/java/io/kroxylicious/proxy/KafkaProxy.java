6a2eb5e09f19fbed9c004529eaa8d901414d1dd3
kwall <kwall@apache.org>
2023-04-28 12:57:54 +0100

    checkstyle fix only

--- kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
+++ kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
