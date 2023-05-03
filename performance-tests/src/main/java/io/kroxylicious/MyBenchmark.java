f34d4e72575cae8e4ef9e967a049a5bc665e96d0
Robert Young <robeyoun@redhat.com>
2023-05-03 18:09:55 +1200

    Add JMH performance test for the two invoker implementations
    Results on my machine:
    
    ```
    Benchmark                                                 Mode  Cnt          Score          Error  Units
    MyBenchmark.testArrayInvokerFilterHasOneInterface        thrpt   25  221737821.925 ± 12247942.467  ops/s
    MyBenchmark.testArrayInvokerFilterHasTwoInterfaces       thrpt   25  228420653.126 ±  5680379.669  ops/s
    MyBenchmark.testInstanceOfInvokerFilterHasOneInterface   thrpt   25   16459601.924 ±   523213.111  ops/s
    MyBenchmark.testInstanceOfInvokerFilterHasTwoInterfaces  thrpt   25   21738237.024 ±  1020860.406  ops/s
    ```

---
+++ performance-tests/src/main/java/io/kroxylicious/MyBenchmark.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious;
+
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.infra.Blackhole;
+
+import io.kroxylicious.proxy.filter.FilterInvoker;
+import io.kroxylicious.proxy.filter.FilterInvokers;
+
+public class MyBenchmark {
+
+    @org.openjdk.jmh.annotations.State(Scope.Benchmark)
+    public static class BenchState {
+        volatile FilterInvoker instanceOfInvokerFilterHasTwoInterfaces = FilterInvokers.instanceOfInvoker(new TwoInterfaceFilter());
+        volatile FilterInvoker arrayInvokerFilterHasTwoInterfaces = FilterInvokers.arrayInvoker(new TwoInterfaceFilter());
+        volatile FilterInvoker instanceOfInvokerFilterHasOneInterface = FilterInvokers.instanceOfInvoker(new OneInterfaceFilter());
+        volatile FilterInvoker arrayInvokerFilterHasOneInterface = FilterInvokers.arrayInvoker(new OneInterfaceFilter());
+    }
+
+    @Benchmark
+    public void testInstanceOfInvokerFilterHasTwoInterfaces(BenchState state, Blackhole blackhole) {
+        invoke(blackhole, state.instanceOfInvokerFilterHasTwoInterfaces);
+    }
+
+    @Benchmark
+    public void testArrayInvokerFilterHasTwoInterfaces(BenchState state, Blackhole blackhole) {
+        invoke(blackhole, state.arrayInvokerFilterHasTwoInterfaces);
+    }
+
+    @Benchmark
+    public void testInstanceOfInvokerFilterHasOneInterface(BenchState state, Blackhole blackhole) {
+        invoke(blackhole, state.instanceOfInvokerFilterHasOneInterface);
+    }
+
+    @Benchmark
+    public void testArrayInvokerFilterHasOneInterface(BenchState state, Blackhole blackhole) {
+        invoke(blackhole, state.arrayInvokerFilterHasOneInterface);
+    }
+
+    private static void invoke(Blackhole blackhole, FilterInvoker filter) {
+        blackhole.consume(filter.shouldHandleRequest(ApiKeys.PRODUCE, ApiKeys.PRODUCE.latestVersion()));
+        blackhole.consume(filter.shouldHandleRequest(ApiKeys.FETCH, ApiKeys.FETCH.latestVersion()));
+        blackhole.consume(filter.shouldHandleResponse(ApiKeys.PRODUCE, ApiKeys.PRODUCE.latestVersion()));
+        blackhole.consume(filter.shouldHandleResponse(ApiKeys.FETCH, ApiKeys.FETCH.latestVersion()));
+    }
+
+}
