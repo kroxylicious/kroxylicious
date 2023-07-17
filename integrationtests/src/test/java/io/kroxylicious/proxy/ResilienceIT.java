67e6c34c37e51cc7100033d8b624a5404f8aeadc
kwall <kwall@apache.org>
2023-07-17 14:26:12 +0100

    slim down ResilienceIT tests
    we've got tests that probe every broker endpoint in the ExpositionIT tests now.  I like
    ResilienceIT for the confidence it brings that produce/consume is working over a restart,
    so I've opted to keep it in a slimmed down form.

--- integrationtests/src/test/java/io/kroxylicious/proxy/ResilienceIT.java
+++ integrationtests/src/test/java/io/kroxylicious/proxy/ResilienceIT.java
@@ -5,19 +5,14 @@
  */
 package io.kroxylicious.proxy;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.security.GeneralSecurityException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.Consumer;
@@ -25,24 +20,14 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.security.auth.SecurityProtocol;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.kroxylicious.net.IntegrationTestInetAddressResolverProvider;
-import io.kroxylicious.proxy.config.ClusterNetworkAddressConfigProviderDefinitionBuilder;
 import io.kroxylicious.proxy.config.ConfigurationBuilder;
-import io.kroxylicious.proxy.config.FilterDefinitionBuilder;
-import io.kroxylicious.proxy.config.VirtualClusterBuilder;
-import io.kroxylicious.proxy.service.HostPort;
 import io.kroxylicious.testing.kafka.api.KafkaCluster;
 import io.kroxylicious.testing.kafka.common.BrokerCluster;
-import io.kroxylicious.testing.kafka.common.KeytoolCertificateGenerator;
 import io.kroxylicious.testing.kafka.junit5ext.KafkaClusterExtension;
 
 import static io.kroxylicious.test.tester.KroxyliciousConfigUtils.proxy;
@@ -63,67 +48,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ResilienceIT {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResilienceIT.class);
 
-    private static final String SNI_BASE_ADDRESS = IntegrationTestInetAddressResolverProvider.generateFullyQualifiedDomainName("sni");
-
     static @BrokerCluster(numBrokers = 3) KafkaCluster cluster;
-    @TempDir
-    private Path certsDirectory;
-    public static final HostPort SNI_BOOTSTRAP = new HostPort("bootstrap." + SNI_BASE_ADDRESS, 9192);
-    public static final String SNI_BROKER_ADDRESS_PATTERN = "broker-$(nodeId)." + SNI_BASE_ADDRESS;
-    private KeytoolCertificateGenerator brokerCertificateGenerator;
-    private Map<String, Object> securityProtocolConfig;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        brokerCertificateGenerator = new KeytoolCertificateGenerator();
-        brokerCertificateGenerator.generateSelfSignedCertificateEntry("test@redhat.com", "*." + SNI_BASE_ADDRESS, "KI", "RedHat", null, null, "US");
-        Path clientTrustStore = certsDirectory.resolve("kafka.truststore.jks");
-        brokerCertificateGenerator.generateTrustStore(brokerCertificateGenerator.getCertFilePath(), "client",
-                clientTrustStore.toAbsolutePath().toString());
-
-        securityProtocolConfig = Map.of(
-                CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name,
-                SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, clientTrustStore.toAbsolutePath().toString(),
-                SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, this.brokerCertificateGenerator.getPassword());
+    @Test
+    public void kafkaProducerShouldTolerateKroxyliciousRestarting(Admin admin) throws Exception {
+        String randomTopic = UUID.randomUUID().toString();
+        admin.createTopics(List.of(new NewTopic(randomTopic, 1, (short) 1))).all().get();
+        testProducerCanSurviveARestart(withDefaultFilters(proxy(cluster)), randomTopic);
     }
 
     @Test
-    public void producerShouldToleratePortPerBrokerExposedKroxyliciousRestarting(Admin admin) throws Exception {
+    public void kafkaConsumerShouldTolerateKroxyliciousRestarting(Admin admin) throws Exception {
         String randomTopic = UUID.randomUUID().toString();
         admin.createTopics(List.of(new NewTopic(randomTopic, 1, (short) 1))).all().get();
-        testProducerCanSurviveARestart(withDefaultFilters(proxy(cluster)), randomTopic, null);
+        testConsumerCanSurviveKroxyliciousRestart(withDefaultFilters(proxy(cluster)), randomTopic);
     }
 
-    @Test
-    public void producerShouldTolerateSniExposedKroxyliciousRestarting(Admin admin) throws Exception {
-        String randomTopic = UUID.randomUUID().toString();
-        admin.createTopics(List.of(new NewTopic(randomTopic, 1, (short) 1))).all().get();
-        testProducerCanSurviveARestart(sniConfiguration(cluster), randomTopic, securityProtocolConfig);
-    }
-
-    @Test
-    public void consumerShouldToleratePortPerBrokerExposedKroxyliciousRestarting(Admin admin) throws Exception {
-        String randomTopic = UUID.randomUUID().toString();
-        admin.createTopics(List.of(new NewTopic(randomTopic, 1, (short) 1))).all().get();
-        testConsumerCanSurviveKroxyliciousRestart(withDefaultFilters(proxy(cluster)), randomTopic, null);
-    }
-
-    @Test
-    public void consumerShouldTolerateSniExposedKroxyliciousRestarting(Admin admin) throws Exception {
-        String randomTopic = UUID.randomUUID().toString();
-        admin.createTopics(List.of(new NewTopic(randomTopic, 1, (short) 1))).all().get();
-        testConsumerCanSurviveKroxyliciousRestart(sniConfiguration(cluster), randomTopic, securityProtocolConfig);
-    }
-
-    private static void testConsumerCanSurviveKroxyliciousRestart(ConfigurationBuilder builder, String topic, Map<String, Object> additionalClientConfig)
+    private static void testConsumerCanSurviveKroxyliciousRestart(ConfigurationBuilder builder, String topic)
             throws Exception {
         var producerConfig = new HashMap<String, Object>(Map.of(CLIENT_ID_CONFIG, "producer",
                 DELIVERY_TIMEOUT_MS_CONFIG, 3_600_000));
         var consumerConfig = new HashMap<String, Object>(Map.of(CLIENT_ID_CONFIG, "consumer",
                 ConsumerConfig.GROUP_ID_CONFIG, "mygroup",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"));
-        Optional.ofNullable(additionalClientConfig).ifPresent(producerConfig::putAll);
-        Optional.ofNullable(additionalClientConfig).ifPresent(consumerConfig::putAll);
 
         Consumer<String, String> consumer;
         try (var tester = kroxyliciousTester(builder);
@@ -150,7 +97,7 @@ public class ResilienceIT {
         }
     }
 
-    private void testProducerCanSurviveARestart(ConfigurationBuilder builder, String topic, Map<String, Object> additionalClientConfig) throws Exception {
+    private void testProducerCanSurviveARestart(ConfigurationBuilder builder, String topic) throws Exception {
 
         var producerConfig = new HashMap<String, Object>(Map.of(CLIENT_ID_CONFIG, "producer",
                 DELIVERY_TIMEOUT_MS_CONFIG, 3_600_000,
@@ -160,8 +107,6 @@ public class ResilienceIT {
         var consumerConfig = new HashMap<String, Object>(Map.of(CLIENT_ID_CONFIG, "consumer",
                 ConsumerConfig.GROUP_ID_CONFIG, "mygroup",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"));
-        Optional.ofNullable(additionalClientConfig).ifPresent(producerConfig::putAll);
-        Optional.ofNullable(additionalClientConfig).ifPresent(consumerConfig::putAll);
 
         Producer<String, String> producer;
         Consumer<String, String> consumer;
@@ -186,30 +131,6 @@ public class ResilienceIT {
             assertThat(records).hasSize(2);
             assertThat(records.iterator()).toIterable().map(ConsumerRecord::value).containsExactly("Hello, world!", "Hello, again!");
         }
-    }
-
-    private ConfigurationBuilder sniConfiguration(KafkaCluster cluster) throws IOException, GeneralSecurityException {
-
-        String bootstrapServers = cluster.getBootstrapServers();
-
-        return new ConfigurationBuilder()
-                .addToVirtualClusters("demo", new VirtualClusterBuilder()
-                        .withNewTls()
-                        .withNewKeyStoreKey()
-                        .withStoreFile(brokerCertificateGenerator.getKeyStoreLocation())
-                        .withNewInlinePasswordStoreProvider(brokerCertificateGenerator.getPassword())
-                        .endKeyStoreKey()
-                        .endTls()
-                        .withNewTargetCluster()
-                        .withBootstrapServers(bootstrapServers)
-                        .endTargetCluster()
-                        .withClusterNetworkAddressConfigProvider(
-                                new ClusterNetworkAddressConfigProviderDefinitionBuilder("SniRouting")
-                                        .withConfig("bootstrapAddress", SNI_BOOTSTRAP)
-                                        .withConfig("brokerAddressPattern", SNI_BROKER_ADDRESS_PATTERN)
-                                        .build())
-                        .build())
-                .addToFilters(new FilterDefinitionBuilder("ApiVersions").build());
     }
 
 }
