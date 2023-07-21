e913ce00b3f851cd5e24e118247ee36952f8e078
kwall <kwall@apache.org>
2023-07-21 18:53:22 +0100

    Fix #472: Respect logFrame/logNetwork options
    * declare loggers in the log4j2 configuration file corresponding to the frame and network loggers
    * also define two environment variables that make it easy to override useful loggers to aid problem resolution.
    
    Signed-off-by: kwall <kwall@apache.org>

--- kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
+++ kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -53,6 +53,7 @@ import io.kroxylicious.proxy.service.HostPort;
 public final class KafkaProxy implements AutoCloseable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProxy.class);
+    private static final Logger STARTUP_SHUTDOWN_LOGGER = LoggerFactory.getLogger("io.kroxylicious.proxy.internal.StartupShutdownLogger");
 
     private final NetworkBindingOperationProcessor bindingOperationProcessor = new DefaultNetworkBindingOperationProcessor();
     private final EndpointRegistry endpointRegistry = new EndpointRegistry(bindingOperationProcessor);
@@ -89,7 +90,7 @@ public final class KafkaProxy implements AutoCloseable {
      * @return This proxy.
      */
     public KafkaProxy startup() throws InterruptedException {
-        LOGGER.info("Kroxylicious starting");
+        STARTUP_SHUTDOWN_LOGGER.info("Kroxylicious starting");
 
         if (running.getAndSet(true)) {
             throw new IllegalStateException("This proxy is already running");
@@ -204,7 +205,7 @@ public final class KafkaProxy implements AutoCloseable {
             throw new IllegalStateException("This proxy is not running");
         }
         try {
-            LOGGER.info("Shutting down");
+            STARTUP_SHUTDOWN_LOGGER.info("Shutting down");
             endpointRegistry.shutdown().handle((u, t) -> {
                 bindingOperationProcessor.close();
                 var closeFutures = new ArrayList<Future<?>>();
