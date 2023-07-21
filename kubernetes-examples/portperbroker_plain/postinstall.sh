9254a44dd0a741131a3bab4a4894b40beeeba97f
kwall <kwall@apache.org>
2023-07-21 12:35:23 +0100

    Add kcat example command into portperbroker_plain example

--- kubernetes-examples/portperbroker_plain/postinstall.sh
+++ kubernetes-examples/portperbroker_plain/postinstall.sh
@@ -7,7 +7,29 @@
 
 set -eo pipefail
 
+STRIMZI_KAFKA=quay.io/strimzi/kafka:0.35.0-kafka-3.4.0
+KCAT=quay.io/kroxylicious/kcat:1.7.1
+TOPIC=my-topic
+BOOTSTRAP=kroxylicious-service:9292
+
+GREEN='\033[0;32m'
+NOCOLOUR='\033[0m'
+
+echo "Here are some commands to try things out. Run the producer and consumer in separate terminals"
+
+echo -e "${GREEN}Apache Kafka Client${NOCOLOUR}"
+
 echo "To produce to kroxylicious:"
-echo "kubectl -n kafka run kafka-producer -ti --image=quay.io/strimzi/kafka:0.35.0-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server kroxylicious-service:9292 --topic my-topic"
+echo "kubectl -n kafka run java-kafka-producer -ti --image=${STRIMZI_KAFKA} --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server ${BOOTSTRAP} --topic ${TOPIC}"
 echo "To consume from kroxylicious:"
-echo "kubectl -n kafka run kafka-consumer -ti --image=quay.io/strimzi/kafka:0.35.0-kafka-3.4.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server kroxylicious-service:9292 --topic my-topic --from-beginning"
+echo "kubectl -n kafka run java-kafka-consumer -ti --image=${STRIMZI_KAFKA} --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server ${BOOTSTRAP} --topic ${TOPIC} --from-beginning"
+
+echo -e "${GREEN}Edenhill Kcat${NOCOLOUR}"
+
+echo "Note: kcat -P will send each line as a separate record, but you need to press ^D to actually get kcat to start sending - see  https://github.com/edenhill/kcat/issues/322"
+
+echo "To produce to kroxylicious:"
+echo "kubectl -n kafka run kcat-kafka-producer -ti --image=${KCAT} --rm=true --restart=Never -- -P -t ${TOPIC} -b ${BOOTSTRAP}"
+echo "To consume from kroxylicious:"
+echo "kubectl -n kafka run kcat-kafka-consumer -ti --image=${KCAT} --rm=true --restart=Never -- -C -t ${TOPIC} -b ${BOOTSTRAP} -o beginning"
+
