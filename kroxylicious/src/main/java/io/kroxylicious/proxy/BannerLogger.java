kroxylicious/kroxylicious

commit 285afefdf781cd706785389d0eccbe984f183829

Fix #472: Respect logFrame/logNetwork options (#473)
* Fix #472: Respect logFrame/logNetwork options

* declare loggers in the log4j2 configuration file corresponding to the frame and network loggers
* also define two environment variables that make it easy to override useful loggers to aid problem resolution.
*
Signed-off-by: kwall <kwall@apache.org>
Co-authored-by: Sam Barker <sbarker@redhat.com>

kroxylicious/src/main/java/io/kroxylicious/proxy/BannerLogger.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+public class BannerLogger {
+
+    public static final String DEFAULT_BANNER_LOCATION = "banner.txt";
+    private final Logger targetLogger;
+    private final Supplier<Stream<String>> bannerReader;
+    private final Level targetLevel;
+
+    public BannerLogger() {
+        this(LoggerFactory.getLogger("io.kroxylicious.proxy.internal.StartupShutdownLogger"),
+                new BannerSupplier(DEFAULT_BANNER_LOCATION));
+    }
+
+    BannerLogger(Logger targetLogger, Supplier<Stream<String>> bannerReader) {
+        this.targetLogger = targetLogger;
+        this.bannerReader = bannerReader;
+        targetLevel = Level.INFO; // TODO should this be configurable?¡
+    }
+
+    public void log() {
+        bannerReader.get().forEach(line -> targetLogger.atLevel(targetLevel).log(line));
+    }
+
+    /* test */ static class BannerSupplier implements Supplier<Stream<String>> {
+
+        private final Supplier<Stream<String>> linesSupplier;
+        private final Set<String> licenseLines;
+
+        BannerSupplier(String resourceName) {
+            this(new FileLinesSupplier(resourceName), new FileLinesSupplier("etc/LICENSE.txt"));
+        }
+
+        BannerSupplier(Supplier<Stream<String>> bannerLinesSupplier, Supplier<Stream<String>> licenceStream) {
+            linesSupplier = bannerLinesSupplier;
+            licenseLines = Stream.concat(Stream.of("===="), licenceStream.get()).collect(Collectors.toSet());
+        }
+
+        @Override
+        public Stream<String> get() {
+            return linesSupplier.get().filter(Predicate.not(licenseLines::contains));
+        }
+    }
+
+    /* test */ static class FileLinesSupplier implements Supplier<Stream<String>> {
+
+        private final String resourceName;
+
+        FileLinesSupplier(String resourceName) {
+            this.resourceName = resourceName;
+        }
+
+        @Override
+        public Stream<String> get() {
+            final InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(resourceName);
+            if (Objects.isNull(resourceStream)) {
+                return Stream.empty();
+            }
+            else {
+                final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceStream));
+                return bufferedReader.lines();
+            }
+        }
+    }
+}

kroxylicious/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -53,6 +53,7 @@ import io.kroxylicious.proxy.service.HostPort;
 public final class KafkaProxy implements AutoCloseable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProxy.class);
+    private static final Logger STARTUP_SHUTDOWN_LOGGER = LoggerFactory.getLogger("io.kroxylicious.proxy.internal.StartupShutdownLogger");
 
     private final NetworkBindingOperationProcessor bindingOperationProcessor = new DefaultNetworkBindingOperationProcessor();
     private final EndpointRegistry endpointRegistry = new EndpointRegistry(bindingOperationProcessor);
@@ -89,11 +90,11 @@ public final class KafkaProxy implements AutoCloseable {
      * @return This proxy.
      */
     public KafkaProxy startup() throws InterruptedException {
-        LOGGER.info("Kroxylicious starting");
-
         if (running.getAndSet(true)) {
             throw new IllegalStateException("This proxy is already running");
         }
+
+        logStartup();
 
         var portConflictDefector = new PortConflictDetector();
         Optional<HostPort> adminHttpHostPort = Optional.ofNullable(shouldBindAdminEndpoint() ? new HostPort(adminHttpConfig.host(), adminHttpConfig.port()) : null);
@@ -125,6 +126,10 @@ public final class KafkaProxy implements AutoCloseable {
         Metrics.inboundDownstreamMessagesCounter();
         Metrics.inboundDownstreamDecodedMessagesCounter();
         return this;
+    }
+
+    private static void logStartup() {
+        new BannerLogger().log();
     }
 
     private ServerBootstrap buildServerBootstrap(EventGroupConfig virtualHostEventGroup, KafkaProxyInitializer kafkaProxyInitializer) {
@@ -204,7 +209,7 @@ public final class KafkaProxy implements AutoCloseable {
             throw new IllegalStateException("This proxy is not running");
         }
         try {
-            LOGGER.info("Shutting down");
+            STARTUP_SHUTDOWN_LOGGER.info("Shutting down");
             endpointRegistry.shutdown().handle((u, t) -> {
                 bindingOperationProcessor.close();
                 var closeFutures = new ArrayList<Future<?>>();

kroxylicious/src/main/resources/log4j2.properties
@@ -11,11 +11,29 @@ appender.console.name = STDOUT
 appender.console.layout.type = PatternLayout
 appender.console.layout.pattern = %d{yyyy-MM-dd HH:mm:ss} <%t> %-5p %c:%L - %m%n
 
-rootLogger.level = WARN
+rootLogger.level = ${env:KROXYLICIOUS_ROOT_LOG_LEVEL:-WARN}
 rootLogger.appenderRefs = stdout
 rootLogger.appenderRef.console.ref = STDOUT
 rootLogger.additivity = false
 
-#logger.netty.name = io.netty
-#logger.netty.level = TRACE
-#logger.netty.additivity = false
+# The (Down|Up)streamNetworkLogger and (Down|Up)streamFrameLogger loggers are required to allow
+# the virtual cluster logNetwork/logFrames feature to work.  The log level needs to be in sync
+# with the logger level used in the code.
+logger.DownstreamNetworkLogger.name = io.kroxylicious.proxy.internal.DownstreamNetworkLogger
+logger.DownstreamNetworkLogger.level = INFO
+
+logger.UpstreamNetworkLogger.name = io.kroxylicious.proxy.internal.UpstreamNetworkLogger
+logger.UpstreamNetworkLogger.level = INFO
+
+logger.DownstreamFrameLogger.name = io.kroxylicious.proxy.internal.DownstreamFrameLogger
+logger.DownstreamFrameLogger.level = INFO
+
+logger.UpstreamFrameLogger.name = io.kroxylicious.proxy.internal.UpstreamFrameLogger
+logger.UpstreamFrameLogger.level = INFO
+
+logger.StartupShutdownLogger.name = io.kroxylicious.proxy.internal.StartupShutdownLogger
+logger.StartupShutdownLogger.level = INFO
+
+logger.test.name = io.kroxylicious.proxy.KafkaProxy
+logger.test.level = ${env:KROXYLICIOUS_LOG_LEVEL:-${env:KROXYLICIOUS_ROOT_LOG_LEVEL:-WARN}}
+

kroxylicious/src/test/java/io/kroxylicious/proxy/BannerLoggerTest.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.spi.LoggingEventBuilder;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BannerLoggerTest {
+
+    private BannerLogger bannerLogger;
+    @Mock
+    private LoggingEventBuilder loggingEventBuilder;
+
+    private Stream<String> bannerStream = Stream.empty();
+
+    @BeforeEach
+    void setUp() {
+        Logger testLogger = mock(Logger.class);
+        when(testLogger.atLevel(any())).thenReturn(loggingEventBuilder);
+        bannerLogger = new BannerLogger(testLogger, () -> bannerStream);
+    }
+
+    @Test
+    void shouldAppendToConfiguredLogger() {
+        // Given
+        bannerStream = Stream.of("banner_text");
+
+        // When
+        bannerLogger.log();
+
+        // Then
+        verify(loggingEventBuilder).log(anyString());
+    }
+
+    @Test
+    void shouldLogSingleLineBanner() {
+        // Given
+        bannerStream = Stream.of("banner_text");
+
+        // When
+        bannerLogger.log();
+
+        // Then
+        verify(loggingEventBuilder).log("banner_text");
+    }
+}

kroxylicious/src/test/java/io/kroxylicious/proxy/BannerSupplierTest.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BannerSupplierTest {
+
+    private List<String> licenseStream;
+
+    @BeforeEach
+    void setUp() {
+        licenseStream = List.of("====", "Copyright Kroxylicious Authors.",
+                "Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0");
+    }
+
+    @Test
+    void shouldRemoveLicenseHeader() {
+        // Given
+        final BannerLogger.BannerSupplier bannerSupplier = new BannerLogger.BannerSupplier(licenseStream::stream, licenseStream::stream);
+
+        // When
+        final Stream<String> actualBannerStream = bannerSupplier.get();
+
+        // Then
+        assertThat(actualBannerStream).isEmpty();
+    }
+
+    @Test
+    void shouldPassThroughBannerStream() {
+        // Given
+        final List<String> bannerLines = List.of("one", "two", "three");
+        final BannerLogger.BannerSupplier bannerSupplier = new BannerLogger.BannerSupplier(bannerLines::stream, Stream::empty);
+
+        // When
+        final Stream<String> actualBannerStream = bannerSupplier.get();
+
+        // Then
+        assertThat(actualBannerStream).containsExactlyInAnyOrderElementsOf(bannerLines);
+    }
+
+    @Test
+    void shouldPassThroughRemainingBannerStream() {
+        // Given
+        final List<String> expectedLines = List.of("one", "two", "three");
+        final List<String> bannerLines = new ArrayList<>();
+        bannerLines.addAll(licenseStream);
+        bannerLines.addAll(expectedLines);
+
+        final BannerLogger.BannerSupplier bannerSupplier = new BannerLogger.BannerSupplier(bannerLines::stream, licenseStream::stream);
+
+        // When
+        final Stream<String> actualBannerStream = bannerSupplier.get();
+
+        // Then
+        assertThat(actualBannerStream).containsExactlyInAnyOrderElementsOf(expectedLines);
+    }
+}

kroxylicious/src/test/java/io/kroxylicious/proxy/FileLinesSupplierTest.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy;
+
+import java.util.stream.Stream;
+
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.Test;
+
+import static java.util.function.Predicate.not;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FileLinesSupplierTest {
+
+    @Test
+    void shouldSupplyEmptyStreamForMissingResource() {
+        // Given
+        final BannerLogger.FileLinesSupplier bannerSupplier = new BannerLogger.FileLinesSupplier("random.txt");
+
+        // When
+        final Stream<String> actualStream = bannerSupplier.get();
+
+        // Then
+        assertThat(actualStream).isEmpty();
+    }
+
+    @Test
+    void shouldSupplyStreamForDefaultResource() {
+        // Given
+        final BannerLogger.FileLinesSupplier bannerSupplier = new BannerLogger.FileLinesSupplier("banner.txt");
+
+        // When
+        final Stream<String> actualStream = bannerSupplier.get();
+
+        // Then
+        assertThat(actualStream).areAtLeastOne(new Condition<>(not(String::isBlank), "Not blank"));
+    }
+}

commit 13e4bc41d72fa1c4953ba50b7e6ccb4a0abd25ca

Fix reader charset issue introduced by 285afefdf781cd706785389d0eccbe984f183829 Fixing issue reported by SonaLift

kroxylicious/src/main/java/io/kroxylicious/proxy/BannerLogger.java
@@ -7,8 +7,10 @@
 package io.kroxylicious.proxy;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -72,13 +74,17 @@ public class BannerLogger {
 
         @Override
         public Stream<String> get() {
-            final InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(resourceName);
-            if (Objects.isNull(resourceStream)) {
-                return Stream.empty();
+            try (var resourceStream = getClass().getClassLoader().getResourceAsStream(resourceName)) {
+                if (Objects.isNull(resourceStream)) {
+                    return Stream.empty();
+                }
+                else {
+                    final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceStream, Charset.defaultCharset()));
+                    return bufferedReader.lines();
+                }
             }
-            else {
-                final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceStream));
-                return bufferedReader.lines();
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
         }
     }