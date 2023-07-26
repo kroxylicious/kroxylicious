5faa42e65fdce327a6a4b1718ee823698f57159c
Sam Barker <sbarker@redhat.com>
2023-07-26 15:04:51 +1200

    Add a script to repeatedly run the same integration test pattern.
    We've seen issues with intermittent integration tests, including surefire reporting VM crashes so being able to repeatedly run a test via maven becomes quite useful.

---
+++ scripts/repeatIT.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Copyright Kroxylicious Authors.
+#
+# Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+#
+
+targetTest=${1:-ExpositionIT}
+
+logFile=/tmp/mvn-${targetTest}-$(date +"%d-%m-%Y_%H:%M").log
+
+mvn clean install -DskipTests=true
+
+for COUNT in {1..10} ; do
+    echo "=============== RUN ${COUNT} STARTING =============== " | tee --append "${logFile}"
+    mvn verify -Dit.test=${targetTest} -pl :integrationtests >> "${logFile}"
+    EC=$?
+    echo "=============== RUN ${COUNT} FINISHED =============== " | tee --append "${logFile}"
+    if [[ ${EC} -ne 0 ]]; then
+      exit $EC
+    fi
+done
