SamBarker/kproxy

Optionally name clusters.
Currently not sure about making the name optional, but it avoids this being a breaking change.

integrationtests/src/test/java/io/kroxylicious/proxy/ExpositionIT.java
@@ -50,7 +50,7 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 /**
  * Integration tests that focus on the ability to present virtual clusters, with various numbers of brokers)
  * to the kafka clients
- *
+ * <p>
  * TODO corner case test - verify kroxy's ability to recover for a temporary port already bound condition.
  */
 @ExtendWith(KafkaClusterExtension.class)
@@ -78,6 +78,7 @@ public class ExpositionIT {
 
         var builder = new ConfigurationBuilder()
                 .addToVirtualClusters("demo", new VirtualClusterBuilder()
+                        .withClusterName("demo")
                         .withNewTargetCluster()
                         .withBootstrapServers(bootstrapServers)
                         .endTargetCluster()

kroxylicious/kroxylicious

integrationtests/src/test/java/io/kroxylicious/proxy/ExpositionIT.java
@@ -224,6 +224,7 @@ public class ExpositionIT {
         return Stream.of(
                 Arguments.of("PortPerBroker",
                         new VirtualClusterBuilder()
+                                .withClusterName("PortPerBroker")
                                 .withNewTls()
                                 .withNewKeyStoreKey()
                                 .withStoreFile(portPerBrokerKeystoreTrustStorePair.brokerKeyStore())
@@ -241,6 +242,7 @@ public class ExpositionIT {
                                 SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, portPerBrokerKeystoreTrustStorePair.password())),
                 Arguments.of("SniRouting",
                         new VirtualClusterBuilder()
+                                .withClusterName("SniRouting")
                                 .withNewTls()
                                 .withNewKeyStoreKey()
                                 .withStoreFile(sniKeystoreTrustStorePair.brokerKeyStore())