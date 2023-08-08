835fa24967eadf0439101bab75955197af0bf22f
Sam Barker <sbarker@redhat.com>
2023-08-08 17:01:15 +1200

    Add assertions to TlsIT
    Signed-off-by: Sam Barker <sbarker@redhat.com>

--- integrationtests/src/test/java/io/kroxylicious/proxy/TlsIT.java
+++ integrationtests/src/test/java/io/kroxylicious/proxy/TlsIT.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,8 +45,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Integration tests focused on Kroxylicious ability to use TLS for both the upstream and downstream.
- *
- * TODO add integration tests covering kroylicious's ability to use JKS and PEM material. Needs https://github.com/kroxylicious/kroxylicious-junit5-extension/issues/120
+ * <p>
+ * TODO add integration tests covering kroylicious's ability to use JKS and PEM material. Needs <a href="https://github.com/kroxylicious/kroxylicious-junit5-extension/issues/120">issues#120</a>
  */
 @ExtendWith(KafkaClusterExtension.class)
 class TlsIT extends BaseIT {
@@ -93,7 +94,8 @@ class TlsIT extends BaseIT {
 
         try (var tester = kroxyliciousTester(builder); var admin = tester.admin("demo")) {
             // do some work to ensure connection is opened
-            createTopic(admin, TOPIC, 1);
+            final CreateTopicsResult createTopicsResult = createTopic(admin, TOPIC, 1);
+            assertThat(createTopicsResult.all()).isDone();
         }
     }
 
@@ -110,8 +112,8 @@ class TlsIT extends BaseIT {
 
         var trustAnchors = params.getTrustAnchors();
         var certificates = trustAnchors.stream().map(TrustAnchor::getTrustedCert).toList();
-        assertThat(certificates).isNotNull();
-        assertThat(certificates).hasSizeGreaterThan(0);
+        assertThat(certificates).isNotNull()
+                .hasSizeGreaterThan(0);
 
         var file = writeTrustToTemporaryFile(certificates);
 
@@ -131,12 +133,13 @@ class TlsIT extends BaseIT {
 
         try (var tester = kroxyliciousTester(builder); var admin = tester.admin("demo")) {
             // do some work to ensure connection is opened
-            createTopic(admin, TOPIC, 1);
+            final CreateTopicsResult createTopicsResult = createTopic(admin, TOPIC, 1);
+            assertThat(createTopicsResult.all()).isDone();
         }
     }
 
     @Test
-    void upstreamUsesTlsInsecure(@Tls KafkaCluster cluster) throws Exception {
+    void upstreamUsesTlsInsecure(@Tls KafkaCluster cluster) {
         var bootstrapServers = cluster.getBootstrapServers();
 
         var builder = new ConfigurationBuilder()
@@ -152,7 +155,8 @@ class TlsIT extends BaseIT {
 
         try (var tester = kroxyliciousTester(builder); var admin = tester.admin("demo")) {
             // do some work to ensure connection is opened
-            createTopic(admin, TOPIC, 1);
+            final CreateTopicsResult createTopicsResult = createTopic(admin, TOPIC, 1);
+            assertThat(createTopicsResult.all()).isDone();
         }
     }
 
@@ -190,7 +194,8 @@ class TlsIT extends BaseIT {
                                 SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, clientTrustStore.toAbsolutePath().toString(),
                                 SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, downstreamCertificateGenerator.getPassword()))) {
             // do some work to ensure connection is opened
-            createTopic(admin, TOPIC, 1);
+            final CreateTopicsResult createTopicsResult = createTopic(admin, TOPIC, 1);
+            assertThat(createTopicsResult.all()).isDone();
         }
     }
 
