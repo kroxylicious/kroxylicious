6815fcbbb6c873c98bbc08f6cb9bc3ae1b0168e4
Keith Wall <kwall@apache.org>
2023-08-15 08:15:56 +0100

    Update package-info.java
    Add paragraph describing behaviour on completion stage that completes exceptionally, or timeout.

--- kroxylicious-api/src/main/java/io/kroxylicious/proxy/filter/package-info.java
+++ kroxylicious-api/src/main/java/io/kroxylicious/proxy/filter/package-info.java
@@ -14,19 +14,21 @@
  * message is to be forwarded to the next filter in the chain (or client/broker if at the chain's beginning
  * or end).  It is also used to carry instructions such as indicating that the connection must be closed,
  * or a message dropped.</p>
- * <h3 id='implementing.deferringForward'>Deferring Forwards</h3>
  * <p>If the filter returns a CompletionStage that is already completed normally, Kroxylicious will immediately
- * perform the action described by the `FilterResult`.</p>
- * <p>The filter may return a `CompletionStage` that is not yet completed. When this happens, Kroxylicious will pause
+ * perform the action described by the FilterResult.</p>
+ * <h3 id='implementing.deferringForward'>Deferring Forwards</h3>
+ * <p>The filter may return a CompletionStage that is not yet completed. When this happens, Kroxylicious will pause
  * reading from the downstream (the Client writes will eventually block), and it begins to queue up in-flight
  * requests/responses arriving at the filter.  This is done so that message order is maintained.  Once the
- * `CompletionStage` completes, the action described  by the `FilterResult` is performed, reading from the downstream
+ * CompletionStage completes, the action described  by the FilterResult is performed, reading from the downstream
  * resumes and any queued up requests/responses are processed.</p>
- * <p>Note: The pausing of reads from the downstream is a relatively costly operation.  To maintain optimal performance
- * filter implementations should minimise the occasions on which an incomplete `CompletionStage` is returned.</p>
+ * <p>IMPORTANT: The pausing of reads from the downstream is a relatively costly operation.  To maintain optimal performance
+ * filter implementations should minimise the occasions on which an incomplete CompletionStage is returned.</p>
+ * <p>If the CompletionStage completes exceptionally, the connection is closed.  This also applies if the
+ * CompletionStage does not complete within a timeout (20000 milliseconds).</p>
  * <h3 id='implementing.createFilterResults'>Creating Filter Result objects</h3>
- * <p>The {@link io.kroxylicious.proxy.filter.KrpcFilterContext} is the factory for the `FilterResult` objects.</p>
- * <p>There are two convenience methods that simply allow a filter to immediately forward a result.:</p>
+ * <p>The {@link io.kroxylicious.proxy.filter.KrpcFilterContext} is the factory for the FilterResul` objects.</p>
+ * <p>There are two convenience methods that simply allow a filter to immediately forward a result:</p>
  * <ul>
  *     <li>{@link io.kroxylicious.proxy.filter.KrpcFilterContext#forwardRequest(org.apache.kafka.common.message.RequestHeaderData, org.apache.kafka.common.protocol.ApiMessage)}, and</li>
  *     <li>{@link io.kroxylicious.proxy.filter.KrpcFilterContext#forwardResponse(org.apache.kafka.common.message.ResponseHeaderData, org.apache.kafka.common.protocol.ApiMessage)}.</li>
