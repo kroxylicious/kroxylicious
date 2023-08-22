4cddb51317dcc4566ec8b94c1069cfa5ae89a929
Robert Young <robeyoun@redhat.com>
2023-08-23 11:23:17 +1200

    Add tests for ClusterNetworkAddressConfigProviderContributorManager

---
+++ kroxylicious/src/test/java/io/kroxylicious/proxy/internal/clusternetworkaddressconfigprovider/TestClusterNetworkAddressConfigProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.internal.clusternetworkaddressconfigprovider;
+
+import org.testcontainers.shaded.org.apache.commons.lang3.NotImplementedException;
+
+import io.kroxylicious.proxy.config.BaseConfig;
+import io.kroxylicious.proxy.service.ClusterNetworkAddressConfigProvider;
+import io.kroxylicious.proxy.service.Context;
+import io.kroxylicious.proxy.service.HostPort;
+
+public record TestClusterNetworkAddressConfigProvider(String shortName, BaseConfig config, Context context) implements ClusterNetworkAddressConfigProvider {
+    @Override
+    public HostPort getClusterBootstrapAddress() {
+        throw new NotImplementedException("not implemented!");
+    }
+
+    @Override
+    public HostPort getBrokerAddress(int nodeId) throws IllegalArgumentException {
+        throw new NotImplementedException("not implemented!");
+    }
+}
