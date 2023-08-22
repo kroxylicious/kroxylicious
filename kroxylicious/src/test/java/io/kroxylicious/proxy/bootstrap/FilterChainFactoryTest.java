ddfda7e32bb457f7c9950438a5bce34862e863eb
Robert Young <robeyoun@redhat.com>
2023-08-23 11:55:05 +1200

    Test filter construction in FilterChainFactory

--- kroxylicious/src/test/java/io/kroxylicious/proxy/bootstrap/FilterChainFactoryTest.java
+++ kroxylicious/src/test/java/io/kroxylicious/proxy/bootstrap/FilterChainFactoryTest.java
@@ -8,13 +8,19 @@ package io.kroxylicious.proxy.bootstrap;
 
 import java.util.List;
 
+import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import io.kroxylicious.proxy.config.Configuration;
+import io.kroxylicious.proxy.config.FilterDefinition;
 import io.kroxylicious.proxy.filter.FilterAndInvoker;
 import io.kroxylicious.proxy.filter.FilterContributorContext;
+import io.kroxylicious.proxy.internal.filter.Config;
+import io.kroxylicious.proxy.internal.filter.TestFilterContributor;
+import io.kroxylicious.proxy.internal.filter.TestKrpcFilter;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -36,6 +42,39 @@ class FilterChainFactoryTest {
         List<FilterAndInvoker> filters = filterChainFactory.createFilters();
         assertNotNull(filters, "Filters list should not be null");
         assertTrue(filters.isEmpty(), "Filters list should be empty");
+    }
+
+    @Test
+    void testSingleFilterConstruction() {
+        FilterContributorContext mockContext = Mockito.mock(FilterContributorContext.class);
+        Config config = new Config();
+        FilterChainFactory filterChainFactory = new FilterChainFactory(new Configuration(null, null, List.of(new FilterDefinition(TestFilterContributor.SHORT_NAME,
+                config)), null, true), mockContext);
+        List<FilterAndInvoker> filters = filterChainFactory.createFilters();
+        assertThat(filters).hasSize(1).first().extracting(FilterAndInvoker::filter).isInstanceOfSatisfying(TestKrpcFilter.class, testKrpcFilter -> {
+            assertThat(testKrpcFilter.context()).isSameAs(mockContext);
+            assertThat(testKrpcFilter.config()).isSameAs(config);
+        });
+    }
+
+    @Test
+    void testMultipleFilterConstruction() {
+        FilterContributorContext mockContext = Mockito.mock(FilterContributorContext.class);
+        Config configA = new Config();
+        FilterDefinition definitionA = new FilterDefinition(TestFilterContributor.SHORT_NAME, configA);
+        Config configB = new Config();
+        FilterDefinition definitionB = new FilterDefinition(TestFilterContributor.SHORT_NAME, configB);
+        FilterChainFactory filterChainFactory = new FilterChainFactory(new Configuration(null, null, List.of(definitionA, definitionB), null, true), mockContext);
+        List<FilterAndInvoker> filters = filterChainFactory.createFilters();
+        ListAssert<FilterAndInvoker> filterAndInvokerListAssert = assertThat(filters).hasSize(2);
+        filterAndInvokerListAssert.element(0).extracting(FilterAndInvoker::filter).isInstanceOfSatisfying(TestKrpcFilter.class, testKrpcFilter -> {
+            assertThat(testKrpcFilter.context()).isSameAs(mockContext);
+            assertThat(testKrpcFilter.config()).isSameAs(configA);
+        });
+        filterAndInvokerListAssert.element(1).extracting(FilterAndInvoker::filter).isInstanceOfSatisfying(TestKrpcFilter.class, testKrpcFilter -> {
+            assertThat(testKrpcFilter.context()).isSameAs(mockContext);
+            assertThat(testKrpcFilter.config()).isSameAs(configB);
+        });
     }
 
 }
--- kroxylicious/src/test/java/io/kroxylicious/proxy/internal/filter/TestKrpcFilter.java
+++ kroxylicious/src/test/java/io/kroxylicious/proxy/internal/filter/TestKrpcFilter.java
@@ -6,9 +6,21 @@
 
 package io.kroxylicious.proxy.internal.filter;
 
+import java.util.concurrent.CompletionStage;
+
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.ApiMessage;
+
 import io.kroxylicious.proxy.config.BaseConfig;
 import io.kroxylicious.proxy.filter.FilterContributorContext;
-import io.kroxylicious.proxy.filter.KrpcFilter;
+import io.kroxylicious.proxy.filter.KrpcFilterContext;
+import io.kroxylicious.proxy.filter.RequestFilter;
+import io.kroxylicious.proxy.filter.RequestFilterResult;
 
-public record TestKrpcFilter(String shortName, BaseConfig config, FilterContributorContext context) implements KrpcFilter {
+public record TestKrpcFilter(String shortName, BaseConfig config, FilterContributorContext context) implements RequestFilter {
+    @Override
+    public CompletionStage<RequestFilterResult> onRequest(ApiKeys apiKey, RequestHeaderData header, ApiMessage request, KrpcFilterContext context) {
+        return context.forwardRequest(header, request);
+    }
 }
