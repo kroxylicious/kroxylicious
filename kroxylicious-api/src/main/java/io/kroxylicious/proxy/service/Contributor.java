6ccd3b540c3e1a86a929235cdc9144aae6c0b2bc
Sam Barker <sbarker@redhat.com>
2023-09-14 12:09:04 +1200

    shortName -> typeName
    Signed-off-by: Sam Barker <sbarker@redhat.com>

--- kroxylicious-api/src/main/java/io/kroxylicious/proxy/service/Contributor.java
+++ kroxylicious-api/src/main/java/io/kroxylicious/proxy/service/Contributor.java
@@ -17,36 +17,36 @@ public interface Contributor<T, S extends Context> {
 
     /**
      * Identifies if this Contributor instance can create instances for the provided short name.
-     * @param shortName The type name to test for.
-     * @return <code>true</code> if the <code>shortName</code> is supported by this contributor.
+     * @param typeName The type name to test for.
+     * @return <code>true</code> if the <code>typeName</code> is supported by this contributor.
      */
-    boolean contributes(String shortName);
+    boolean contributes(String typeName);
 
     /**
      * @deprecated subsumed into {@link ConfigurationDefinition} and thus {@link Contributor#getConfigDefinition(String)}
      *
      * Gets the concrete type of the configuration required by this service instance.
-     * @param shortName service short name
+     * @param typeName service short name
      *
      * @return class of a concrete type, or null if this contributor does not offer this short name.
      */
     @Deprecated(forRemoval = true, since = "0.3.0")
-    Class<? extends BaseConfig> getConfigType(String shortName);
+    Class<? extends BaseConfig> getConfigType(String typeName);
 
     /**
      * Defines the configuration requirements of this contributor for the given short name.
-     * @param shortName the name of the type.
+     * @param typeName the name of the type.
      * @return the ConfigurationDefinition for the short name
      */
-    ConfigurationDefinition getConfigDefinition(String shortName);
+    ConfigurationDefinition getConfigDefinition(String typeName);
 
     /**
      * Creates an instance of the service.
      *
-     * @param shortName service short name
+     * @param typeName service short name
      * @param context   context containing service configuration which may be null if the service instance does not accept configuration.
      * @return the service instance, or null if this contributor does not offer this short name.
      */
-    T getInstance(String shortName, S context);
+    T getInstance(String typeName, S context);
 
 }
