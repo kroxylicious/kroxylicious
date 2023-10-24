605c5891e89aa7ac6a9efb70e07990a53ce34747
kwall <kwall@apache.org>
2023-10-24 11:48:41 +0100

    bom pom needs to specify the release profile. mvn deploy needs processAllModules=true in order to process the BOM too
    Signed-off-by: kwall <kwall@apache.org>

--- scripts/release.sh
+++ scripts/release.sh
@@ -131,7 +131,7 @@ git tag -f "${RELEASE_TAG}"
 git push "${REPOSITORY}" "${RELEASE_TAG}" ${GIT_DRYRUN:-}
 
 echo "Deploying release to maven central"
-mvn deploy -Prelease -DskipTests=true -DreleaseSigningKey="${GPG_KEY}" ${MVN_DEPLOY_DRYRUN:-}
+mvn deploy -Prelease -DskipTests=true -DreleaseSigningKey="${GPG_KEY}" -DprocessAllModules=true ${MVN_DEPLOY_DRYRUN:-}
 
 PREPARE_DEVELOPMENT_BRANCH="prepare-development-${RELEASE_DATE}"
 git checkout -b ${PREPARE_DEVELOPMENT_BRANCH} ${TEMPORARY_RELEASE_BRANCH}
