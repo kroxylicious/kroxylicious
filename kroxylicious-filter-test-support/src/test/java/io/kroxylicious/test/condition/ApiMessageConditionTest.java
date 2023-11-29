e140bf88e12550142aa71a1d4da9847917224ed1
Sam Barker <sbarker@redhat.com>
2023-11-29 15:59:25 +1300

    Test factory methods
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- kroxylicious-filter-test-support/src/test/java/io/kroxylicious/test/condition/ApiMessageConditionTest.java
+++ kroxylicious-filter-test-support/src/test/java/io/kroxylicious/test/condition/ApiMessageConditionTest.java
@@ -7,9 +7,14 @@
 package io.kroxylicious.test.condition;
 
 import org.apache.kafka.common.message.ProduceRequestData;
+import org.apache.kafka.common.message.ProduceResponseData;
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.protocol.ApiMessage;
 import org.apache.kafka.common.requests.ProduceRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,5 +61,31 @@ class ApiMessageConditionTest {
 
         // Then
         assertThat(matches).isFalse();
+    }
+
+    @ParameterizedTest
+    @EnumSource(ApiKeys.class)
+    void shouldCreateConditionForApiKeyEnum(ApiKeys apiKey) {
+        // Given
+        ApiMessageCondition<ApiMessage> apiMessageCondition = ApiMessageCondition.forApiKey(apiKey);
+
+        // When
+        final boolean matches = apiMessageCondition.matches(produceRequestData);
+
+        // Then
+        assertThat(matches).describedAs("expected %s (%s) to return %s for ProduceRequestData", apiKey,  ApiKeys.PRODUCE == apiKey).isEqualTo(ApiKeys.PRODUCE == apiKey);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ApiKeys.class)
+    void shouldCreateConditionForApiKeyShort(ApiKeys apiKey) {
+        // Given
+        ApiMessageCondition<ApiMessage> apiMessageCondition = ApiMessageCondition.forApiKey(apiKey.id);
+
+        // When
+        final boolean matches = apiMessageCondition.matches(produceRequestData);
+
+        // Then
+        assertThat(matches).describedAs("expected %s (%s) to return %s for ProduceRequestData", apiKey, apiKey.id,  ApiKeys.PRODUCE == apiKey).isEqualTo(ApiKeys.PRODUCE == apiKey);
     }
 }
