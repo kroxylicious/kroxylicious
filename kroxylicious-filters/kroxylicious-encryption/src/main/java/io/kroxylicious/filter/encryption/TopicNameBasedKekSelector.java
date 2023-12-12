01cc2214517927fa505a07c37f4ed8a2e590f707
Sam Barker <sam@quadrocket.co.uk>
2023-12-13 12:22:06 +1300

    PR Feedback: Update TopicNameBasedKekSelector javadoc
    Co-authored-by: Keith Wall <kwall@redhat.com>
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-filters/kroxylicious-encryption/src/main/java/io/kroxylicious/filter/encryption/TopicNameBasedKekSelector.java
+++ kroxylicious-filters/kroxylicious-encryption/src/main/java/io/kroxylicious/filter/encryption/TopicNameBasedKekSelector.java
@@ -21,7 +21,8 @@ public abstract class TopicNameBasedKekSelector {
 
     /**
      * Returns a completion stage whose value, on successful completion, is a map from each of the given topic
-     * names to the KEK id to use for encrypting records in that topic.
+     * names to the KEK id to use for encrypting records in that topic. If the topic is not to be encrypted,
+     * the topic mapping will have a null value.
      * @param topicNames A set of topic names
      * @return A completion stage for the map form topic name to KEK id.
      */
