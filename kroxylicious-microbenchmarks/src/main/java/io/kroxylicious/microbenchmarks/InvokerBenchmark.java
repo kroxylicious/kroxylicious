2f598d19b2cac40002630fcd07877bd55e8cbcec
kwall <kwall@apache.org>
2024-02-27 08:32:06 +0000

    addressing review feedback from Sam Barker <sam@quadrocket.co.uk>
    Signed-off-by: kwall <kwall@apache.org>

--- kroxylicious-microbenchmarks/src/main/java/io/kroxylicious/microbenchmarks/InvokerBenchmark.java
+++ kroxylicious-microbenchmarks/src/main/java/io/kroxylicious/microbenchmarks/InvokerBenchmark.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.kroxylicious.benchmarks;
+package io.kroxylicious.microbenchmarks;
 
 import org.apache.kafka.common.protocol.ApiKeys;
 import org.openjdk.jmh.annotations.Benchmark;
