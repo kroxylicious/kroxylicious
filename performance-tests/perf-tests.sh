29d058f6b7d3f637506e018f93c28633e2781e38
Francisco Vila <fvila@redhat.com>
2024-03-13 09:48:32 +0100

    move KAFKA_IMAGE together with the other variables

--- performance-tests/perf-tests.sh
+++ performance-tests/perf-tests.sh
@@ -25,8 +25,9 @@ KROXYLICIOUS_CHECKOUT=${KROXYLICIOUS_CHECKOUT:-${PERF_TESTS_DIR}/..}
 KAFKA_VERSION=${KAFKA_VERSION:-$(mvn -f ${KROXYLICIOUS_CHECKOUT}/pom.xml org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=kafka.version -q -DforceStdout -pl kroxylicious-systemtests)}
 STRIMZI_VERSION=${STRIMZI_VERSION:-$(mvn -f ${KROXYLICIOUS_CHECKOUT}/pom.xml org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=strimzi.version -q -DforceStdout)}
 KAFKA_TOOL_IMAGE=${KAFKA_TOOL_IMAGE:-quay.io/strimzi/kafka:${STRIMZI_VERSION}-kafka-${KAFKA_VERSION}}
+KAFKA_IMAGE=${KAFKA_IMAGE:-"quay.io/ogunalp/kafka-native:latest-kafka-${KAFKA_VERSION}"}
 PERF_NETWORK=performance-tests_perf_network
-export KAFKA_VERSION KAFKA_TOOL_IMAGE
+export KAFKA_VERSION KAFKA_TOOL_IMAGE KAFKA_IMAGE
 
 runDockerCompose () {
   docker-compose -f ${PERF_TESTS_DIR}/docker-compose.yaml "${@}"
@@ -160,7 +161,6 @@ ON_SHUTDOWN+=("rm -rf ${TMP}")
 
 # Bring up Kafka
 ON_SHUTDOWN+=("runDockerCompose down")
-export KAFKA_IMAGE=${KAFKA_IMAGE:-"quay.io/ogunalp/kafka-native:latest-kafka-${KAFKA_VERSION}"}
 runDockerCompose pull
 runDockerCompose up --detach --wait kafka
 
