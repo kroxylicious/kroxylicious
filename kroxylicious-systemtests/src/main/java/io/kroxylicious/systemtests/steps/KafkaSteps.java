07db5edd0ee826ae2798bf8852bd83c040e35dc3
Francisco Vila <57452611+franvila@users.noreply.github.com>
2024-03-13 07:13:18 +0100

    Fix topic creation for system tests (#1075)
    * increate timeout to create a topic and add another check for deleting the topic
    
    * renamed the method
    
    * formatted
    
    * setting timeout inside the waitForPodRunSucceded method
    
    * fix imports
    
    * added wait for pod to leave pending state
    
    * added wait for pod to leave pending state
    
    * bail out early if the pod fails (rather than waiting until the timeout expires)
    
    Signed-off-by: kwall <kwall@apache.org>
    
    * remove code smell
    
    * fix
    
    ---------
    
    Signed-off-by: kwall <kwall@apache.org>
    Co-authored-by: kwall <kwall@apache.org>

--- kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/steps/KafkaSteps.java
+++ kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/steps/KafkaSteps.java
@@ -23,6 +23,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class KafkaSteps {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaSteps.class);
+    private static final String ADMIN_CLIENT_COMMAND = "admin-client";
+    private static final String TOPIC_COMMAND = "topic";
+    private static final String BOOTSTRAP_ARG = "--bootstrap-server=";
+    private static final String NEVER_POLICY = "Never";
 
     private KafkaSteps() {
     }
@@ -41,14 +45,15 @@ public class KafkaSteps {
         kubeClient().getClient().run().inNamespace(deployNamespace).withNewRunConfig()
                 .withImage(Constants.TEST_CLIENTS_IMAGE)
                 .withName(podName)
-                .withRestartPolicy("Never")
-                .withCommand("admin-client")
-                .withArgs("topic", "create", "--bootstrap-server=" + bootstrap, "--topic=" + topicName, "--topic-partitions=" + partitions,
+                .withRestartPolicy(NEVER_POLICY)
+                .withCommand(ADMIN_CLIENT_COMMAND)
+                .withArgs(TOPIC_COMMAND, "create", BOOTSTRAP_ARG + bootstrap, "--topic=" + topicName, "--topic-partitions=" + partitions,
                         "--topic-rep-factor=" + replicas)
                 .done();
 
         DeploymentUtils.waitForPodRunSucceeded(deployNamespace, podName, Duration.ofSeconds(30));
-        LOGGER.debug("Admin client pod log: {}", kubeClient().logsInSpecificNamespace(deployNamespace, podName));
+        String log = kubeClient().logsInSpecificNamespace(deployNamespace, podName);
+        LOGGER.debug("Admin client create pod log: {}", log);
     }
 
     /**
@@ -59,18 +64,40 @@ public class KafkaSteps {
      * @param bootstrap the bootstrap
      */
     public static void deleteTopic(String deployNamespace, String topicName, String bootstrap) {
+        if (!topicExists(deployNamespace, topicName, bootstrap)) {
+            LOGGER.warn("Nothing to delete. Topic was not created");
+            return;
+        }
         LOGGER.info("Deleting topic {}", topicName);
         String podName = Constants.KAFKA_ADMIN_CLIENT_LABEL + "-delete";
         kubeClient().getClient().run().inNamespace(deployNamespace).withNewRunConfig()
                 .withImage(Constants.TEST_CLIENTS_IMAGE)
                 .withName(podName)
-                .withRestartPolicy("Never")
-                .withCommand("admin-client")
-                .withArgs("topic", "delete", "--bootstrap-server=" + bootstrap, "--topic=" + topicName)
+                .withRestartPolicy(NEVER_POLICY)
+                .withCommand(ADMIN_CLIENT_COMMAND)
+                .withArgs(TOPIC_COMMAND, "delete", BOOTSTRAP_ARG + bootstrap, "--topic=" + topicName)
                 .done();
 
         DeploymentUtils.waitForPodRunSucceeded(deployNamespace, podName, Duration.ofSeconds(30));
-        LOGGER.debug("Admin client pod log: {}", kubeClient().logsInSpecificNamespace(deployNamespace, podName));
+        String log = kubeClient().logsInSpecificNamespace(deployNamespace, podName);
+        LOGGER.debug("Admin client delete pod log: {}", log);
+    }
+
+    private static boolean topicExists(String deployNamespace, String topicName, String bootstrap) {
+        LOGGER.info("Deleting topic {}", topicName);
+        String podName = Constants.KAFKA_ADMIN_CLIENT_LABEL + "-list";
+        kubeClient().getClient().run().inNamespace(deployNamespace).withNewRunConfig()
+                .withImage(Constants.TEST_CLIENTS_IMAGE)
+                .withName(podName)
+                .withRestartPolicy(NEVER_POLICY)
+                .withCommand(ADMIN_CLIENT_COMMAND)
+                .withArgs(TOPIC_COMMAND, "list", BOOTSTRAP_ARG + bootstrap)
+                .done();
+
+        DeploymentUtils.waitForPodRunSucceeded(deployNamespace, podName, Duration.ofSeconds(30));
+        String log = kubeClient().logsInSpecificNamespace(deployNamespace, podName);
+        LOGGER.debug("Admin client list pod log: {}", log);
+        return log.contains(topicName);
     }
 
     /**
