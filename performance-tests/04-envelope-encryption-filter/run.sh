ed60d52d58e32e9cab26feaddfe3904436428c62
Luca Molteni <volothamp@gmail.com>
2024-04-11 15:40:21 +0200

    Write output files for all tests - Disabled healthchecks (to be removed) - Run async profiler after warmup in producer test - seccomp:unconfined to use async profiler - Create output directories, copy output flamegraph to local directory

--- performance-tests/04-envelope-encryption-filter/run.sh
+++ performance-tests/04-envelope-encryption-filter/run.sh
@@ -4,6 +4,10 @@
 #
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. "${SCRIPT_DIR}/../common-perf.sh"
 
 set -euo pipefail
 
