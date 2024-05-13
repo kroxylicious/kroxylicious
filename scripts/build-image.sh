f9b875447e09b12a22a248a84b9e42f5f45b88af
Sam Barker <sbarker@redhat.com>
2024-05-13 18:56:55 +1200

    configure builds to expire.
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -10,6 +10,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . "${SCRIPT_DIR}/common.sh"
 cd "${SCRIPT_DIR}/.."
 KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION:=$(./mvnw org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=project.version -q -DforceStdout)}
+IMAGE_EXPIRY=${IMAGE_EXPIRY:-'8h'}
 
 if [[ -z ${REGISTRY_DESTINATION:-} ]]; then
   echo "Please set REGISTRY_DESTINATION to a value like 'quay.io/<myorg>/kroxylicious', exiting"
@@ -17,10 +18,25 @@ if [[ -z ${REGISTRY_DESTINATION:-} ]]; then
 fi
 
 IMAGE="${REGISTRY_DESTINATION}:${KROXYLICIOUS_VERSION}"
-${CONTAINER_ENGINE} build -t "${IMAGE}" --build-arg "KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION}" \
+declare -a LABELS=()
+#TODO expand this for additional optional labels
+if [ -n "${TEMP_BUILD:-}" ]; then
+  LABELS+=("quay.expires-after=${IMAGE_EXPIRY}")
+fi
+
+LABEL_ARGS=
+if [ ${#LABELS[@]} -gt 0 ]; then
+  for lbl in "${LABELS[@]}"; do
+    LABEL_ARGS+="--label ${lbl}"
+  done
+fi
+
+
+${CONTAINER_ENGINE} build -t "${IMAGE}" ${LABEL_ARGS} --build-arg "KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION}" \
                                         --build-arg "CURRENT_USER=${USER}" \
                                         --build-arg "CURRENT_USER_UID=$(id -u)" \
                                         .
+
 if [[ -n ${PUSH_IMAGE:-} ]]; then
   REGISTRY_SERVER=${REGISTRY_SERVER:-$(extractRegistryServer "${REGISTRY_DESTINATION}")}
   echo "Pushing image to ${REGISTRY_SERVER}"
