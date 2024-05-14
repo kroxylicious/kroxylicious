c12c6084904046a5923df45ef7a1e3820284ab7a
Sam Barker <sbarker@redhat.com>
2024-05-14 12:34:51 +1200

    add getopts to deal with labels and tags
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -9,8 +9,53 @@ set -eo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . "${SCRIPT_DIR}/common.sh"
 cd "${SCRIPT_DIR}/.."
-KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION:=$(./mvnw org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=project.version -q -DforceStdout)}
+KROXYLICIOUS_VERSION=$(./mvnw org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=project.version -q -DforceStdout)
+LABELS=()
+IMAGE_TAGS=()
 IMAGE_EXPIRY=${IMAGE_EXPIRY:-'8h'}
+
+YELLOW='\033[1;33m'
+NOCOLOR='\033[0m'
+
+function array_to_arg_line() {
+  local ARG_NAME=$1
+  shift
+  local ARR=("$@")
+  local arg_line=
+  if [ ${#ARR[@]} -gt 0 ]; then
+    for value in "${ARR[@]}"; do
+      arg_line+="--${ARG_NAME} ${value}"
+    done
+  else
+    echo -e >&2 "${YELLOW}No ${ARG_NAME} to add${NOCOLOR}"
+  fi
+  echo "${arg_line}"
+}
+
+while getopts ":l:t:bh" opt; do
+  case $opt in
+    l) LABELS+=("${OPTARG}")
+    ;;
+    t) IMAGE_TAGS+=("${OPTARG}")
+    ;;
+    b) TEMP_BUILD=true
+    ;;
+    h)
+      1>&2 cat << EOF
+usage: $0 [-l <label>] [-t tag>] [-h]
+ -l a label to add to the image
+ -t a tag to add to the image
+ -h this help message
+EOF
+      exit 1
+    ;;
+    \?) echo "Invalid option -$opt ${OPTARG}" >&2
+    exit 1
+    ;;
+  esac
+
+done
+
 
 if [[ -z ${REGISTRY_DESTINATION:-} ]]; then
   echo "Please set REGISTRY_DESTINATION to a value like 'quay.io/<myorg>/kroxylicious', exiting"
@@ -18,21 +63,17 @@ if [[ -z ${REGISTRY_DESTINATION:-} ]]; then
 fi
 
 IMAGE="${REGISTRY_DESTINATION}:${KROXYLICIOUS_VERSION}"
-declare -a LABELS=()
-#TODO expand this for additional optional labels
+
 if [ -n "${TEMP_BUILD:-}" ]; then
   LABELS+=("quay.expires-after=${IMAGE_EXPIRY}")
 fi
 
-LABEL_ARGS=
-if [ ${#LABELS[@]} -gt 0 ]; then
-  for lbl in "${LABELS[@]}"; do
-    LABEL_ARGS+="--label ${lbl}"
-  done
-fi
+LABEL_ARGS=$(array_to_arg_line "label" "${LABELS[@]}")
+TAG_ARGS=$(array_to_arg_line "tag" "${IMAGE_TAGS[@]}")
 
 
-${CONTAINER_ENGINE} build -t "${IMAGE}" ${LABEL_ARGS} --build-arg "KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION}" \
+${CONTAINER_ENGINE} build -t "${IMAGE}" ${LABEL_ARGS} ${TAG_ARGS} \
+                                        --build-arg "KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION}" \
                                         --build-arg "CURRENT_USER=${USER}" \
                                         --build-arg "CURRENT_USER_UID=$(id -u)" \
                                         .
