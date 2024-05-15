b1078a653dba8bed886a12eb1a39ebc3c089bf42
Sam Barker <sbarker@redhat.com>
2024-05-15 17:10:38 +1200

    Merge results into a single json document per test
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- performance-tests/perf-test-commits.sh
+++ performance-tests/perf-test-commits.sh
@@ -1,19 +1,35 @@
 #! /usr/bin/env bash
-
 #
 # Copyright Kroxylicious Authors.
 #
 # Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
 #
+
 PERF_TESTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "${PERF_TESTS_DIR}"/../scripts/common.sh
+
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+FIND_COMMAND="find"
+if [ "$OS" = 'Darwin' ] && resolveCommand gfind ; then
+  FIND_COMMAND=gfind
+  ENABLE_REGEX="-regextype posix-extended"
+elif [ "$OS" = 'Darwin' ] ; then
+  # for MacOS
+  ENABLE_REGEX="-E"
+else
+  # for Linux and Windows
+  ENABLE_REGEX="-regextype posix-extended"
+fi
+
 COMMITS=( "$@" )
 
 #Cross platform temp directory creation based on https://unix.stackexchange.com/a/84980
 RESULTS_DIR=${RESULTS_DIR:=$(mktemp -d -t kroxyliciousPerfTestResults.XXXXXX 2>/dev/null || mktemp -d -t 'kroxyliciousPerfTestResults')}
+JSON_TEMP_DIR=${JSON_TEMP_DIR:=$(mktemp -d -t kroxyliciousPerfTestJson.XXXXXX 2>/dev/null || mktemp -d -t 'kroxyliciousPerfTestJson')}
 echo -e "Writing results to: ${GREEN}${RESULTS_DIR}${NOCOLOR}"
-
-GREEN='\033[0;32m'
-NOCOLOR='\033[0m'
 
 export PUSH_IMAGE=y #remove this once pulling is optional to save some time.
 export TEMP_BUILD=y
@@ -39,6 +55,33 @@ runPerfTest() {
   "${PERF_TESTS_DIR}/perf-tests.sh"
 }
 
+mergeResults() {
+  echo -e "Merging results in:  ${GREEN}${RESULTS_DIR}${NOCOLOR}"
+  mapfile -t TEST_NAMES < <(${FIND_COMMAND} "${RESULTS_DIR}" -type d ${ENABLE_REGEX} -regex  ".*\/[0-9]{2}-.*" -exec basename {} \; | sort | uniq)
+
+  for TEST_NAME in "${TEST_NAMES[@]}"; do
+   echo "generating ${TEST_NAME}"
+   JQ_COMMAND=".[0]"
+   idx=0
+   for COMMIT in "${COMMITS[@]}"; do
+     if [ ${idx} -ne 0 ]; then
+       JQ_COMMAND+=" * .[$((idx++))]"
+     else
+       ((idx++))
+     fi
+     jq -cn \
+      --arg commit "${COMMIT}" \
+      --arg key "${TEST_NAME}" \
+      --slurpfile producer_results "${RESULTS_DIR}/${COMMIT}/${TEST_NAME}/producer.json" \
+      --slurpfile consumer_results "${RESULTS_DIR}/${COMMIT}/${TEST_NAME}/consumer.json" \
+      '{producer: {($key): {($commit): $producer_results}}, consumer: {($key): {($commit): $consumer_results}}}' \
+     > "${JSON_TEMP_DIR}/${TEST_NAME}-${COMMIT}.json"
+   done
+
+   jq  -c -s "${JQ_COMMAND}" "${JSON_TEMP_DIR}/${TEST_NAME}"-*.json > "${RESULTS_DIR}/${TEST_NAME}-all.json"
+  done
+}
+
 for COMMIT in "${COMMITS[@]}"; do
     checkoutCommit "${COMMIT}"
 
@@ -48,3 +91,5 @@ for COMMIT in "${COMMITS[@]}"; do
 
     runPerfTest "${SHORT_COMMIT}"
 done
+
+mergeResults
