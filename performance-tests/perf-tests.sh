ad750417ce135eae38f05a7df69b2e734f478287
Sam Barker <sam@quadrocket.co.uk>
2024-05-15 13:15:29 +1200

    Simplify and cleanup
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED

--- performance-tests/perf-tests.sh
+++ performance-tests/perf-tests.sh
@@ -96,7 +96,6 @@ setupAsyncProfilerKroxy() {
 
   mkdir -p "${LOADER_DIR}"
   unzip -o -q /tmp/asprof/ap-loader-all.jar -d "${LOADER_DIR}"
-  chmod -R +rw "${LOADER_DIR}"
 }
 
 deleteAsyncProfilerKroxy() {
@@ -117,7 +116,7 @@ startAsyncProfilerKroxy() {
 
   echo "TARGETARCH: ${TARGETARCH}"
 
-  ${CONTAINER_ENGINE} exec -it ${KROXYLICIOUS_CONTAINER_ID} mkdir -p "${LOADER_DIR}/"{bin,libs,lib} && chmod +r -R "${LOADER_DIR}"
+  ${CONTAINER_ENGINE} exec -it ${KROXYLICIOUS_CONTAINER_ID} mkdir -p "${LOADER_DIR}/lib" && chmod +r -R "${LOADER_DIR}"
   ${CONTAINER_ENGINE} cp "${LOADER_DIR}/libs/libasyncProfiler-3.0-${TARGETARCH}.so" "${KROXYLICIOUS_CONTAINER_ID}:${LOADER_DIR}/lib/libasyncProfiler.so"
 
   java -Dap_loader_extraction_dir=${LOADER_DIR} -jar /tmp/asprof/ap-loader-all.jar profiler start "${KROXYLICIOUS_PID}"
