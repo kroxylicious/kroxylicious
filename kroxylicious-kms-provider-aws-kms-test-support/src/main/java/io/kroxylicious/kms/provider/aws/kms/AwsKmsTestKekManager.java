839beaaa921276bb84a767e26e946b09f8173d53
Francisco Vila <fvila@redhat.com>
2024-09-17 12:52:58 +0200

    Refactored Vault and AWS test kek Managers
    Signed-off-by: Francisco Vila <fvila@redhat.com>

---
+++ kroxylicious-kms-provider-aws-kms-test-support/src/main/java/io/kroxylicious/kms/provider/aws/kms/AwsKmsTestKekManager.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kms.provider.aws.kms;
+
+public class AwsTestKekManager {
+}
---
+++ kroxylicious-kms-test-support/src/main/java/io/kroxylicious/kms/service/AbstractTestKekManager.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kms.service;
+
+public class AbstractTestKekManager {
+}
