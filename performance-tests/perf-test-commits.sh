debb5cbdecdd0bc33c09174c03b69f7d10f6332e
Sam Barker <sbarker@redhat.com>
2024-09-20 10:27:12 +1200

    Make it clearer we support multiple refs not just commits
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- performance-tests/perf-test-commits.sh
+++ performance-tests/perf-test-commits.sh
@@ -33,7 +33,7 @@ else
   ENABLE_REGEX="-regextype posix-extended"
 fi
 
-COMMITS=( "$@" )
+GIT_REFS=( "$@" )
 SHORT_COMMITS=( )
 
 #Cross platform temp directory creation based on https://unix.stackexchange.com/a/84980
@@ -84,19 +84,19 @@ mergeResults() {
    echo -e "generating ${GREEN}${TEST_NAME}${NOCOLOR}"
    JQ_COMMAND=".[0]"
    idx=0
-   for COMMIT in "${SHORT_COMMITS[@]}"; do
+   for REF in "${SHORT_COMMITS[@]}"; do
      if [ ${idx} -ne 0 ]; then
        JQ_COMMAND+=" * .[$((idx++))]"
      else
        ((idx++))
      fi
      jq -cn \
-      --arg commit "${COMMIT}" \
+      --arg commit "${REF}" \
       --arg key "${TEST_NAME}" \
-      --slurpfile producer_results "${RESULTS_DIR}/${COMMIT}/${TEST_NAME}/producer.json" \
-      --slurpfile consumer_results "${RESULTS_DIR}/${COMMIT}/${TEST_NAME}/consumer.json" \
+      --slurpfile producer_results "${RESULTS_DIR}/${REF}/${TEST_NAME}/producer.json" \
+      --slurpfile consumer_results "${RESULTS_DIR}/${REF}/${TEST_NAME}/consumer.json" \
       '{producer: {($key): {($commit): $producer_results}}, consumer: {($key): {($commit): $consumer_results}}}' \
-     > "${JSON_TEMP_DIR}/${TEST_NAME}-${COMMIT}.json"
+     > "${JSON_TEMP_DIR}/${TEST_NAME}-${REF}.json"
    done
 
    jq  -c -s "${JQ_COMMAND}" "${JSON_TEMP_DIR}/${TEST_NAME}"-*.json > "${RESULTS_DIR}/${TEST_NAME}-all.json"
@@ -105,8 +105,8 @@ mergeResults() {
 
 cloneRepo
 
-for COMMIT in "${COMMITS[@]}"; do
-    checkoutCommit "${COMMIT}"
+for REF in "${GIT_REFS[@]}"; do
+    checkoutCommit "${REF}"
 
     SHORT_COMMIT=$(git rev-parse --short HEAD)
     SHORT_COMMITS+=("${SHORT_COMMIT}")
