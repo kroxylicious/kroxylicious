8efe6160676f91265552e9c211e13ec853a34e71
Sam Barker <sbarker@redhat.com>
2024-11-01 12:05:01 +1300

    Sonar java:S5803: Don't call VisibleForTesting from prod code.
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/ProxyChannelStateMachine.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/ProxyChannelStateMachine.java
@@ -120,7 +120,6 @@ public class ProxyChannelStateMachine {
     @Nullable
     private KafkaProxyBackendHandler backendHandler;
 
-    @VisibleForTesting
     ProxyChannelState state() {
         return state;
     }
