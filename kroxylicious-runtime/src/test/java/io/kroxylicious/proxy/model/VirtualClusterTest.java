a39cbf15e61613951c2daa6fa915af3d20b64b76
Sam Barker <sbarker@redhat.com>
2024-11-29 11:04:50 +1300

    Flesh out integration test
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/model/VirtualClusterTest.java
+++ kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/model/VirtualClusterTest.java
@@ -7,18 +7,29 @@
 package io.kroxylicious.proxy.model;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import javax.net.ssl.SSLEngine;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import io.netty.buffer.ByteBufAllocator;
 
 import io.kroxylicious.proxy.config.TargetCluster;
 import io.kroxylicious.proxy.config.tls.InsecureTls;
 import io.kroxylicious.proxy.config.tls.KeyPair;
 import io.kroxylicious.proxy.config.tls.Tls;
+import io.kroxylicious.proxy.config.tls.TlsClientAuth;
 import io.kroxylicious.proxy.config.tls.TlsTestConstants;
 import io.kroxylicious.proxy.internal.clusternetworkaddressconfigprovider.PortPerBrokerClusterNetworkAddressConfigProvider;
 
 import static io.kroxylicious.proxy.service.HostPort.parse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 
 class VirtualClusterTest {
 
@@ -43,6 +54,63 @@ class VirtualClusterTest {
 
         // Then
         assertThat(virtualCluster).isNotNull().extracting("downstreamSslContext").isNotNull();
-        assertThat(virtualCluster).isNotNull().extracting("upstreamSslContext").isNotNull();
+        assertThat(virtualCluster)
+                .isNotNull()
+                .satisfies(vc -> assertThat(vc.getUpstreamSslContext()).isPresent());
+    }
+
+    @ParameterizedTest
+    @MethodSource("io.kroxylicious.proxy.model.VirtualClusterTest#clientAuthSettings")
+    void shouldRequireDownstreamClientAuth(TlsClientAuth clientAuth, Consumer<SSLEngine> sslEngineAssertions) {
+        // Given
+        final KeyPair keyPair = new KeyPair(TlsTestConstants.getResourceLocationOnFilesystem("server.key"),
+                TlsTestConstants.getResourceLocationOnFilesystem("server.crt"),
+                null);
+        final Optional<Tls> tls = Optional.of(new Tls(keyPair, new InsecureTls(false), clientAuth));
+        final PortPerBrokerClusterNetworkAddressConfigProvider.PortPerBrokerClusterNetworkAddressConfigProviderConfig clusterNetworkAddressConfigProviderConfig = new PortPerBrokerClusterNetworkAddressConfigProvider.PortPerBrokerClusterNetworkAddressConfigProviderConfig(
+                parse("localhost:1235"),
+                "localhost", 19092, 0, 1);
+        final PortPerBrokerClusterNetworkAddressConfigProvider clusterNetworkAddressConfigProvider = new PortPerBrokerClusterNetworkAddressConfigProvider(
+                clusterNetworkAddressConfigProviderConfig);
+
+        // When
+        final VirtualCluster virtualCluster = new VirtualCluster("wibble", new TargetCluster("bootstrap:9092", tls), clusterNetworkAddressConfigProvider, tls, false,
+                false);
+
+        // Then
+        assertThat(virtualCluster)
+                .isNotNull()
+                .satisfies(vc -> assertThat(vc.getDownstreamSslContext())
+                        .isPresent()
+                        .hasValueSatisfying(
+                                sslContext -> {
+                                    assertThat(sslContext.isClient()).isFalse();
+                                    assertThat(sslContext.isServer()).isTrue();
+                                    assertThat(sslContext.newEngine(ByteBufAllocator.DEFAULT)).satisfies(sslEngineAssertions);
+                                }));
+    }
+
+    public static Stream<Arguments> clientAuthSettings() {
+        return Stream.of(
+                argumentSet("don't expect client side auth",
+                        TlsClientAuth.NONE,
+                        (Consumer<SSLEngine>) (SSLEngine sslEngine) -> {
+                            assertThat(sslEngine.getWantClientAuth()).isFalse();
+                            assertThat(sslEngine.getNeedClientAuth()).isFalse();
+                        }
+                ),
+                argumentSet("want client side auth",
+                        TlsClientAuth.REQUESTED,
+                        (Consumer<SSLEngine>) (SSLEngine sslEngine) -> {
+                            assertThat(sslEngine.getWantClientAuth()).isTrue();
+                            assertThat(sslEngine.getNeedClientAuth()).isFalse();
+                        }),
+                argumentSet("need client side auth",
+                        TlsClientAuth.REQUIRED,
+                        (Consumer<SSLEngine>) (SSLEngine sslEngine) -> {
+                            assertThat(sslEngine.getWantClientAuth()).isFalse();
+                            assertThat(sslEngine.getNeedClientAuth()).isTrue();
+                        })
+        );
     }
 }
