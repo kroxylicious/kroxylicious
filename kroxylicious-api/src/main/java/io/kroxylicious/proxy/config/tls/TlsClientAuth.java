8fb9b05230972c8e66ec249a10c9fd8a381e0ddd
Alan Robinson <alan.robinson3@engineering.digital.dwp.gov.uk>
2024-12-03 11:35:20 +0000

    remove unused variables as we now directly map in the virtual cluster
    Signed-off-by: Alan Robinson <alan.robinson3@engineering.digital.dwp.gov.uk>

--- kroxylicious-api/src/main/java/io/kroxylicious/proxy/config/tls/TlsClientAuth.java
+++ kroxylicious-api/src/main/java/io/kroxylicious/proxy/config/tls/TlsClientAuth.java
@@ -8,15 +8,8 @@ package io.kroxylicious.proxy.config.tls;
 
 public enum TlsClientAuth {
 
-    REQUIRED("required", "REQUIRE"),
-    REQUESTED("requested", "OPTIONAL"),
-    NONE("none", "NONE");
-
-    private final String clientAuth;
-    private final String nettyClientAuth;
-
-    TlsClientAuth(String clientAuth, String nettyClientAuth) {
-        this.clientAuth = clientAuth;
-        this.nettyClientAuth = nettyClientAuth;
-    }
+    REQUIRED,
+    REQUESTED,
+    NONE;
+    
 }
