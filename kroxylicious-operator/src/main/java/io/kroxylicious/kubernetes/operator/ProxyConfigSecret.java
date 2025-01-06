8baa517eab6ed1e3ca71baf4087d87a60812599b
Tom Bentley <tbentley@redhat.com>
2025-01-06 15:00:51 +1300

    fix
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-operator/src/main/java/io/kroxylicious/kubernetes/operator/ProxyConfigSecret.java
+++ kroxylicious-operator/src/main/java/io/kroxylicious/kubernetes/operator/ProxyConfigSecret.java
@@ -14,7 +14,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
