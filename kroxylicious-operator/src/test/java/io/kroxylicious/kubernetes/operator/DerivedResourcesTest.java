baaf2cddc2fcdfb9817fc785b85cd6e815571239
Tom Bentley <tombentley@users.noreply.github.com>
2025-01-08 05:43:27 +0000

    Generic filter crd (#1700)
    * Add a generic Filter CRD
    * Change the validation example to use generic Filter; kill off filter-specific CRDs
    
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/DerivedResourcesTest.java
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/DerivedResourcesTest.java
@@ -48,7 +48,6 @@ import io.kroxylicious.kubernetes.operator.config.RuntimeDecl;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
@@ -205,7 +204,6 @@ class DerivedResourcesTest {
             assertMinimalMetadata(kafkaProxy.getMetadata(), inFileName);
 
             unusedFiles.remove(input);
-            unusedFiles.remove(testDir.resolve("operator-config.yaml"));
             unusedFiles.removeAll(childFilesMatching(testDir, "in-*"));
 
             Context<KafkaProxy> context;
@@ -312,8 +310,7 @@ class DerivedResourcesTest {
 
         doReturn(resourceContext).when(context).managedDependentResourceContext();
 
-        var configFile = testDir.resolve("operator-config.yaml");
-        var runtimeDecl = Files.exists(configFile) ? configFromFile(configFile) : new RuntimeDecl(List.of());
+        var runtimeDecl = OperatorMain.runtimeDecl();
         Set<GenericKubernetesResource> filterInstances = new HashSet<>();
         for (var filterApi : runtimeDecl.filterApis()) {
             String fileName = "in-" + filterApi.kind() + "-*.yaml";
