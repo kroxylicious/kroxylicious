b8e863eb7487c29c33a38e55a9644f10fd12582a
kroxylicious-robot <153509303+kroxylicious-robot@users.noreply.github.com>
2025-01-16 12:15:58 +0000

    chore(deps): update ghcr.io/navikt/mock-oauth2-server docker tag to v2.1.10
    Signed-off-by: kroxylicious-robot <153509303+kroxylicious-robot@users.noreply.github.com>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/filter/oauthbearer/OauthBearerValidationIT.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/filter/oauthbearer/OauthBearerValidationIT.java
@@ -66,7 +66,7 @@ import static org.assertj.core.api.InstanceOfAssertFactories.DOUBLE;
 @EnabledIf(value = "isDockerAvailable", disabledReason = "docker unavailable")
 class OauthBearerValidationIT {
 
-    private static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("ghcr.io/navikt/mock-oauth2-server:2.1.8");
+    private static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("ghcr.io/navikt/mock-oauth2-server:2.1.10");
 
     private static final int OAUTH_SERVER_PORT = 28089;
     private static final String JWKS_ENDPOINT_URL = "http://localhost:" + OAUTH_SERVER_PORT + "/default/jwks";
