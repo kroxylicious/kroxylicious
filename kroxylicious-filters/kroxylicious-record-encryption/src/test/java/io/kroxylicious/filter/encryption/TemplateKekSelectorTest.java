0237c8f06fb13d9a85ebdfe9fe3d37995d30ce6b
Keith Wall <kwall@apache.org>
2025-01-22 15:44:56 +0000

    feat: New KekSelector that is capable of deriving a KEK from a topic name prefix
    Signed-off-by: Keith Wall <kwall@apache.org>

--- kroxylicious-filters/kroxylicious-record-encryption/src/test/java/io/kroxylicious/filter/encryption/TemplateKekSelectorTest.java
+++ kroxylicious-filters/kroxylicious-record-encryption/src/test/java/io/kroxylicious/filter/encryption/TemplateKekSelectorTest.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.kroxylicious.filter.encryption.encrypt;
+package io.kroxylicious.filter.encryption;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -13,12 +13,11 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import io.kroxylicious.filter.encryption.TemplateKekSelector;
-import io.kroxylicious.filter.encryption.config.TemplateConfig;
 import io.kroxylicious.filter.encryption.config.TopicNameBasedKekSelector;
 import io.kroxylicious.kms.provider.kroxylicious.inmemory.InMemoryKms;
 import io.kroxylicious.kms.provider.kroxylicious.inmemory.UnitTestingKmsService;
@@ -37,10 +36,13 @@ import static org.mockito.Mockito.when;
 class TemplateKekSelectorTest {
 
     private UnitTestingKmsService kmsService;
+    private InMemoryKms kms;
 
     @BeforeEach
     void beforeEach() {
         kmsService = UnitTestingKmsService.newInstance();
+        kmsService.initialize(new UnitTestingKmsService.Config());
+        kms = kmsService.buildKms();
     }
 
     @AfterEach
@@ -50,62 +52,73 @@ class TemplateKekSelectorTest {
 
     @Test
     void shouldRejectUnknownPlaceholders() {
-        assertThatThrownBy(() -> getSelector(null, "foo-${topicId}-bar"))
+        assertThatThrownBy(() -> getSelector(kms, "foo-${topicId}-bar"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Unknown template parameter: topicId");
     }
 
     @Test
     void shouldResolveWhenAliasExists() {
-        kmsService.initialize(new UnitTestingKmsService.Config());
-        var kms = kmsService.buildKms();
         var selector = getSelector(kms, "topic-${topicName}");
 
         var kek = kms.generateKey();
         kms.createAlias(kek, "topic-my-topic");
-        var map = selector.selectKek(Set.of("my-topic")).toCompletableFuture().join();
-        assertThat(map)
+        assertThat(selector.selectKek(Set.of("my-topic")))
+                .succeedsWithin(Duration.ofSeconds(1))
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, UUID.class))
+                .hasSize(1)
+                .containsEntry("my-topic", kek);
+    }
+
+    @Test
+    void supportTemplateWithoutTopicPlaceholders() {
+        var selector = getSelector(kms, "fixed");
+
+        var kek = kms.generateKey();
+        kms.createAlias(kek, "fixed");
+        assertThat(selector.selectKek(Set.of("my-topic")))
+                .succeedsWithin(Duration.ofSeconds(1))
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, UUID.class))
                 .hasSize(1)
                 .containsEntry("my-topic", kek);
     }
 
     @Test
     void shouldNotThrowWhenAliasDoesNotExist() {
-        kmsService.initialize(new UnitTestingKmsService.Config());
-        var kms = kmsService.buildKms();
         var selector = getSelector(kms, "topic-${topicName}");
 
-        var map = selector.selectKek(Set.of("my-topic")).toCompletableFuture().join();
-        assertThat(map)
+        assertThat(selector.selectKek(Set.of("my-topic")))
+                .succeedsWithin(Duration.ofSeconds(1))
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, UUID.class))
                 .hasSize(1)
                 .containsEntry("my-topic", null);
     }
 
     @Test
     void shouldNotThrowWhenAliasDoesNotExist_UnknownAliasExceptionWrappedInCompletionException() throws ExecutionException, InterruptedException {
-        var kms = mock(InMemoryKms.class);
+        var mockKms = mock(InMemoryKms.class);
         var result = CompletableFuture.completedFuture(null)
-                .<UUID> thenApply((u) -> {
+                .<UUID> thenApply(u -> {
                     // this exception will be wrapped by a CompletionException
                     throw new UnknownAliasException("mock alias exception");
                 });
-        when(kms.resolveAlias(anyString())).thenReturn(result);
-        var selector = getSelector(kms, "topic-${topicName}");
-        var map = selector.selectKek(Set.of("my-topic")).toCompletableFuture().get();
-        assertThat(map)
+        when(mockKms.resolveAlias(anyString())).thenReturn(result);
+        var selector = getSelector(mockKms, "topic-${topicName}");
+        assertThat(selector.selectKek(Set.of("my-topic")))
+                .succeedsWithin(Duration.ofSeconds(1))
+                .asInstanceOf(InstanceOfAssertFactories.map(String.class, UUID.class))
                 .hasSize(1)
                 .containsEntry("my-topic", null);
     }
 
     @Test
     void serviceExceptionsArePropagated() {
-        var kms = mock(InMemoryKms.class);
+        var mockKms = mock(InMemoryKms.class);
         var result = CompletableFuture.<UUID> failedFuture(new KmsException("bang!"));
-        when(kms.resolveAlias(anyString())).thenReturn(result);
+        when(mockKms.resolveAlias(anyString())).thenReturn(result);
 
-        var selector = getSelector(kms, "topic-${topicName}");
-        var stage = selector.selectKek(Set.of("my-topic"));
-        assertThat(stage)
+        var selector = getSelector(mockKms, "topic-${topicName}");
+        assertThat(selector.selectKek(Set.of("my-topic")))
                 .isCompletedExceptionally()
                 .failsWithin(Duration.ZERO)
                 .withThrowableThat()
@@ -114,7 +127,7 @@ class TemplateKekSelectorTest {
 
     @NonNull
     private <K> TopicNameBasedKekSelector<K> getSelector(Kms<K, ?> kms, String template) {
-        var config = new TemplateConfig(template);
+        var config = new TemplateKekSelector.TemplateConfig(template);
         return new TemplateKekSelector<K>().buildSelector(kms, config);
     }
 
