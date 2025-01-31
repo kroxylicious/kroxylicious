dd0076e02dd98ac52de0f8271fbce261af261503
Robert Young <robeyoun@redhat.com>
2025-01-31 14:28:44 +1300

    Learn topic id to name mapping from CreateTopicResponse
    Why:
    If Filters need to obtain topic names for some topic ids that happened
    to be be created by this filter instance, then we do not have to call
    out and retrieve Metadata to obtain the topic name, we already have it
    from the CreateTopicResponse.
    
    Signed-off-by: Robert Young <robeyoun@redhat.com>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/filter/TopicNameLearningFilter.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/filter/TopicNameLearningFilter.java
@@ -7,29 +7,57 @@
 package io.kroxylicious.proxy.internal.filter;
 
 import java.util.concurrent.CompletionStage;
+import java.util.function.Supplier;
 
 import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.message.CreateTopicsResponseData;
 import org.apache.kafka.common.message.MetadataResponseData;
 import org.apache.kafka.common.message.ResponseHeaderData;
 
+import io.kroxylicious.proxy.filter.CreateTopicsResponseFilter;
 import io.kroxylicious.proxy.filter.FilterContext;
 import io.kroxylicious.proxy.filter.MetadataResponseFilter;
 import io.kroxylicious.proxy.filter.ResponseFilterResult;
 import io.kroxylicious.proxy.model.VirtualClusterModel;
 
-public record TopicNameLearningFilter(VirtualClusterModel model) implements MetadataResponseFilter {
+public record TopicNameLearningFilter(VirtualClusterModel model) implements MetadataResponseFilter, CreateTopicsResponseFilter {
+
+    @Override
+    public boolean shouldHandleMetadataResponse(short apiVersion) {
+        // topic ids won't be set below version 12
+        return apiVersion >= 12;
+    }
+
     @Override
     public CompletionStage<ResponseFilterResult> onMetadataResponse(short apiVersion, ResponseHeaderData header, MetadataResponseData response, FilterContext context) {
         MetadataResponseData.MetadataResponseTopicCollection topics = response.topics();
         if (topics != null) {
-            topics.forEach(topic -> {
-                String topicName = topic.name();
-                Uuid topicId = topic.topicId();
-                if (topicId != null && !topicId.equals(Uuid.ZERO_UUID) && topicName != null && !topicName.isEmpty()) {
-                    model.rememberTopicIdMapping(topicId, topicName);
-                }
-            });
+            topics.forEach(topic -> rememberTopicName(topic::name, topic::topicId));
         }
         return context.forwardResponse(header, response);
     }
+
+    @Override
+    public boolean shouldHandleCreateTopicsResponse(short apiVersion) {
+        // topic ids won't be set below version 7
+        return apiVersion >= 7;
+    }
+
+    @Override
+    public CompletionStage<ResponseFilterResult> onCreateTopicsResponse(short apiVersion, ResponseHeaderData header, CreateTopicsResponseData response,
+                                                                        FilterContext context) {
+        if (response.topics() != null) {
+            response.topics().forEach(topic -> rememberTopicName(topic::name, topic::topicId));
+        }
+        return context.forwardResponse(header, response);
+    }
+
+    private void rememberTopicName(Supplier<String> topicNameSupplier, Supplier<Uuid> topicIdSupplier) {
+        String topicName = topicNameSupplier.get();
+        Uuid topicId = topicIdSupplier.get();
+        if (topicId != null && !topicId.equals(Uuid.ZERO_UUID) && topicName != null && !topicName.isEmpty()) {
+            model.rememberTopicIdMapping(topicId, topicName);
+        }
+    }
+
 }
--- kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/internal/filter/TopicNameLearningFilterTest.java
+++ kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/internal/filter/TopicNameLearningFilterTest.java
@@ -9,11 +9,14 @@ package io.kroxylicious.proxy.internal.filter;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.message.CreateTopicsResponseData;
 import org.apache.kafka.common.message.MetadataResponseData;
 import org.apache.kafka.common.message.ResponseHeaderData;
+import org.apache.kafka.common.protocol.ApiKeys;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -50,58 +53,109 @@ class TopicNameLearningFilterTest {
     @BeforeEach
     void beforeEach() {
         topicNameLearningFilter = new TopicNameLearningFilter(virtualClusterModel);
-        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
+    }
+
+    @Test
+    void onlyInterceptMetadataThatCanCarryTopicIds() {
+        IntStream tooOld = IntStream.range(ApiKeys.METADATA.oldestVersion(), 12);
+        IntStream hasTopicIds = IntStream.range(12, ApiKeys.METADATA.latestVersion(true));
+        tooOld.forEach(i -> assertThat(topicNameLearningFilter.shouldHandleMetadataResponse((short) i)).isFalse());
+        hasTopicIds.forEach(i -> assertThat(topicNameLearningFilter.shouldHandleMetadataResponse((short) i)).isTrue());
+    }
+
+    @Test
+    void onlyInterceptCreateTopicResponseVersionsThatCanCarryTopicIds() {
+        IntStream tooOld = IntStream.range(ApiKeys.CREATE_TOPICS.oldestVersion(), 7);
+        IntStream hasTopicIds = IntStream.range(7, ApiKeys.CREATE_TOPICS.latestVersion(true));
+        tooOld.forEach(i -> assertThat(topicNameLearningFilter.shouldHandleCreateTopicsResponse((short) i)).isFalse());
+        hasTopicIds.forEach(i -> assertThat(topicNameLearningFilter.shouldHandleCreateTopicsResponse((short) i)).isTrue());
     }
 
     static Stream<MetadataResponseData> learnsNothingFromMetadataAndForwards() {
         MetadataResponseData empty = new MetadataResponseData();
         MetadataResponseData nullTopic = new MetadataResponseData().setTopics(null);
-        MetadataResponseData topicWithDefaultName = withSingleTopic(t -> {
+        MetadataResponseData topicWithDefaultName = metadataWithSingleTopic(t -> {
             t.setTopicId(TOPIC_UUID);
         });
-        MetadataResponseData topicWithNullName = withSingleTopic(t -> {
+        MetadataResponseData topicWithNullName = metadataWithSingleTopic(t -> {
             t.setTopicId(TOPIC_UUID);
             t.setName(null);
         });
-        MetadataResponseData topicWithDefaultTopicId = withSingleTopic(t -> {
+        MetadataResponseData topicWithDefaultTopicId = metadataWithSingleTopic(t -> {
             t.setName(TOPIC_NAME);
         });
-        MetadataResponseData topicWithNullTopidId = withSingleTopic(t -> {
+        MetadataResponseData topicWithNullTopidId = metadataWithSingleTopic(t -> {
             t.setTopicId(null);
             t.setName(TOPIC_NAME);
         });
         return Stream.of(empty, nullTopic, topicWithDefaultName, topicWithNullName, topicWithDefaultTopicId, topicWithNullTopidId);
     }
 
-    private static MetadataResponseData withSingleTopic(Consumer<MetadataResponseData.MetadataResponseTopic> topicModifier) {
-        MetadataResponseData response = new MetadataResponseData();
-        MetadataResponseData.MetadataResponseTopic topic = new MetadataResponseData.MetadataResponseTopic();
-        topicModifier.accept(topic);
-        response.topics().add(topic);
-        return response;
+    public static Stream<CreateTopicsResponseData> learnsNothingFromCreateTopicResponseAndForwards() {
+        CreateTopicsResponseData empty = new CreateTopicsResponseData();
+        CreateTopicsResponseData nullTopics = new CreateTopicsResponseData().setTopics(null);
+        CreateTopicsResponseData topicWithDefaultName = createTopicWithSingleTopic(t -> {
+            t.setTopicId(TOPIC_UUID);
+        });
+        CreateTopicsResponseData topicWithNullName = createTopicWithSingleTopic(t -> {
+            t.setTopicId(TOPIC_UUID);
+            t.setName(null);
+        });
+        CreateTopicsResponseData topicWithDefaultTopicId = createTopicWithSingleTopic(t -> {
+            t.setName(TOPIC_NAME);
+        });
+        CreateTopicsResponseData topicWithNullTopidId = createTopicWithSingleTopic(t -> {
+            t.setTopicId(null);
+            t.setName(TOPIC_NAME);
+        });
+        return Stream.of(empty, nullTopics, topicWithDefaultName, topicWithNullName, topicWithDefaultTopicId, topicWithNullTopidId);
     }
 
     @ParameterizedTest
     @MethodSource
     void learnsNothingFromMetadataAndForwards(MetadataResponseData data) {
+        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
         CompletionStage<ResponseFilterResult> resultStage = onMetadataResponse(data);
         assertThat(resultStage).isSameAs(FORWARD_FUTURE);
         verify(virtualClusterModel, never()).rememberTopicIdMapping(any(), any());
     }
 
+    @ParameterizedTest
+    @MethodSource
+    void learnsNothingFromCreateTopicResponseAndForwards(CreateTopicsResponseData data) {
+        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
+        CompletionStage<ResponseFilterResult> resultStage = onCreateTopicsResponse(data);
+        assertThat(resultStage).isSameAs(FORWARD_FUTURE);
+        verify(virtualClusterModel, never()).rememberTopicIdMapping(any(), any());
+    }
+
     @Test
-    void learnsTopicName() {
-        MetadataResponseData metadataResponseData = withSingleTopic(t -> {
+    void learnsTopicNameFromMetadataAndForwards() {
+        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
+        MetadataResponseData metadataResponseData = metadataWithSingleTopic(t -> {
             t.setName(TOPIC_NAME);
             t.setTopicId(TOPIC_UUID);
         });
-        CompletionStage<ResponseFilterResult> response1 = onMetadataResponse(metadataResponseData);
-        assertThat(response1).isSameAs(FORWARD_FUTURE);
+        CompletionStage<ResponseFilterResult> filterResult = onMetadataResponse(metadataResponseData);
+        assertThat(filterResult).isSameAs(FORWARD_FUTURE);
         verify(virtualClusterModel).rememberTopicIdMapping(TOPIC_UUID, TOPIC_NAME);
     }
 
     @Test
-    void learnsAllTopicNames() {
+    void learnsTopicNameFromCreateTopicAndForwards() {
+        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
+        CreateTopicsResponseData response = createTopicWithSingleTopic(t -> {
+            t.setName(TOPIC_NAME);
+            t.setTopicId(TOPIC_UUID);
+        });
+        CompletionStage<ResponseFilterResult> filterResult = onCreateTopicsResponse(response);
+        assertThat(filterResult).isSameAs(FORWARD_FUTURE);
+        verify(virtualClusterModel).rememberTopicIdMapping(TOPIC_UUID, TOPIC_NAME);
+    }
+
+    @Test
+    void learnsAllTopicNameFromMetadataAndForwards() {
+        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
         MetadataResponseData response = new MetadataResponseData();
         MetadataResponseData.MetadataResponseTopic topic = new MetadataResponseData.MetadataResponseTopic();
         topic.setName(TOPIC_NAME);
@@ -121,9 +175,52 @@ class TopicNameLearningFilterTest {
         verify(virtualClusterModel).rememberTopicIdMapping(topicId2, topicName2);
     }
 
+    @Test
+    void learnsAllTopicNameFromCreateTopicAndForwards() {
+        when(filterContext.forwardResponse(any(), any())).thenReturn(FORWARD_FUTURE);
+        CreateTopicsResponseData response = new CreateTopicsResponseData();
+        CreateTopicsResponseData.CreatableTopicResult topic = new CreateTopicsResponseData.CreatableTopicResult();
+        topic.setName(TOPIC_NAME);
+        topic.setTopicId(TOPIC_UUID);
+        response.topics().add(topic);
+
+        CreateTopicsResponseData.CreatableTopicResult topic2 = new CreateTopicsResponseData.CreatableTopicResult();
+        String topicName2 = "topic2";
+        Uuid topicId2 = Uuid.randomUuid();
+        topic2.setName(topicName2);
+        topic2.setTopicId(topicId2);
+        response.topics().add(topic2);
+
+        CompletionStage<ResponseFilterResult> filterResult = onCreateTopicsResponse(response);
+        assertThat(filterResult).isSameAs(FORWARD_FUTURE);
+        verify(virtualClusterModel).rememberTopicIdMapping(TOPIC_UUID, TOPIC_NAME);
+        verify(virtualClusterModel).rememberTopicIdMapping(topicId2, topicName2);
+    }
+
     private CompletionStage<ResponseFilterResult> onMetadataResponse(MetadataResponseData data) {
         return topicNameLearningFilter.onMetadataResponse((short) 12, new ResponseHeaderData(), data,
                 filterContext);
+    }
+
+    private CompletionStage<ResponseFilterResult> onCreateTopicsResponse(CreateTopicsResponseData data) {
+        return topicNameLearningFilter.onCreateTopicsResponse((short) 7, new ResponseHeaderData(), data,
+                filterContext);
+    }
+
+    private static MetadataResponseData metadataWithSingleTopic(Consumer<MetadataResponseData.MetadataResponseTopic> topicModifier) {
+        MetadataResponseData response = new MetadataResponseData();
+        MetadataResponseData.MetadataResponseTopic topic = new MetadataResponseData.MetadataResponseTopic();
+        topicModifier.accept(topic);
+        response.topics().add(topic);
+        return response;
+    }
+
+    private static CreateTopicsResponseData createTopicWithSingleTopic(Consumer<CreateTopicsResponseData.CreatableTopicResult> topicModifier) {
+        CreateTopicsResponseData response = new CreateTopicsResponseData();
+        CreateTopicsResponseData.CreatableTopicResult topic = new CreateTopicsResponseData.CreatableTopicResult();
+        topicModifier.accept(topic);
+        response.topics().add(topic);
+        return response;
     }
 
 }
