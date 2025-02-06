7881ccbe93293651f91632a66ecfa764d925aec2
Sam Barker <sbarker@redhat.com>
2025-02-06 13:34:27 +0100

    brute force io-uring support in perf tests
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

---
+++ performance-tests/07-record-encryption-filter-io-uring/run.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Copyright Kroxylicious Authors.
+#
+# Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+#
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. "${SCRIPT_DIR}/../common-perf.sh"
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. "${SCRIPT_DIR}/../common-perf.sh"
+
+CFG=04-record-encryption-filter/config.yaml
+ENDPOINT=kroxylicious:9092
+
+KROXYLICIOUS_CONFIG=${CFG} runDockerCompose up --detach --wait kroxylicious vault
+
+${CONTAINER_ENGINE} exec vault vault secrets enable transit 1>/dev/null
+${CONTAINER_ENGINE} exec vault vault write -f transit/keys/KEK_${TOPIC} 1>/dev/null
+
+setKroxyliciousContainerIdPID
+
+ENDPOINT=${ENDPOINT} doPerfTest
+
+unsetKroxyliciousContainerIdPID
+
+runDockerCompose rm -s -f kroxylicious vault
+
+
+
+
