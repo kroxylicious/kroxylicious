21ec1f0b4c42931eea3285d5502acc4e26e8f152
Kroxylicious Robot <153509303+kroxylicious-robot@users.noreply.github.com>
2025-02-25 09:35:32 +0100

    chore(deps): update hashicorp/vault docker tag to v1.18.5 (#1850)
    Signed-off-by: k-wall <18440250+k-wall@users.noreply.github.com>
    Co-authored-by: k-wall <18440250+k-wall@users.noreply.github.com>

--- kroxylicious-kms-provider-hashicorp-vault-test-support/src/main/java/io/kroxylicious/kms/provider/hashicorp/vault/VaultTestKmsFacade.java
+++ kroxylicious-kms-provider-hashicorp-vault-test-support/src/main/java/io/kroxylicious/kms/provider/hashicorp/vault/VaultTestKmsFacade.java
@@ -15,7 +15,7 @@ import org.testcontainers.vault.VaultContainer;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 public class VaultTestKmsFacade extends AbstractVaultTestKmsFacade {
-    private static final DockerImageName HASHICORP_VAULT = DockerImageName.parse("hashicorp/vault:1.18.4");
+    private static final DockerImageName HASHICORP_VAULT = DockerImageName.parse("hashicorp/vault:1.18.5");
 
     @SuppressWarnings("rawtypes")
     private VaultContainer vaultContainer;
