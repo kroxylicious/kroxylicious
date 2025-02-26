b7d3fe00933648618f9154600ad45984a5ffb59a
Francisco Vila <fvila@redhat.com>
2025-02-26 16:21:27 +0100

    first approach
    Signed-off-by: Francisco Vila <fvila@redhat.com>

---
+++ kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/logs/CollectorElement.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.systemtests.logs;
+
+public class CollectorElement {
+}
