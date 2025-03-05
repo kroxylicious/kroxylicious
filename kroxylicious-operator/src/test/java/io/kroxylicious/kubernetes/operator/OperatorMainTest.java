b9bc817568c6c0b6be8e260269aa81bf94ebad35
Sam Barker <sbarker@redhat.com>
2025-03-05 17:17:10 +1300

    Cleanup unit test by enabling crud mode in the mock and `expectCustomResource`
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/OperatorMainTest.java
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/OperatorMainTest.java
@@ -6,7 +6,6 @@
 
 package io.kroxylicious.kubernetes.operator;
 
-import java.net.HttpURLConnection;
 import java.util.concurrent.TimeUnit;
 
 import org.awaitility.Awaitility;
@@ -16,28 +15,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import io.fabric8.kubernetes.api.model.APIResource;
-import io.fabric8.kubernetes.api.model.APIResourceBuilder;
-import io.fabric8.kubernetes.api.model.APIResourceList;
-import io.fabric8.kubernetes.api.model.APIResourceListBuilder;
-import io.fabric8.kubernetes.api.model.GenericKubernetesResourceBuilder;
-import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.api.model.KubernetesList;
-import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
-import io.fabric8.kubernetes.api.model.ListMetaBuilder;
-import io.fabric8.kubernetes.api.model.Secret;
-import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.WatchEvent;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
 import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.search.MeterNotFoundException;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 
-import io.kroxylicious.kubernetes.api.v1alpha1.KafkaProxy;
-import io.kroxylicious.kubernetes.api.v1alpha1.VirtualKafkaCluster;
+import io.kroxylicious.kubernetes.api.v1alpha1.KafkaProxyBuilder;
 import io.kroxylicious.kubernetes.filter.api.v1alpha1.KafkaProtocolFilter;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,7 +35,6 @@ import static org.mockito.Mockito.verify;
 @EnableKubernetesMockClient(crud = true)
 class OperatorMainTest {
 
-    private static final String START_RESOURCE_VERSION = "1000";
     KubernetesClient kubeClient;
     KubernetesMockServer mockServer;
 
@@ -60,14 +46,6 @@ class OperatorMainTest {
     @BeforeEach
     void setUp() {
         expectApiResources();
-
-        expectWatchOn("/apis/kroxylicious.io/v1alpha1/kafkaproxies", KafkaProxy.class, "kafkaproxy", "kroxylicious.io/v1alpha1", 1000);
-        expectWatchOn("/apis/kroxylicious.io/v1alpha1/virtualkafkaclusters", VirtualKafkaCluster.class, "virtualkafkacluster", "kroxylicious.io/v1alpha1", 1000);
-        expectWatchOn("/apis/filter.kroxylicious.io/v1alpha1/kafkaprotocolfilters", KafkaProtocolFilter.class, "kafkaprotocolfilter", "kroxylicious.io/v1alpha1", 0);
-        expectWatchOn("/apis/apps/v1/deployments", Deployment.class, "deployment", "v1", 1000);
-        expectWatchOn("/api/v1/secrets", Secret.class, "secret", "v1", 1000);
-        expectWatchOn("/api/v1/services", Service.class, "service", "v1", 1000);
-
         operatorMain = new OperatorMain(() -> globalRegistry, kubeClient);
     }
 
@@ -86,10 +64,11 @@ class OperatorMainTest {
     @Test
     void shouldRegisterOperatorMetrics() {
         // Given
+        final KafkaProxyBuilder proxyBuilder = new KafkaProxyBuilder().withKind("KafkaProxy").withNewMetadata().withName("myCoolProxy").endMetadata();
         operatorMain.run();
 
         // When
-
+        kubeClient.resource(proxyBuilder.build()).create();
 
         // Then
         Awaitility.await()
@@ -111,58 +90,13 @@ class OperatorMainTest {
     }
 
     private void expectApiResources() {
-        final APIResource kafkaProtocolFilterCrd = new APIResourceBuilder()
-                .withCategories("kroxylicious-plugins")
-                .withKind("KafkaProtocolFilter")
-                .withName("kafkaprotocolfilters")
-                .withNamespaced(true)
-                .withShortNames("kpf")
-                .withSingularName("kafkaprotocolfilter")
-                .withVerbs("delete", "deletecollection", "get", "list", "patch", "create", "update", "watch")
+        final CustomResourceDefinition kafkaProtocolFilterCrd = CustomResourceDefinitionContext.v1CRDFromCustomResourceType(KafkaProtocolFilter.class).withNewMetadata()
+                .endMetadata()
                 .build();
-
-        final APIResourceList apiResourceList = new APIResourceListBuilder()
-                .withGroupVersion("filter.kroxylicious.io/v1alpha1")
-                .withApiVersion("v1")
-                .addToResources(kafkaProtocolFilterCrd)
+        mockServer.expectCustomResource(CustomResourceDefinitionContext.fromCrd(kafkaProtocolFilterCrd));
+        final CustomResourceDefinition kafkaProxyCrd = CustomResourceDefinitionContext.v1CRDFromCustomResourceType(KafkaProtocolFilter.class).withNewMetadata()
+                .endMetadata()
                 .build();
-
-        mockServer.expect()
-                .get()
-                .withPath("/apis/filter.kroxylicious.io/v1alpha1")
-                .andReturn(HttpURLConnection.HTTP_OK, apiResourceList)
-                .always();
-    }
-
-    private <T extends HasMetadata> void expectWatchOn(String path, Class<T> resultType, String kind, String apiVersion, int delayMs) {
-        mockServer.expect()
-                .get()
-                .withPath(path + "?resourceVersion=0")
-                .andReturn(HttpURLConnection.HTTP_OK, getList(resultType, kind, apiVersion))
-                .always();
-        mockServer.expect()
-                .withPath(path + "?allowWatchBookmarks=true&resourceVersion=" + START_RESOURCE_VERSION + "&timeoutSeconds=600&watch=true")
-                .andUpgradeToWebSocket()
-                .open()
-                .waitFor(delayMs)
-                .andEmit(new WatchEvent(new GenericKubernetesResourceBuilder()
-                        .withKind(kind)
-                        .withApiVersion(apiVersion)
-                        .withNewMetadata()
-                        .withName("random")
-                        .withNamespace("randomNs")
-                        .endMetadata()
-                        .build(), "ADDED"))
-                .done()
-                .always();
-    }
-
-    @SuppressWarnings("unused")
-    private <T extends HasMetadata> KubernetesList getList(Class<T> resourceClass, String kind, String apiVersion) {
-        return new KubernetesListBuilder()
-                .withKind(kind)
-                .withApiVersion(apiVersion)
-                .withMetadata(new ListMetaBuilder().withResourceVersion(START_RESOURCE_VERSION).build())
-                .build();
+        mockServer.expectCustomResource(CustomResourceDefinitionContext.fromCrd(kafkaProxyCrd));
     }
 }
