b7b6b28334a8d67ab70a68616b1d0171c3ade991
Keith Wall <kwall@apache.org>
2025-03-06 14:25:16 +0000

    Report absence of KCR at the Proxy level
    Signed-off-by: Keith Wall <kwall@apache.org>

--- kroxylicious-operator/src/main/java/io/kroxylicious/kubernetes/operator/ClusterCondition.java
+++ kroxylicious-operator/src/main/java/io/kroxylicious/kubernetes/operator/ClusterCondition.java
@@ -6,6 +6,13 @@
 
 package io.kroxylicious.kubernetes.operator;
 
+import java.util.Locale;
+import java.util.Optional;
+
+import io.kroxylicious.kubernetes.api.v1alpha1.virtualkafkaclusterspec.TargetCluster;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
 import static io.kroxylicious.kubernetes.api.v1alpha1.kafkaproxystatus.clusters.Conditions.Status;
 
 public record ClusterCondition(String cluster, ConditionType type, Status status, String reason, String message) {
@@ -29,6 +36,18 @@ public record ClusterCondition(String cluster, ConditionType type, Status status
     static ClusterCondition filterKindNotKnown(String cluster, String filterName, String kind) {
         return new ClusterCondition(cluster, ConditionType.Accepted, Status.FALSE, INVALID,
                 String.format("Filter \"%s\" has a kind %s that is not known to this operator.", filterName, kind));
+    }
+
+    static ClusterCondition targetClusterRefNotExists(String cluster, TargetCluster targetCluster) {
+        return new ClusterCondition(cluster, ConditionType.Accepted, Status.FALSE, INVALID,
+                String.format("Target Cluster \"%s\" does not exist.", kubeName(targetCluster).orElse("<unknown>")));
+    }
+
+    @NonNull
+    private static Optional<String> kubeName(TargetCluster targetCluster) {
+        return Optional.ofNullable(targetCluster.getClusterRef())
+                .map(r -> "%s%s/%s".formatted(r.getKind().toLowerCase(Locale.ROOT), Optional.ofNullable(r.getGroup()).map(
+                        ".%s"::formatted).orElse(""), r.getName()));
     }
 
 }
