0f195c9d12d6dbb180e1336bd86c90d18e108bcf
Tom Bentley <tbentley@redhat.com>
2025-03-17 14:58:52 +1300

    Add some tests
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

---
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/api/common/KafkaCRefTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kubernetes.api.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KafkaCRefTest {
+
+    @Test
+    void shouldEqualAKafkaKindedAnyRef() {
+        var kafkaCRefFoo = new KafkaCRefBuilder().withName("foo").build();
+        var anyFoo = new AnyLocalRefBuilder().withName("foo").withKind(kafkaCRefFoo.getKind()).withGroup(kafkaCRefFoo.getGroup()).build();
+        assertThat(kafkaCRefFoo).isEqualTo(anyFoo);
+        assertThat(anyFoo).isEqualTo(kafkaCRefFoo);
+        assertThat(kafkaCRefFoo).hasSameHashCodeAs(anyFoo);
+    }
+
+}
