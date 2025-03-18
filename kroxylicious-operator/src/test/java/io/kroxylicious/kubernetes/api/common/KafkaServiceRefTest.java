6a01388311f7bb980af1a076955df70b74765b62
Robert Young <robeyoun@redhat.com>
2025-03-18 15:16:41 +1300

    Suppress sonar warning
    Signed-off-by: Robert Young <robeyoun@redhat.com>

--- kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/api/common/KafkaServiceRefTest.java
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/api/common/KafkaServiceRefTest.java
@@ -12,6 +12,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class KafkaServiceRefTest {
 
+    // we knowingly use equals across types because we want the property that specific LocalRef types are equal to any other LocalRef
+    // with the same group, kind and name.
+    @SuppressWarnings("java:S5845")
     @Test
     void shouldEqualAKafkaKindedAnyRef() {
         var kafkaCRefFoo = new KafkaServiceRefBuilder().withName("foo").build();
