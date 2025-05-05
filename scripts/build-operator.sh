3b632a97be11c60f0b9069e93e4101c1de7497db
Sam Barker <sam@quadrocket.co.uk>
2025-05-05 15:59:29 +1200

    PR Review: Build image tag from constituent parts
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- scripts/build-operator.sh
+++ scripts/build-operator.sh
@@ -12,4 +12,7 @@ info "building operator image in minikube for commit ${GIT_HASH}"
 IMAGE_TAG="${IMAGE_TAG:-dev-git-${GIT_HASH}}"
 KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION:-$(mvn org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=project.version -q -DforceStdout)}
 TARGETARCH=${TARGETARCH:-$(uname -m)}
-minikube image build . -f Dockerfile.operator -t quay.io/${QUAY_ORG}/operator:${IMAGE_TAG} --build-opt=build-arg=KROXYLICIOUS_VERSION="${KROXYLICIOUS_VERSION}" --build-opt=build-arg=TARGETARCH="${TARGETARCH}"
+IMAGE_REGISTRY=${IMAGE_REGISTRY:-"quay.io"}
+IMAGE_REGISTRY_ORG=${IMAGE_REGISTRY_ORG:-${QUAY_ORG}}
+IMAGE_NAME=${IMAGE_NAME:operator}
+minikube image build . -f Dockerfile.operator -t ${IMAGE_REGISTRY}/${IMAGE_REGISTRY_ORG}/${IMAGE_NAME}:${IMAGE_TAG} --build-opt=build-arg=KROXYLICIOUS_VERSION="${KROXYLICIOUS_VERSION}" --build-opt=build-arg=TARGETARCH="${TARGETARCH}"
