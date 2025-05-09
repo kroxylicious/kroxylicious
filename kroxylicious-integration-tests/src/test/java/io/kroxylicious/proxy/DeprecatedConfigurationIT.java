216061bb34f78a56b39c3c292e9f3f22b6e1e67a
Keith Wall <kwall@apache.org>
2025-05-09 18:53:11 +0100

    Fix #1873: Remove deprecated top-level configuration property filters
    Signed-off-by: Keith Wall <kwall@apache.org>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/DeprecatedConfigurationIT.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/DeprecatedConfigurationIT.java
@@ -6,19 +6,11 @@
 
 package io.kroxylicious.proxy;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Stream;
 
 import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.Serdes;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,21 +18,13 @@ import org.junit.jupiter.params.provider.MethodSource;
 import io.kroxylicious.proxy.config.ClusterNetworkAddressConfigProviderDefinition;
 import io.kroxylicious.proxy.config.ClusterNetworkAddressConfigProviderDefinitionBuilder;
 import io.kroxylicious.proxy.config.ConfigurationBuilder;
-import io.kroxylicious.proxy.config.FilterDefinition;
-import io.kroxylicious.proxy.config.NamedFilterDefinitionBuilder;
 import io.kroxylicious.proxy.config.VirtualClusterBuilder;
-import io.kroxylicious.proxy.filter.simpletransform.FetchResponseTransformation;
-import io.kroxylicious.proxy.filter.simpletransform.UpperCasing;
 import io.kroxylicious.proxy.internal.clusternetworkaddressconfigprovider.PortPerBrokerClusterNetworkAddressConfigProvider;
 import io.kroxylicious.proxy.internal.clusternetworkaddressconfigprovider.RangeAwarePortPerNodeClusterNetworkAddressConfigProvider;
 import io.kroxylicious.proxy.service.HostPort;
 import io.kroxylicious.testing.kafka.api.KafkaCluster;
-import io.kroxylicious.testing.kafka.junit5ext.Topic;
 
-import static io.kroxylicious.test.tester.KroxyliciousConfigUtils.proxy;
 import static io.kroxylicious.test.tester.KroxyliciousTesters.kroxyliciousTester;
-import static org.apache.kafka.clients.producer.ProducerConfig.CLIENT_ID_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 
@@ -50,39 +34,6 @@ import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 public class DeprecatedConfigurationIT extends BaseIT {
 
     private static final HostPort PROXY_ADDRESS = HostPort.parse("localhost:9192");
-
-    @Test
-    @SuppressWarnings("deprecation")
-    void shouldSupportTopLevelFiltersProperty(KafkaCluster cluster, Topic topic1) throws Exception {
-
-        FilterDefinition filterDefinition = new NamedFilterDefinitionBuilder(
-                "filter-1", FetchResponseTransformation.class.getName())
-                .withConfig("transformation", UpperCasing.class.getName())
-                .withConfig("transformationConfig", Map.of("charset", "UTF-8")).build().asFilterDefinition();
-        var config = proxy(cluster)
-                .addToFilters(filterDefinition);
-
-        try (var tester = kroxyliciousTester(config);
-                var producer = tester.producer(Serdes.String(), Serdes.String(),
-                        Map.of(CLIENT_ID_CONFIG, "shouldSupportTopLevelFiltersProperty",
-                                DELIVERY_TIMEOUT_MS_CONFIG, 3_600_000));
-                var consumer = tester.consumer(Serdes.String(), Serdes.String(),
-                        Map.of(
-                                ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString(),
-                                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"))) {
-
-            producer.send(new ProducerRecord<>(topic1.name(), "my-key", "hello")).get();
-            producer.flush();
-
-            consumer.subscribe(Set.of(topic1.name()));
-            var records = consumer.poll(Duration.ofSeconds(100));
-            assertThat(records).hasSize(1);
-            assertThat(records.records(topic1.name()))
-                    .singleElement()
-                    .extracting(ConsumerRecord::value)
-                    .isEqualTo("HELLO");
-        }
-    }
 
     @SuppressWarnings("removal")
     static Stream<Arguments> shouldSupportDeprecatedClusterNetworkAddressConfigProvider() {
