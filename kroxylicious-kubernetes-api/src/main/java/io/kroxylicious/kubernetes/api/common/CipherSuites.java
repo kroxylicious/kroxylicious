8c4c8318470e0a35413ed565007c510f4d7f5da2
Keith Wall <kwall@apache.org>
2025-05-12 09:17:35 +0100

    addressing review comments
    Signed-off-by: Keith Wall <kwall@apache.org>

--- kroxylicious-kubernetes-api/src/main/java/io/kroxylicious/kubernetes/api/common/CipherSuites.java
+++ kroxylicious-kubernetes-api/src/main/java/io/kroxylicious/kubernetes/api/common/CipherSuites.java
@@ -16,7 +16,7 @@ import com.fasterxml.jackson.annotation.JsonSetter;
  * TLS cipher suite controls.
  */
 @com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
-@com.fasterxml.jackson.annotation.JsonPropertyOrder({ "allow", "deby" })
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({ "allow", "deny" })
 @com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
 @javax.annotation.processing.Generated("io.fabric8.java.generator.CRGeneratorRunner")
 @lombok.ToString()
