5bf1573b1ddcb04fa0ccefd54264051240612b0f
Sam Barker <sam@quadrocket.co.uk>
2025-05-16 11:54:10 +1200

    Address sonar winning
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/KindInstallKT.java
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/KindInstallKT.java
@@ -30,7 +30,6 @@ class KindInstallKT extends AbstractInstallKT {
 
     private static final String IMAGE_NAME = OperatorInfo.fromResource().imageName();
     private static final String IMAGE_ARCHIVE = OperatorInfo.fromResource().imageArchive();
-    private static boolean loaded = false;
 
     @BeforeAll
     static void beforeAll() throws IOException, InterruptedException {
@@ -39,17 +38,12 @@ class KindInstallKT extends AbstractInstallKT {
                 "load",
                 "image-archive",
                 IMAGE_ARCHIVE);
-        loaded = true;
     }
 
     @AfterAll
-    static void afterAll() throws IOException, InterruptedException {
-        // if (loaded) {
+    static void afterAll() {
         // TODO deleting image from kind would look something like `podman exec -it $(kind get clusters | head -1)-control-plane crictl rmi quay.io/kroxylicious/operator:0.13.0-SNAPSHOT`
         // but for that to work we need to know the container engine docker -vs- podman and the tag (which we don't currently have in the properties)
-        // LOGGER.info("Deleting imagestream {}", IMAGE_STREAM_NAME);
-        // exec("kind", "delete", "imagestream", IMAGE_STREAM_NAME);
-        // }
     }
 
     public static boolean isEnvironmentValid() throws IOException, InterruptedException {
