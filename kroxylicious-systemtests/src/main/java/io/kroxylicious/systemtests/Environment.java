66fb4f5a911c984cca4d69a14e03fb285966b0ff
Francisco Vila <fvila@redhat.com>
2025-05-26 09:29:28 +0200

    added test clients image base env variable
    Signed-off-by: Francisco Vila <fvila@redhat.com>

--- kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/Environment.java
+++ kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/Environment.java
@@ -51,6 +51,8 @@ public class Environment {
     private static final String AWS_KROXYLICIOUS_ACCESS_KEY_ID_ENV = "AWS_KROXYLICIOUS_ACCESS_KEY_ID";
     private static final String AWS_KROXYLICIOUS_SECRET_ACCESS_KEY_ENV = "AWS_KROXYLICIOUS_SECRET_ACCESS_KEY";
     private static final String AWS_REGION_ENV = "AWS_REGION";
+    private static final String TEST_CLIENTS_IMAGE_BASE_ENV = "TEST_CLIENTS_IMAGE_BASE";
+    private static final String ARCHITECTURE_ENV = "ARCHITECTURE";
 
     /**
      * The kafka version default value
@@ -101,6 +103,7 @@ public class Environment {
     public static final String AWS_KROXYLICIOUS_ACCESS_KEY_ID_DEFAULT = AWS_ACCESS_KEY_ID_DEFAULT;
     private static final String AWS_KROXYLICIOUS_SECRET_ACCESS_KEY_DEFAULT = AWS_SECRET_ACCESS_KEY_DEFAULT;
     public static final String AWS_REGION_DEFAULT = "us-east-2";
+    private static final String TEST_CLIENTS_IMAGE_BASE_DEFAULT = "quay.io/strimzi-test-clients/test-clients:latest-kafka";
 
     public static final String KAFKA_VERSION = ENVIRONMENT_VARIABLES.getOrDefault(KAFKA_VERSION_ENV, KAFKA_VERSION_DEFAULT);
     public static final String KROXYLICIOUS_OPERATOR_VERSION = ENVIRONMENT_VARIABLES.getOrDefault(KROXYLICIOUS_OPERATOR_VERSION_ENV, KROXYLICIOUS_VERSION_DEFAULT);
@@ -140,6 +143,9 @@ public class Environment {
     public static final String KROXYLICIOUS_OPERATOR_ORG = ENVIRONMENT_VARIABLES.getOrDefault(KROXYLICIOUS_OPERATOR_ORG_ENV, KROXYLICIOUS_OPERATOR_ORG_DEFAULT);
     public static final String KROXYLICIOUS_OPERATOR_REGISTRY = ENVIRONMENT_VARIABLES.getOrDefault(KROXYLICIOUS_OPERATOR_REGISTRY_ENV,
             KROXYLICIOUS_OPERATOR_REGISTRY_DEFAULT);
+
+    public static final String TEST_CLIENTS_IMAGE_BASE = ENVIRONMENT_VARIABLES.getOrDefault(TEST_CLIENTS_IMAGE_BASE_ENV, TEST_CLIENTS_IMAGE_BASE_DEFAULT);
+    public static final String ARCHITECTURE = ENVIRONMENT_VARIABLES.getOrDefault(ARCHITECTURE_ENV, "");
 
     private static String readMetadataProperty(String property) {
         var p = new Properties();
