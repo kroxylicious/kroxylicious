85bcd1244e793174db3ffaed129729940724907f
Sam Barker <sam@quadrocket.co.uk>
2025-05-29 14:33:06 +1200

    Fix formatting
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>
    Signed-off-by: Ujjwal <ukalia@confluent.io>

--- kroxylicious-systemtests/src/test/java/io/kroxylicious/systemtests/OperatorChangeDetectionST.java
+++ kroxylicious-systemtests/src/test/java/io/kroxylicious/systemtests/OperatorChangeDetectionST.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
