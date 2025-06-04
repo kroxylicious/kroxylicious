f5425b37c647de6ac084c5ba1a8be37a5efe20ef
ShubhamRwt <shubhamrwt02@gmail.com>
2025-06-04 23:01:08 +0530

    Revert changes
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    Signed-off-by: Ujjwal <ukalia@confluent.io>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/util/Metrics.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/util/Metrics.java
@@ -232,7 +232,7 @@ public class Metrics {
 
     @NonNull
     private static String required(String value) {
-        if ((Objects.isNull(value))) {
+        if ((Objects.isNull(value) || value.trim().isEmpty())) {
             throw new IllegalArgumentException("tag value supplied without a value");
         }
         return value;
