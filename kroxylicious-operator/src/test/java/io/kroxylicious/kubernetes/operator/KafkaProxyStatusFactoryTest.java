bf31df56c9acb5588c24a7ef2d8349cf6f4c23c0
Sam Barker <sam@quadrocket.co.uk>
2025-06-13 16:18:25 +1200

    Cleanup replicaCount handling.
    Should improve coverage too.
    
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/KafkaProxyStatusFactoryTest.java
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/KafkaProxyStatusFactoryTest.java
@@ -11,12 +11,18 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.util.UUID;
 
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.assertj.core.api.InstanceOfAssertFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.kroxylicious.kubernetes.api.common.Condition;
 import io.kroxylicious.kubernetes.api.v1alpha1.KafkaProxy;
 import io.kroxylicious.kubernetes.api.v1alpha1.KafkaProxyBuilder;
+import io.kroxylicious.kubernetes.api.v1alpha1.KafkaProxyStatus;
+import io.kroxylicious.kubernetes.operator.assertj.AssertFactory;
+import io.kroxylicious.kubernetes.operator.assertj.KafkaProxyStatusAssert;
 import io.kroxylicious.kubernetes.operator.checksum.MetadataChecksumGenerator;
 
 import static io.kroxylicious.kubernetes.operator.assertj.OperatorAssertions.assertThat;
@@ -89,4 +95,17 @@ class KafkaProxyStatusFactoryTest {
         assertThat(patchedProxy).doesNotHaveAnnotation(Annotations.REFERENT_CHECKSUM_ANNOTATION_KEY);
     }
 
+    @Test
+    void shouldIncludeReplicaCountInStatus() {
+        // Given
+
+        // When
+        KafkaProxy patchedProxy = kafkaProxyStatusFactory.newTrueConditionStatusPatch(KAFKA_PROXY, Condition.Type.ResolvedRefs, 5);
+
+        // Then
+        assertThat(patchedProxy)
+                .isNotNull()
+                .extracting(KafkaProxy::getStatus, AssertFactory.status())
+                .replicas(5);
+    }
 }
