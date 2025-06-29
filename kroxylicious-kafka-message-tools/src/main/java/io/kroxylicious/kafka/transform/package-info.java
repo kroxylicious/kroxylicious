9b10f7969bf8bcea8a0adbd4ac491bbda1dd8758
Tom Bentley <tbentley@redhat.com>
2025-06-30 09:34:23 +1200

    @NonNull by default in kroxylicious-kafka-message-tools
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-kafka-message-tools/src/main/java/io/kroxylicious/kafka/transform/package-info.java
+++ kroxylicious-kafka-message-tools/src/main/java/io/kroxylicious/kafka/transform/package-info.java
@@ -8,4 +8,12 @@
  * <p>Utilities for dealing with {@link org.apache.kafka.common.record.Record}s, {@link org.apache.kafka.common.record.RecordBatch}es
  * and {@link org.apache.kafka.common.record.MemoryRecords}.</p>
  */
+@ReturnValuesAreNonnullByDefault
+@DefaultAnnotationForParameters(NonNull.class)
+@DefaultAnnotation(NonNull.class)
 package io.kroxylicious.kafka.transform;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotationForParameters;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.ReturnValuesAreNonnullByDefault;
