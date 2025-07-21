86c38ddb9ea0b394126403077b4238c0636387cd
Keith Wall <kwall@apache.org>
2025-07-21 10:16:34 +0100

    build: Adopt Central Publishing Portal
    why: The OSSRH service has reached  end-of-life, we need to adopt CPP
    Signed-off-by: Keith Wall <kwall@apache.org>

--- scripts/stage-docs.sh
+++ scripts/stage-docs.sh
@@ -11,13 +11,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Tmp fix - revert the nounset change made by common to match the expectations of this script.
 set +u
 
+RELEASE_DATE=$(date -u '+%Y-%m-%d')
 REPOSITORY="origin"
 BRANCH_FROM="main"
 DRY_RUN="false"
 ORIGINAL_GH_DEFAULT_REPO=""
-while getopts ":v:u:dh" opt; do
+while getopts ":v:b:u:dh" opt; do
   case $opt in
     v) RELEASE_VERSION="${OPTARG}"
+    ;;
+    b) RELEASE_DOCS_BRANCH="${OPTARG}"
     ;;
     u) WEBSITE_REPO_URL="${OPTARG}"
     ;;
@@ -25,8 +28,9 @@ while getopts ":v:u:dh" opt; do
     ;;
     h)
       1>&2 cat << EOF
-usage: $0 -v version -u url [-b branch] [-r repository] [-d] [-h]
+usage: $0 -v version -u url -b branch [-r repository] [-d] [-h]
  -v version number e.g. 0.3.0
+ -b name of the release branch to create in the website repository
  -u url of the website repository e.g. git@github.com:kroxylicious/kroxylicious.github.io.git
  -d dry-run mode
  -h this help message
@@ -42,6 +46,16 @@ done
 
 if [[ -z ${RELEASE_VERSION} ]]; then
   echo "No version specified, aborting"
+  exit 1
+fi
+
+if [[ -z ${RELEASE_DOCS_BRANCH} ]]; then
+  echo "No release branch specified, aborting"
+  exit 1
+fi
+
+if [[ -z ${RUN_ID_LABEL} ]]; then
+  echo "No run id label specified, aborting"
   exit 1
 fi
 
@@ -83,9 +97,7 @@ trap cleanup EXIT
 
 git stash --all
 
-RELEASE_DATE=$(date -u '+%Y-%m-%d')
 RELEASE_TAG="v${RELEASE_VERSION}"
-RELEASE_DOCS_BRANCH="prepare-${RELEASE_TAG}-release-docs-${RELEASE_DATE}"
 
 WEBSITE_TMP=$(mktemp -d)
 
@@ -172,4 +184,8 @@ gh repo set-default "$(git remote get-url "${REPOSITORY}")"
 echo "Creating pull request to publish release documentation to website."
 # Open PR to merge branch to `main` in `kroxylicious/kroxylicious.github.io`
 BODY="Prepare ${RELEASE_TAG} release documentation for publishing to website"
-gh pr create --head "${RELEASE_DOCS_BRANCH}" --base "${BRANCH_FROM}" --title "Kroxylicious ${RELEASE_TAG} release documentation ${RELEASE_DATE}" --body "${BODY}" --repo "$(gh repo set-default -v)"
+gh pr create --head "${RELEASE_DOCS_BRANCH}" \
+             --base "${BRANCH_FROM}" \
+             --title "Kroxylicious ${RELEASE_TAG} release documentation ${RELEASE_DATE}" \
+             --body "${BODY}" \
+             --repo "$(gh repo set-default -v)"
--- scripts/stage-release.sh
+++ scripts/stage-release.sh
@@ -14,10 +14,9 @@ set +u
 REPOSITORY="origin"
 BRANCH_FROM="main"
 WORK_BRANCH_NAME="release-work-$(openssl rand -hex 12)"
-DRY_RUN="false"
 SKIP_VALIDATION="false"
 RELEASE_NOTES_DIR=${RELEASE_NOTES_DIR:-.releaseNotes}
-while getopts ":v:b:k:r:n:w:dsh" opt; do
+while getopts ":l:v:b:k:r:n:w:sh" opt; do
   case $opt in
     v) RELEASE_VERSION="${OPTARG}"
     ;;
@@ -29,23 +28,23 @@ while getopts ":v:b:k:r:n:w:dsh" opt; do
     ;;
     k) GPG_KEY="${OPTARG}"
     ;;
-    w) WORK_BRANCH_NAME="${OPTARG}"
+    l) RUN_ID_LABEL="${OPTARG}"
     ;;
-    d) DRY_RUN="true"
+    w) WORK_BRANCH_NAME="${OPTARG}"
     ;;
     s) SKIP_VALIDATION="true"
     ;;
     h)
       1>&2 cat << EOF
-usage: $0 -k keyid -v version [-b branch] [-r repository] [-s] [-d] [-h]
+usage: $0 -k keyid -v version -i github-workflow runid [-b branch] [-r repository] [-s] [-d] [-h]
  -k short key id used to sign the release
  -v version number e.g. 0.3.0
  -b branch to release from (defaults to 'main')
  -n development version e.g. 0.4.0-SNAPSHOT
+ -i GitHub Workflow Run Id
  -r the remote name of the kroxylicious repository (defaults to 'origin')
  -w release work branch
  -s skips validation
- -d dry-run mode
  -h this help message
 EOF
       exit 1
@@ -54,8 +53,12 @@ EOF
     exit 1
     ;;
   esac
-
 done
+
+if [[ -z "${RUN_ID_LABEL}" ]]; then
+    echo "No run id label. Please specify -l <run id label>" 1>&2
+    exit 1
+fi
 
 if [[ -z "${GPG_KEY}" ]]; then
     echo "GPG_KEY not set unable to sign the release. Please specify -k <YOUR_GPG_KEY>" 1>&2
@@ -65,6 +68,12 @@ fi
 if [[ -z ${RELEASE_VERSION} ]]; then
   echo "No version specified, aborting"
   exit 1
+fi
+
+if ! command -v gh &> /dev/null
+then
+    echo "gh command could not be found."
+    exit 1
 fi
 
 GREEN='\033[0;32m'
@@ -95,11 +104,11 @@ cleanup() {
     # Note that git branch -D echos the sha of the deleted branch to
     # stdout.  This is great for debugging the release process as it
     # lets the developer restore to the state of the tree.
-    if [[ "${DRY_RUN:-false}" == true && ${TEMPORARY_RELEASE_BRANCH} ]]; then
+    if [[ ${TEMPORARY_RELEASE_BRANCH} ]]; then
         git branch -D "${TEMPORARY_RELEASE_BRANCH}" || true
     fi
 
-    if [[ "${DRY_RUN:-false}" == true && ${PREPARE_DEVELOPMENT_BRANCH} ]]; then
+    if [[ ${PREPARE_DEVELOPMENT_BRANCH} ]]; then
         git branch -D "${PREPARE_DEVELOPMENT_BRANCH}" || true
     fi
 }
@@ -126,17 +135,6 @@ INITIAL_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate 
 TEMPORARY_RELEASE_BRANCH="${WORK_BRANCH_NAME}-rel"
 git checkout -b "${TEMPORARY_RELEASE_BRANCH}" "${REPOSITORY}/${BRANCH_FROM}"
 
-if [[ "${DRY_RUN:-false}" == true ]]; then
-    DEPLOY_DRY_RUN_DIR=$(mktemp -d)
-    #Disable the shell check as the colour codes only work with interpolation.
-    # shellcheck disable=SC2059
-    printf "${BLUE}Dry-run mode:${NC} no remote tags or PRs will be created, artefacts will be deployed to: ${DEPLOY_DRY_RUN_DIR}\n"
-    MVN_DEPLOY_DRYRUN="-DaltDeploymentRepository=ossrh::file:${DEPLOY_DRY_RUN_DIR}"
-    GIT_DRYRUN="--dry-run"
-else
-    MVN_DEPLOY_DRYRUN=""
-fi
-
 if [[ "${SKIP_VALIDATION:-false}" != true ]]; then
     printf "Validating the build is ${GREEN}green${NC}"
     mvn -q clean verify
@@ -160,13 +158,21 @@ git commit --message "Release version ${RELEASE_TAG}" --signoff
 
 git tag -f "${RELEASE_TAG}"
 
-git push "${REPOSITORY}" "${RELEASE_TAG}" ${GIT_DRYRUN:-}
+git push "${REPOSITORY}" "${RELEASE_TAG}"
 
 echo "Deploying release"
 
-# shellcheck disable=SC2086
-# Quoting leads to an extra space which causes maven to barf!
-mvn -q -Prelease,dist -DskipTests=true -DskipDocs=true -DskipKTs=true -DreleaseSigningKey="${GPG_KEY}" ${MVN_DEPLOY_DRYRUN} -DprocessAllModules=true deploy
+MVN_DEPLOY_OUTPUT=$(mktemp)
+mvn -Prelease,dist -DskipTests=true -DskipDocs=true -DskipKTs=true -DskipDocker=true -DreleaseSigningKey="${GPG_KEY}" -DprocessAllModules=true deploy | tee ${MVN_DEPLOY_OUTPUT}
+DEPLOYMENT_ID=$(awk -F'[ .]' '/Uploaded bundle successfully/ {print $9}' < ${MVN_DEPLOY_OUTPUT})
+
+if [[ -z "${DEPLOYMENT_ID}" ]]; then
+     echo "Failed to find Central Publishing Portal deployment id in Maven deploy output" 1>&2
+     exit 1
+fi
+
+echo "Found Central Publishing Portal deployment id: ${DEPLOYMENT_ID}"
+echo "${DEPLOYMENT_ID}" > DEPLOYMENT.ID
 
 echo "Release deployed. Extracting release notes in: ${RELEASE_NOTES_DIR}"
 mkdir -p "${RELEASE_NOTES_DIR}"
@@ -192,15 +198,6 @@ git add kroxylicious-api/pom.xml
 
 git commit --message "Start next development version" --signoff
 
-if [[ "${DRY_RUN:-false}" == true ]]; then
-    exit 0
-fi
-
-if ! command -v gh &> /dev/null
-then
-    echo "gh command could not be found. Please create a pull request by hand https://github.com/kroxylicious/kroxylicious/compare"
-    exit
-fi
 
 ORIGINAL_GH_DEFAULT_REPO=$(gh repo set-default -v | (grep -v 'no default repository' || true))
 gh repo set-default "$(git remote get-url "${REPOSITORY}")"
@@ -227,4 +224,10 @@ BODY="Release version ${RELEASE_VERSION}"
 git push "${REPOSITORY}" HEAD
 
 echo "Creating pull request to merge the released version."
-gh pr create --head "${PREPARE_DEVELOPMENT_BRANCH}" --base "${BRANCH_FROM}" --title "Kroxylicious release version ${RELEASE_VERSION} development version ${NEXT_VERSION}" --body "${BODY}" --repo "$(gh repo set-default -v)"
+gh pr create --head "${PREPARE_DEVELOPMENT_BRANCH}" \
+             --base "${BRANCH_FROM}" \
+             --title "Kroxylicious release version ${RELEASE_VERSION} development version ${NEXT_VERSION}" \
+             --body "${BODY}" \
+             --repo "$(gh repo set-default -v)" \
+             --label "${RUN_ID_LABEL}"
+
