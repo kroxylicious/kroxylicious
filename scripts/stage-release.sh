4ce573eda276c6f418d441e253127ad0807947b0
Robert Young <robertyoungnz@gmail.com>
2025-08-01 15:07:17 +1200

    bugfix: asset signatures not attached to release
    The 0.13.0 release did not have accompanying .asc files alongside the
    release assets.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- scripts/stage-release.sh
+++ scripts/stage-release.sh
@@ -211,10 +211,10 @@ cp kroxylicious-api/target/japicmp/japicmp.html "${API_COMPATABILITY_REPORT}"
 gh release create --title "${RELEASE_TAG}" \
   --notes-file "${RELEASE_NOTES_DIR}/release-notes_01" \
   --draft "${RELEASE_TAG}" \
-  ./kroxylicious-app/target/kroxylicious-app-*.tar.gz \
-  ./kroxylicious-app/target/kroxylicious-app-*.zip \
-  ./kroxylicious-operator-dist/target/kroxylicious-operator-*.tar.gz \
-  ./kroxylicious-operator-dist/target/kroxylicious-operator-*.zip \
+  ./kroxylicious-app/target/kroxylicious-app-*.tar.gz* \
+  ./kroxylicious-app/target/kroxylicious-app-*.zip* \
+  ./kroxylicious-operator-dist/target/kroxylicious-operator-*.tar.gz* \
+  ./kroxylicious-operator-dist/target/kroxylicious-operator-*.zip* \
   "${API_COMPATABILITY_REPORT}"
 
 
