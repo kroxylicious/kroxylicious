f4bb2c012e0e42d1b114ce47822f2f74d668c8a9
Sam Barker <sam@quadrocket.co.uk>
2025-08-11 15:14:56 +0100

    Unit test the frame logger
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/FrameLoggingHandler.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/FrameLoggingHandler.java
@@ -11,27 +11,39 @@ import io.netty.channel.ChannelPromise;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.internal.logging.InternalLogLevel;
+import io.netty.util.internal.logging.InternalLogger;
+
+import io.kroxylicious.proxy.tag.VisibleForTesting;
 
 public class FrameLoggingHandler extends LoggingHandler {
     private final InternalLogLevel frameLevel;
+    private InternalLogger nettyLogger;
 
     public FrameLoggingHandler(String name, LogLevel frameLevel) {
         super(name);
         this.frameLevel = frameLevel.toInternalLevel();
+        this.nettyLogger = logger;
+    }
+
+    @VisibleForTesting
+    FrameLoggingHandler(String name, LogLevel frameLevel, InternalLogger nettyLogger) {
+        super(name);
+        this.frameLevel = frameLevel.toInternalLevel();
+        this.nettyLogger = nettyLogger;
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (logger.isEnabled(frameLevel)) {
-            logger.log(frameLevel, format(ctx, "READ", msg));
+        if (nettyLogger.isEnabled(frameLevel)) {
+            nettyLogger.log(frameLevel, format(ctx, "READ", msg));
         }
         ctx.fireChannelRead(msg);
     }
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        if (logger.isEnabled(frameLevel)) {
-            logger.log(frameLevel, format(ctx, "WRITE", msg));
+        if (nettyLogger.isEnabled(frameLevel)) {
+            nettyLogger.log(frameLevel, format(ctx, "WRITE", msg));
         }
         ctx.write(msg, promise);
     }
---
+++ kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/internal/FrameLoggingHandlerTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.internal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.util.internal.logging.InternalLogLevel;
+import io.netty.util.internal.logging.InternalLogger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FrameLoggingHandlerTest {
+
+    public static final String FRAME_LOGGER = "FrameLogger";
+
+    @Mock
+    InternalLogger nettyLogger;
+
+    private EmbeddedChannel embeddedChannel;
+
+    @BeforeEach
+    void setUp() {
+        FrameLoggingHandler frameLogger = new FrameLoggingHandler(FRAME_LOGGER, LogLevel.INFO, nettyLogger);
+        embeddedChannel = new EmbeddedChannel(frameLogger);
+    }
+
+    @Test
+    void shouldConstructInstance() {
+        // Given
+
+        // When
+        FrameLoggingHandler frameLoggingHandler = new FrameLoggingHandler(FRAME_LOGGER, LogLevel.INFO);
+
+        // Then
+        assertThat(frameLoggingHandler)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("frameLevel", InternalLogLevel.INFO);
+        // Note the change of type we construct with a LogLevel but assert a InternalLogLevel
+        // I have no clue why they have two different enums.
+    }
+
+    @Test
+    void shouldNotLogReadIfLevelDisabled() throws Exception {
+        // Given
+        when(nettyLogger.isEnabled(InternalLogLevel.INFO)).thenReturn(false);
+
+        // When
+        embeddedChannel.writeInbound("TestMessage");
+
+        // Then
+        verify(nettyLogger).isEnabled(InternalLogLevel.INFO);
+        verifyNoMoreInteractions(nettyLogger);
+    }
+
+    @Test
+    void shouldLogReadIfLevelEnabled() throws Exception {
+        // Given
+        when(nettyLogger.isEnabled(InternalLogLevel.INFO)).thenReturn(true);
+
+        // When
+        embeddedChannel.writeInbound("TestMessage");
+
+        // Then
+        verify(nettyLogger).log(eq(InternalLogLevel.INFO), contains("READ"));
+    }
+
+    @Test
+    void shouldNotLogWriteIfLevelDisabled() throws Exception {
+        // Given
+        when(nettyLogger.isEnabled(InternalLogLevel.INFO)).thenReturn(false);
+
+        // When
+        embeddedChannel.writeOutbound("TestMessage");
+
+        // Then
+        verify(nettyLogger).isEnabled(InternalLogLevel.INFO);
+        verifyNoMoreInteractions(nettyLogger);
+    }
+
+    @Test
+    void shouldLogWriteIfLevelEnabled() throws Exception {
+        // Given
+        when(nettyLogger.isEnabled(InternalLogLevel.INFO)).thenReturn(true);
+
+        // When
+        embeddedChannel.writeOutbound("TestMessage");
+
+        // Then
+        verify(nettyLogger).log(eq(InternalLogLevel.INFO), contains("WRITE"));
+    }
+}
