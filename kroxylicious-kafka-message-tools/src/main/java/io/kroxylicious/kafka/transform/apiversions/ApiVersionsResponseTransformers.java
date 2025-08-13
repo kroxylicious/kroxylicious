5e272f6978f3aff1f7d2c5d985d20cd8b4710237
Robert Young <robertyoungnz@gmail.com>
2025-08-13 12:00:16 +1200

    Add ApiVersionResponseTransformer with implementations
    Why:
    We have three filters that want to implement the same behaviour,
    intercepting api versions to downgrade the maximum version of an RPC.
    The kafka-message-tools module looks like a good place to put these
    stateless, reusable transformations of the RPCs.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

---
+++ kroxylicious-kafka-message-tools/src/main/java/io/kroxylicious/kafka/transform/apiversions/ApiVersionsResponseTransformers.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kafka.transform.apiversions;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.kafka.common.protocol.ApiKeys;
+
+public class ApiVersionsResponseTransformers {
+    private ApiVersionsResponseTransformers() {
+    }
+
+    public static ApiVersionsResponseTransformer removeApiKeys(Set<ApiKeys> keysToRemove) {
+        return new ApiVersionRemover(keysToRemove);
+    }
+
+    public static ApiVersionsResponseTransformer limitMaxVersionForApiKeys(Map<ApiKeys, Short> mapVersionLimits) {
+        return new ApiVersionMaxVersionLimiter(mapVersionLimits);
+    }
+
+    public static ApiVersionsResponseTransformer preventTopicIdBearingProduceRequests() {
+        short lastVersionBeforeTopicIdsIntroducedToProduce = (short) 12;
+        return limitMaxVersionForApiKeys(Map.of(ApiKeys.PRODUCE, lastVersionBeforeTopicIdsIntroducedToProduce));
+    }
+
+}
