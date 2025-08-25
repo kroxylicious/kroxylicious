b1047923256be72b446a75b6e0707cbb18778aea
Keith Wall <kwall@apache.org>
2025-08-25 14:36:50 +0100

    addressing review comments
    Signed-off-by: Keith Wall <kwall@apache.org>

--- kroxylicious-docs-validator/src/test/java/io/kroxylicious/doctools/validator/QuickstartDT.java
+++ kroxylicious-docs-validator/src/test/java/io/kroxylicious/doctools/validator/QuickstartDT.java
@@ -9,7 +9,6 @@ package io.kroxylicious.doctools.validator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.UncheckedIOException;
