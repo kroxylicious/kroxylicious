f8438a8cfe9cec2919f6136e281a073d4e463251
Keith Wall <kwall@apache.org>
2025-08-25 18:09:25 +0100

    refactor: refactor all dockerfiles to pull tini from image.
    Signed-off-by: Keith Wall <kwall@apache.org>

--- scripts/build-operator.sh
+++ scripts/build-operator.sh
@@ -12,5 +12,4 @@ IMAGE_TAG="${IMAGE_TAG:-dev-git-${GIT_HASH}}"
 OPERATOR_PULL_SPEC=$(buildPullSpec "operator")
 info "building operator image (${OPERATOR_PULL_SPEC}) in minikube for commit ${GIT_HASH}"
 KROXYLICIOUS_VERSION=${KROXYLICIOUS_VERSION:-$(mvn org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=project.version -q -DforceStdout)}
-TARGETARCH=${TARGETARCH:-$(uname -m)}
-minikube image build . -f Dockerfile.operator -t "${OPERATOR_PULL_SPEC}" --build-opt=build-arg=KROXYLICIOUS_VERSION="${KROXYLICIOUS_VERSION}" --build-opt=build-arg=TARGETARCH="${TARGETARCH}"
+minikube image build . -f Dockerfile.operator -t "${OPERATOR_PULL_SPEC}" --build-opt=build-arg=KROXYLICIOUS_VERSION="${KROXYLICIOUS_VERSION}"
