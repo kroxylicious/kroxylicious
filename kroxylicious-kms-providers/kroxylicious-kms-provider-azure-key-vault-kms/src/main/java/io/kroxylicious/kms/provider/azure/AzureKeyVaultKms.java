c86e115b8d4efeabafa5a4613ddaebec9428c04d
Robert Young <robertyoungnz@gmail.com>
2025-08-26 16:52:21 +1200

    Add an Azure Key Vault KMS implementation
    We support pluggable KMS implementations for the Record Encryption
    Filter. This adds support for Azure Key Vault and it's related variant
    Managed HSM.
    
    Features:
    1. Support 'RSA' and 'RSA-HSM' key types, wrapping using `RSA-OAEP-256`
       but emit a warning that it is not quantum-resistant.
    2. Support 'oct' and 'oct-HSM' key type with AES-GCM wrapping (will only
       work with Managed HSM)
    3. Support only client credentials oauth flow with Entra using clientId
       + clientSecret. This supports workloads running anywhere.
    4. Support TLS customization of the authentication client and key vault client.
    5. DEK bytes are generated proxy-side with a SecureRandom
    6. Edek stores the keyName, keyVersion, edek.
    7. User will supply tenantId for authentication. It is possible to
       obtain this by making an HTTP request to Key Vault with no
       Authorization header. But it should be an easy thing for users to
       supply.
    
    Auth:
    With the oauth client credentials flow we obtain a Bearer token along
    with a remaining lifetime in seconds (default 1 hour). We share the
    token across all filters for a single Filter definition. When it gets
    near to expiry (1 minute away), then we trigger an asynchronous refresh.
    The established token continues to be used while the refresh is in
    progress.
    
    Example configuration:
    ```yaml
    kms: AzureKeyVault
    kmsConfig:
      keyVaultBaseUrl: https://kv-my-vault-name.vault.azure.net
      tls:
        ... client trust for key vault
      entraIdentity:
         oauthEndpointUrl: https://login.microsoftonline.com // optional defaults to this value
         clientId:
           passwordFile: /path/to/id
         clientSecret:
           passwordFile: /path/to/secret
         tenantId: "abds-1232dsaa"
         scope: https://vault.azure.net/.default // optional, defaults to this value
         tls:
           ... client trust configuration for oauth
    ```
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

---
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-azure-key-vault-kms/src/main/java/io/kroxylicious/kms/provider/azure/AzureKeyVaultKms.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kms.provider.azure;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.random.RandomGenerator;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.kroxylicious.kms.provider.azure.keyvault.GetKeyResponse;
+import io.kroxylicious.kms.provider.azure.keyvault.JsonWebKey;
+import io.kroxylicious.kms.provider.azure.keyvault.KeyVaultClient;
+import io.kroxylicious.kms.provider.azure.keyvault.SupportedKeyType;
+import io.kroxylicious.kms.service.DekPair;
+import io.kroxylicious.kms.service.Kms;
+import io.kroxylicious.kms.service.KmsException;
+import io.kroxylicious.kms.service.Serde;
+import io.kroxylicious.kms.service.UnknownAliasException;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+public class AzureKeyVaultKms implements Kms<WrappingKey, AzureKeyVaultEdek> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AzureKeyVaultKms.class);
+
+    private static final List<String> REQUIRED_OPERATIONS = List.of("wrapKey", "unwrapKey");
+    private static final int AES_256_KEY_SIZE = 32;
+    private static final AzureKeyVaultEdekSerde EDEK_SERDE = new AzureKeyVaultEdekSerde();
+    private final KeyVaultClient client;
+    private final RandomGenerator random;
+
+    public AzureKeyVaultKms(KeyVaultClient client, RandomGenerator random) {
+        this.client = client;
+        this.random = random;
+    }
+
+    @Override
+    public CompletionStage<DekPair<AzureKeyVaultEdek>> generateDekPair(WrappingKey wrappingKey) {
+        LOG.debug("Generating dek pair for key version {}", wrappingKey);
+        byte[] bytes = generateDek();
+        CompletionStage<byte[]> wrap = client.wrap(wrappingKey, bytes);
+        return wrap.handle((wrappedBytes, throwable) -> {
+            if (throwable != null) {
+                throw new KmsException("request to wrap DEK with key " + wrappingKey + " failed", throwable);
+            }
+            else {
+                return new DekPair<>(new AzureKeyVaultEdek(wrappingKey.keyName(), wrappingKey.keyVersion(), wrappedBytes),
+                        recordEncryptionKey(bytes));
+            }
+        });
+    }
+
+    // todo use random bytes from KMS if it is Managed HSM, standard Key Vault we have to generated DEK proxy side
+    private byte[] generateDek() {
+        byte[] bytes = new byte[AES_256_KEY_SIZE];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+
+    @NonNull
+    private static SecretKeySpec recordEncryptionKey(byte[] bytes) {
+        // note that this algorithm is nothing to do with the KMS wrapping algorithm, this is required for the SecretKey
+        // to be usable by the record encryption algorithms.
+        return new SecretKeySpec(bytes, "AES");
+    }
+
+    @Override
+    public CompletionStage<SecretKey> decryptEdek(AzureKeyVaultEdek edek) {
+        LOG.debug("Decrypting dek pair for key version {}", edek);
+        return client.getKey(edek.keyName())
+                .thenCompose(r -> {
+                    Optional<SupportedKeyType> keyType = r.supportedKeyType();
+                    return keyType.map(
+                            supportedKeyType -> unwrap(edek, supportedKeyType)).orElseGet(
+                                    () -> CompletableFuture.failedFuture(
+                                            new KmsException(
+                                                    "key '" + edek.keyName() + "' we are trying to unwrap has type " + r.key().keyType() + " which is not supported")));
+                });
+    }
+
+    private CompletionStage<SecretKey> unwrap(AzureKeyVaultEdek edek, SupportedKeyType supportedKeyType) {
+        return client.unwrap(new WrappingKey(edek.keyName(), edek.keyVersion(), supportedKeyType), edek.edek())
+                .handle((bytes, throwable) -> {
+                    if (throwable != null) {
+                        throw new KmsException("failed to unwrap edek for key '" + edek.keyName() + "'", throwable);
+                    }
+                    else {
+                        return recordEncryptionKey(bytes);
+                    }
+                });
+    }
+
+    @Override
+    public Serde<AzureKeyVaultEdek> edekSerde() {
+        return EDEK_SERDE;
+    }
+
+    /**
+     * For Azure Key Vault we expect the alias to equal a key name.
+     * @param alias The alias
+     * @return a wrapping key for the alias
+     * @throws UnknownAliasException if the named key was not found
+     * @throws KmsException if there are any problems obtaining a key usable for wrapping, like unexpected
+     * response codes, key attributes indicating the key is disabled, key attributes indicating wrapKey and
+     * unwrapKey are not supported operations, and key types that are not supported like EC.
+     */
+    @Override
+    public CompletionStage<WrappingKey> resolveAlias(String alias) {
+        LOG.debug("Resolving alias {}", alias);
+        return client.getKey(alias).handle((response, throwable) -> {
+            if (throwable != null) {
+                if (throwable instanceof UnexpectedHttpStatusCodeException e && e.getStatusCode() == 404) {
+                    throw new UnknownAliasException(alias);
+                }
+                throw new KmsException("failed to check existence of '" + alias + "'", throwable);
+            }
+            else if (response == null) {
+                throw new KmsException("get key returned null for: '" + alias + "'");
+            }
+            else {
+                LOG.debug("resolved alias {} to {}", alias, response);
+                JsonWebKey key = response.key();
+                SupportedKeyType keyType = validateKeyAndExtractType(alias, response, key);
+                if (!keyType.isQuantumResistant()) {
+                    LOG.warn("key {} is of type {} which is not quantum resistant. If you are using Managed HSM consider using an AES key", alias,
+                            response.key().keyType());
+                }
+                return WrappingKey.parse(alias, response.key().keyId(), keyType);
+            }
+        });
+    }
+
+    @NonNull
+    private static SupportedKeyType validateKeyAndExtractType(String alias, GetKeyResponse response, JsonWebKey key) {
+        if (REQUIRED_OPERATIONS.stream().anyMatch(p -> !key.keyOperations().contains(p))) {
+            throw new KmsException("key '" + alias + "' key_ops " + key.keyOperations() + " does not contain all of " + REQUIRED_OPERATIONS);
+        }
+        if (!response.attributes().enabled()) {
+            throw new KmsException("key '" + alias + "' is not enabled");
+        }
+        Optional<SupportedKeyType> supportedKeyType = response.supportedKeyType();
+        if (supportedKeyType.isEmpty()) {
+            throw new KmsException("key '" + alias + "' has an unsupported type " + response.key().keyType());
+        }
+        return supportedKeyType.get();
+    }
+
+}
---
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-azure-key-vault-kms/src/test/java/io/kroxylicious/kms/provider/azure/auth/OauthClientCredentialsTokenServiceTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kms.provider.azure.auth;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import org.assertj.core.api.ThrowableAssertAlternative;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.kroxylicious.kms.httpmock.MockServer.Header;
+import io.kroxylicious.kms.httpmock.MockServer.Request;
+import io.kroxylicious.kms.httpmock.MockServer.Responder;
+import io.kroxylicious.kms.provider.azure.MalformedResponseBodyException;
+import io.kroxylicious.kms.provider.azure.UnexpectedHttpStatusCodeException;
+import io.kroxylicious.kms.provider.azure.config.auth.EntraIdentityConfig;
+import io.kroxylicious.proxy.config.secret.InlinePassword;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+import static io.kroxylicious.kms.httpmock.MockServer.getHttpServer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OauthClientCredentialsTokenServiceTest {
+
+    public static final String TOKEN = "abcdef";
+    public static final String TENANT_ID = "my-tenant";
+    public static final String CLIENT_ID = "id";
+    public static final String CLIENT_SECRET = "secret";
+
+    @TempDir
+    Path tempDir;
+
+    @CsvSource(value = { "https://scope/.default", "null" }, nullValues = "null")
+    @ParameterizedTest
+    void getToken(@Nullable String inputScope) {
+        Instant fixedInstant = Instant.now();
+        Clock clock = Clock.fixed(fixedInstant, ZoneId.of("UTC"));
+        int expirySeconds = 3200;
+        Responder resp = exchange -> {
+            var response = Map.of("token_type", "Bearer", "expires_in", expirySeconds, "access_token", TOKEN);
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            String responseBody = new ObjectMapper().writeValueAsString(response);
+            exchange.sendResponseHeaders(200, responseBody.length());
+            exchange.getResponseBody().write(responseBody.getBytes(StandardCharsets.UTF_8));
+        };
+        try (var server = getHttpServer(resp);
+                OauthClientCredentialsTokenService service = new OauthClientCredentialsTokenService(
+                        new EntraIdentityConfig(server.address(), TENANT_ID, new InlinePassword(CLIENT_ID), new InlinePassword(CLIENT_SECRET), inputScope, null),
+                        clock)) {
+            assertThat(service.getBearerToken().toCompletableFuture()).succeedsWithin(Duration.of(5, ChronoUnit.SECONDS))
+                    .satisfies(token -> {
+                        assertThat(token).isNotNull();
+                        assertThat(token.token()).isEqualTo(TOKEN);
+                        assertThat(token.created()).isEqualTo(fixedInstant);
+                        assertThat(token.expires()).isEqualTo(fixedInstant.plusSeconds(expirySeconds));
+                    });
+            Request request = server.singleReceivedRequest();
+            assertThat(request.method()).isEqualTo("POST");
+            String scope = URLEncoder.encode(inputScope == null ? "https://vault.azure.net/.default" : inputScope, StandardCharsets.UTF_8);
+            assertThat(request.body()).isEqualTo(
+                    "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&scope=" + scope);
+            assertThat(request.headers()).contains(new Header("Content-type", "application/x-www-form-urlencoded"));
+            assertThat(request.requestUri()).isEqualTo(URI.create("/" + TENANT_ID + "/oauth2/v2.0/token"));
+        }
+    }
+
+    static Stream<Arguments> invalidResponse() {
+        return Stream.of(Arguments.argumentSet("unknown server error", (Responder) exchange -> {
+            exchange.sendResponseHeaders(500, 0);
+            exchange.getResponseBody().close();
+        }, (Consumer<ThrowableAssertAlternative<?>>) throwableAssert -> throwableAssert.isInstanceOfSatisfying(UnexpectedHttpStatusCodeException.class, e -> {
+            assertThat(e.getStatusCode()).isEqualTo(500);
+        })),
+                Arguments.argumentSet("redirect", (Responder) exchange -> {
+                    exchange.getResponseHeaders().set("Location", "https://malicious/oauth/");
+                    exchange.sendResponseHeaders(302, 0);
+                    exchange.getResponseBody().close();
+                }, (Consumer<ThrowableAssertAlternative<?>>) throwableAssert -> throwableAssert.isInstanceOfSatisfying(UnexpectedHttpStatusCodeException.class, e -> {
+                    assertThat(e.getStatusCode()).isEqualTo(302);
+                })),
+                Arguments.argumentSet("empty body", (Responder) exchange -> {
+                    exchange.sendResponseHeaders(200, 0);
+                    exchange.getResponseBody().close();
+                }, (Consumer<ThrowableAssertAlternative<?>>) throwableAssert -> throwableAssert.isInstanceOf(MalformedResponseBodyException.class)
+                        .withMessage("response body is null or empty")),
+                Arguments.argumentSet("malformed json", (Responder) exchange -> {
+                    String responseBody = "it aint good JSON";
+                    exchange.sendResponseHeaders(200, responseBody.length());
+                    exchange.getResponseBody().write(responseBody.getBytes(StandardCharsets.UTF_8));
+                }, (Consumer<ThrowableAssertAlternative<?>>) throwableAssert -> throwableAssert.isInstanceOf(MalformedResponseBodyException.class)
+                        .withMessage("failed to decode response body")));
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void invalidResponse(Responder resp, Consumer<ThrowableAssertAlternative<?>> asserter) throws IOException {
+        Instant fixedInstant = Instant.now();
+        Clock clock = Clock.fixed(fixedInstant, ZoneId.of("UTC"));
+        try (var server = getHttpServer(resp);
+                OauthClientCredentialsTokenService service = new OauthClientCredentialsTokenService(
+                        new EntraIdentityConfig(server.address(), TENANT_ID, new InlinePassword(CLIENT_ID), new InlinePassword(CLIENT_SECRET), null, null),
+                        clock)) {
+            ThrowableAssertAlternative<?> causeAssert = assertThat(service.getBearerToken().toCompletableFuture()).failsWithin(
+                    Duration.of(5, ChronoUnit.SECONDS))
+                    .withThrowableThat().isInstanceOf(ExecutionException.class).havingCause();
+            asserter.accept(causeAssert);
+        }
+    }
+
+}
