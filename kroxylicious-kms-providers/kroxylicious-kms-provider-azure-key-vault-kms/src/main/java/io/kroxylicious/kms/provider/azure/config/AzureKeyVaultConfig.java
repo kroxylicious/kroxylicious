f9bfd98ee59664c7f19bb61ffa0b8e261a4abaf7
Robert Young <robertyoungnz@gmail.com>
2025-08-26 16:52:21 +1200

    Add an Azure Key Vault KMS implementation
    We support pluggable KMS implementations for the Record Encryption
    Filter. This adds support for Azure Key Vault and it's related variant
    Managed HSM.
    
    Features:
    1. Support 'RSA' and 'RSA-HSM' key types, wrapping using `RSA-OAEP-256`
       but emit a warning that it is not quantum-resistant.
    2. Support 'oct' and 'oct-HSM' key type with AES-GCM wrapping (will only
       work with Managed HSM)
    3. Support only client credentials oauth flow with Entra using clientId
       + clientSecret. This supports workloads running anywhere.
    4. Support TLS customization of the authentication client and key vault client.
    5. DEK bytes are generated proxy-side with a SecureRandom
    6. Edek stores the keyName, keyVersion, edek.
    7. User will supply tenantId for authentication. It is possible to
       obtain this by making an HTTP request to Key Vault with no
       Authorization header. But it should be an easy thing for users to
       supply.
    
    Auth:
    With the oauth client credentials flow we obtain a Bearer token along
    with a remaining lifetime in seconds (default 1 hour). We share the
    token across all filters for a single Filter definition. When it gets
    near to expiry (1 minute away), then we trigger an asynchronous refresh.
    The established token continues to be used while the refresh is in
    progress.
    
    Example configuration:
    ```yaml
    kms: AzureKeyVault
    kmsConfig:
      keyVaultBaseUrl: https://kv-my-vault-name.vault.azure.net
      tls:
        ... client trust for key vault
      entraIdentity:
         oauthEndpointUrl: https://login.microsoftonline.com // optional defaults to this value
         clientId:
           passwordFile: /path/to/id
         clientSecret:
           passwordFile: /path/to/secret
         tenantId: "abds-1232dsaa"
         scope: https://vault.azure.net/.default // optional, defaults to this value
         tls:
           ... client trust configuration for oauth
    ```
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

---
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-azure-key-vault-kms/src/main/java/io/kroxylicious/kms/provider/azure/config/AzureKeyVaultConfig.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kms.provider.azure.config;
+
+import java.net.URI;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import io.kroxylicious.kms.provider.azure.config.auth.EntraIdentityConfig;
+import io.kroxylicious.proxy.config.tls.Tls;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+@JsonPropertyOrder({ "entraIdentity", "keyVaultBaseUrl", "tls" })
+public record AzureKeyVaultConfig(@JsonProperty(required = true) EntraIdentityConfig entraIdentity,
+                                  @JsonProperty(required = true) String keyVaultBaseUrl,
+                                  @JsonInclude(JsonInclude.Include.NON_NULL) @JsonProperty(value = "tls") @Nullable Tls tls) {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AzureKeyVaultConfig.class);
+
+    public AzureKeyVaultConfig {
+        Objects.requireNonNull(entraIdentity);
+        Objects.requireNonNull(keyVaultBaseUrl);
+        if (!keyVaultBaseUrl.startsWith("https://")) {
+            LOG.warn("keyVaultBaseUrl {} does not begin with https://, production installations should use a secure endpoint", keyVaultBaseUrl);
+        }
+        try {
+            URI.create(keyVaultBaseUrl);
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("vault base url cannot be parsed as a URI", e);
+        }
+    }
+}
---
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-azure-key-vault-kms/src/main/java/io/kroxylicious/kms/provider/azure/keyvault/GetKeyResponse.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kms.provider.azure.keyvault;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record GetKeyResponse(@JsonProperty(required = true) JsonWebKey key, @JsonProperty(required = true) KeyAttributes attributes) {
+    public GetKeyResponse {
+        Objects.requireNonNull(key, "key cannot be null");
+        Objects.requireNonNull(attributes, "attributes cannot be null");
+    }
+
+    /**
+     * @return if this key type is supported, return it's key type, else empty
+     */
+    public Optional<SupportedKeyType> supportedKeyType() {
+        return SupportedKeyType.fromKeyType(key().keyType());
+    }
+}
