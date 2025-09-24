d56511fac93829fdd090655c0651d630590857f7
Tom Bentley <tbentley@redhat.com>
2025-09-24 13:01:51 +1200

    Spoke too soon
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-authorizer-providers/kroxylicious-authorizer-acl/src/test/java/io/kroxylicious/authorizer/provider/acl/FakeTopicResource.java
+++ kroxylicious-authorizer-providers/kroxylicious-authorizer-acl/src/test/java/io/kroxylicious/authorizer/provider/acl/FakeTopicResource.java
@@ -4,7 +4,9 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.kroxylicious.authorizer.provider.acl.simple;
+package io.kroxylicious.authorizer.provider.acl;
+
+import java.util.Set;
 
 import io.kroxylicious.authorizer.service.Operation;
 
@@ -13,5 +15,13 @@ public enum FakeTopicResource implements Operation<FakeTopicResource> {
     CREATE,
     WRITE,
     ALTER,
-    READ
+    READ;
+
+    @Override
+    public Set<FakeTopicResource> implies() {
+        return switch (this) {
+            case READ, WRITE, ALTER -> Set.of(DESCRIBE);
+            default -> Set.of();
+        };
+    }
 }
--- kroxylicious-authorizer-providers/kroxylicious-authorizer-acl/src/test/java/io/kroxylicious/authorizer/provider/acl/TypePatternMatchTest.java
+++ kroxylicious-authorizer-providers/kroxylicious-authorizer-acl/src/test/java/io/kroxylicious/authorizer/provider/acl/TypePatternMatchTest.java
@@ -4,14 +4,12 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.kroxylicious.authorizer.provider.acl.simple;
+package io.kroxylicious.authorizer.provider.acl;
 
 import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
-
-
 
 import static org.assertj.core.api.Assertions.assertThat;
 
