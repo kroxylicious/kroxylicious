399cd9962415096dd0b8564f95b466a500b050b2
Keith Wall <kwall@apache.org>
2025-10-03 12:12:03 +0100

    move state class to top-level. make state records into class so they can have a private ctor. add start state static method
    Signed-off-by: Keith Wall <kwall@apache.org>

---
+++ kroxylicious-filters/kroxylicious-sasl-inspection/src/main/java/io/kroxylicious/filters/sasl/inspection/State.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.filters.sasl.inspection;
+
+/**
+ * State machine used that follows SASL handshaking and authentication exchange between client/server.
+ *
+ * <pre><code>
+ *
+ * START ───→ RequiringHandshakeRequest      AllowingHandshakeRequest ←────╮
+ *                       │                                 │               │
+ *                       │      ╭──────────────────────────╯               │
+ *                       ↓      ↓                                          │
+ *            AwaitingHandshakeResponse                                    │
+ *                       │                                                 │
+ *                       ↓                                                 │
+ *      ╭───→ RequiringAuthenticateRequest                                 │
+ *      │                │                                                 │
+ *      │                ↓                                                 │
+ *      │   AwaitingAuthenticateResponse                                   │
+ *      │                │                                                 │
+ *      ╰────────────────┼─────────────────────────────────────────────────╯
+ *                       ↓
+ *          DisallowingAuthenticateRequest
+ *
+ * </code></pre>
+ *
+ */
+sealed interface State
+        permits State.ExpectingHandshakeRequestState, State.AwaitingAuthenticateResponse, State.AwaitingHandshakeResponse, State.DisallowingAuthenticateRequest,
+        State.RequiringAuthenticateRequest {
+
+    static RequiringHandshakeRequest start() {
+        return new RequiringHandshakeRequest();
+    }
+
+    sealed interface ExpectingHandshakeRequestState extends State permits RequiringHandshakeRequest, AllowingHandshakeRequest {
+        /**
+         * Transition to the next state.
+         *
+         * @param saslObserver sasl observer
+         * @return the awaiting handshake response state.
+         */
+        AwaitingHandshakeResponse nextState(SaslObserver saslObserver);
+    }
+
+    /** A SASL handshake request is required. */
+    final class RequiringHandshakeRequest implements ExpectingHandshakeRequestState {
+        private RequiringHandshakeRequest() {
+        }
+
+        /**
+         * Transition to the next state.
+         *
+         * @param saslObserver sasl observer
+         * @return the awaiting handshake response state.
+         */
+        @Override
+        public AwaitingHandshakeResponse nextState(SaslObserver saslObserver) {
+            return new AwaitingHandshakeResponse(saslObserver, NegotiationType.INITIAL);
+        }
+    }
+
+    /** We're waiting for a SASL handshake response from the server. */
+    final class AwaitingHandshakeResponse implements State {
+        private final SaslObserver saslObserver;
+        private final NegotiationType negotiationType;
+
+        private AwaitingHandshakeResponse(SaslObserver saslObserver, NegotiationType negotiationType) {
+            this.saslObserver = saslObserver;
+            this.negotiationType = negotiationType;
+        }
+
+        /**
+         * Transition to the next state.
+         *
+         * @return the requiring authenticate request state.
+         */
+        public RequiringAuthenticateRequest nextState() {
+            return new RequiringAuthenticateRequest(saslObserver, negotiationType);
+        }
+
+        public SaslObserver saslObserver() {
+            return saslObserver;
+        }
+    }
+
+    /**
+     * A SASL authenticate request is required.
+     */
+    final class RequiringAuthenticateRequest implements State {
+        private final SaslObserver saslObserver;
+        private final NegotiationType negotiationType;
+
+        private RequiringAuthenticateRequest(SaslObserver saslObserver, NegotiationType negotiationType) {
+            this.saslObserver = saslObserver;
+            this.negotiationType = negotiationType;
+        }
+
+        /**
+         * Transition to the next state.
+         *
+         * @param authRequestApiSupportsReauth true if the request indicates the broker/client support KIP-368.
+         * @return the awaiting authenticate request state.
+         */
+        public AwaitingAuthenticateResponse nextState(boolean authRequestApiSupportsReauth) {
+            // Flag indicating if the client and broker supports re-authentication (KIP-368). If this is not the first
+            // authentication request, or the apiVersion is > 0 we know that the client supports reauth.
+            var clientSupportsReauthentication = negotiationType == NegotiationType.REAUTH || authRequestApiSupportsReauth;
+            return new AwaitingAuthenticateResponse(saslObserver, negotiationType, clientSupportsReauthentication);
+        }
+
+        SaslObserver saslObserver() {
+            return saslObserver;
+        }
+
+    }
+
+    /**
+     * We're waiting for a SASL authenticate response from the server
+     */
+    final class AwaitingAuthenticateResponse implements State {
+        private final SaslObserver saslObserver;
+        private final NegotiationType negotiationType;
+        private final boolean clientSupportsReauthentication;
+
+        private AwaitingAuthenticateResponse(SaslObserver saslObserver, NegotiationType negotiationType, boolean clientSupportsReauthentication) {
+            this.saslObserver = saslObserver;
+            this.negotiationType = negotiationType;
+            this.clientSupportsReauthentication = clientSupportsReauthentication;
+        }
+
+        /**
+         * Transition to the next state.
+         * @param saslFinished true if sasl negotiation finished
+         * @return the allowing or disallowing authenticate request state.
+         */
+        State nextState(boolean saslFinished) {
+            if (saslFinished) {
+                if (clientSupportsReauthentication) {
+                    return new AllowingHandshakeRequest();
+                }
+                else {
+                    return new DisallowingAuthenticateRequest();
+                }
+            }
+            else {
+                return new RequiringAuthenticateRequest(saslObserver, negotiationType);
+            }
+        }
+
+        SaslObserver saslObserver() {
+            return saslObserver;
+        }
+    }
+
+    /**
+     * Authentication has been successful and a future SASL authenticate request is allowed for reauthentication.
+     * @see <a href="https://cwiki.apache.org/confluence/display/KAFKA/KIP-368%3A+Allow+SASL+Connections+to+Periodically+Re-Authenticate">KIP-368</a>.
+     */
+    final class AllowingHandshakeRequest implements ExpectingHandshakeRequestState {
+        private AllowingHandshakeRequest() {
+        }
+
+        /**
+         * Transition to the next state.
+         *
+         * @param saslObserver sasl observer
+         * @return the awaiting handshake response state.
+         */
+        @Override
+        public AwaitingHandshakeResponse nextState(SaslObserver saslObserver) {
+            return new AwaitingHandshakeResponse(saslObserver, NegotiationType.REAUTH);
+        }
+
+    }
+
+    /**
+     * Authentication has been successful, but no future SASL authenticate request is allowed.
+     * @see <a href="https://cwiki.apache.org/confluence/display/KAFKA/KIP-368%3A+Allow+SASL+Connections+to+Periodically+Re-Authenticate">KIP-368</a>.
+     */
+    final class DisallowingAuthenticateRequest implements State {
+        private DisallowingAuthenticateRequest() {
+        }
+    }
+
+    enum NegotiationType {
+        // the first SASL negotiation
+        INITIAL,
+        // subsequent SASL negotiations when the client/broker support re-auth per KIP-368
+        REAUTH
+    }
+}
