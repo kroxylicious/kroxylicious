11291de36cc8d43fd9f874c2e4a45b16b2320bb1
Tom Bentley <tbentley@redhat.com>
2025-10-14 14:15:32 +1300

    Unrelated SASL plain termination changes
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

---
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/testplugins/SaslPlainTerminationConfig.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.testplugins;
+
+import java.util.Map;
+
+public record SaslPlainTerminationConfig(Map<String, String> userNameToPassword) {
+}
