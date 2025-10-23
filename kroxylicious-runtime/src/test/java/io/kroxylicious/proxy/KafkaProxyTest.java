b46d249eb79981ca8030dc6a2fa7cbe9d02a0ab1
Sam Barker <sam@quadrocket.co.uk>
2025-10-23 17:21:22 +1300

    Auto close HTTP client
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/KafkaProxyTest.java
+++ kroxylicious-runtime/src/test/java/io/kroxylicious/proxy/KafkaProxyTest.java
@@ -196,11 +196,11 @@ class KafkaProxyTest {
         var configParser = new ConfigParser();
         try (var proxy = new KafkaProxy(configParser, configParser.parseConfiguration(config), Features.defaultFeatures())) {
             proxy.startup();
-
-            var client = HttpClient.newHttpClient();
-            var uri = URI.create("http://localhost:9190/livez");
-            var response = client.send(HttpRequest.newBuilder(uri).GET().build(), HttpResponse.BodyHandlers.discarding());
-            assertThat(response.statusCode()).isEqualTo(200);
+            try (var client = HttpClient.newHttpClient()) {
+                var uri = URI.create("http://localhost:9190/livez");
+                var response = client.send(HttpRequest.newBuilder(uri).GET().build(), HttpResponse.BodyHandlers.discarding());
+                assertThat(response.statusCode()).isEqualTo(200);
+            }
         }
     }
 
