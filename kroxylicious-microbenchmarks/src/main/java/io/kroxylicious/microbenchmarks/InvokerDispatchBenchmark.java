02327bd3f14030cf417ef276b3bf81945a9236ac
Tom Bentley <tbentley@redhat.com>
2025-10-31 10:38:12 +1300

    kroxylicious-microbenchmarks
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-microbenchmarks/src/main/java/io/kroxylicious/microbenchmarks/InvokerDispatchBenchmark.java
+++ kroxylicious-microbenchmarks/src/main/java/io/kroxylicious/microbenchmarks/InvokerDispatchBenchmark.java
@@ -201,6 +201,11 @@ public class InvokerDispatchBenchmark {
         }
 
         @Override
+        public void clientSaslAuthenticationSuccess(String mechanism, Subject subject) {
+
+        }
+
+        @Override
         public void clientSaslAuthenticationFailure(String mechanism, String authorizedId, Exception exception) {
 
         }
@@ -211,8 +216,8 @@ public class InvokerDispatchBenchmark {
         }
 
         @Override
-        public CompletionStage<Subject> authenticatedSubject() {
-            return null;
+        public Subject authenticatedSubject() {
+            return Subject.anonymous();
         }
 
         @Override
