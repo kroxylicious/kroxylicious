71e508b59ceb13d5a2074e2e249b3a551292feb6
Piotr Płaczek <piotrpdev@gmail.com>
2025-11-03 12:33:33 +0000

    move microbenchmark filters to io.kroxylicious.microbenchmarks.filters
    Signed-off-by: Piotr Płaczek <piotrpdev@gmail.com>

--- kroxylicious-microbenchmarks/src/main/java/io/kroxylicious/microbenchmarks/filters/FourInterfaceFilter2.java
+++ kroxylicious-microbenchmarks/src/main/java/io/kroxylicious/microbenchmarks/filters/FourInterfaceFilter2.java
@@ -4,20 +4,20 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.kroxylicious.filters;
+package io.kroxylicious.microbenchmarks.filters;
 
 import java.util.concurrent.CompletionStage;
 
-import org.apache.kafka.common.message.ApiVersionsRequestData;
-import org.apache.kafka.common.message.ApiVersionsResponseData;
+import org.apache.kafka.common.message.FetchRequestData;
+import org.apache.kafka.common.message.FetchResponseData;
 import org.apache.kafka.common.message.ProduceRequestData;
 import org.apache.kafka.common.message.ProduceResponseData;
 import org.apache.kafka.common.message.RequestHeaderData;
 import org.apache.kafka.common.message.ResponseHeaderData;
 import org.openjdk.jmh.infra.Blackhole;
 
-import io.kroxylicious.proxy.filter.ApiVersionsRequestFilter;
-import io.kroxylicious.proxy.filter.ApiVersionsResponseFilter;
+import io.kroxylicious.proxy.filter.FetchRequestFilter;
+import io.kroxylicious.proxy.filter.FetchResponseFilter;
 import io.kroxylicious.proxy.filter.FilterContext;
 import io.kroxylicious.proxy.filter.ProduceRequestFilter;
 import io.kroxylicious.proxy.filter.ProduceResponseFilter;
@@ -26,7 +26,7 @@ import io.kroxylicious.proxy.filter.ResponseFilterResult;
 
 import static io.kroxylicious.microbenchmarks.InvokerDispatchBenchmark.CONSUME_TOKENS;
 
-public class FourInterfaceFilter0 implements ProduceResponseFilter, ProduceRequestFilter, ApiVersionsRequestFilter, ApiVersionsResponseFilter {
+public class FourInterfaceFilter2 implements ProduceResponseFilter, ProduceRequestFilter, FetchRequestFilter, FetchResponseFilter {
 
     @Override
     public CompletionStage<RequestFilterResult> onProduceRequest(short apiVersion, RequestHeaderData header, ProduceRequestData request, FilterContext context) {
@@ -42,15 +42,13 @@ public class FourInterfaceFilter0 implements ProduceResponseFilter, ProduceReque
     }
 
     @Override
-    public CompletionStage<RequestFilterResult> onApiVersionsRequest(short apiVersion, RequestHeaderData header, ApiVersionsRequestData request,
-                                                                     FilterContext context) {
+    public CompletionStage<RequestFilterResult> onFetchRequest(short apiVersion, RequestHeaderData header, FetchRequestData request, FilterContext context) {
         Blackhole.consumeCPU(CONSUME_TOKENS);
         return null;
     }
 
     @Override
-    public CompletionStage<ResponseFilterResult> onApiVersionsResponse(short apiVersion, ResponseHeaderData header, ApiVersionsResponseData response,
-                                                                       FilterContext context) {
+    public CompletionStage<ResponseFilterResult> onFetchResponse(short apiVersion, ResponseHeaderData header, FetchResponseData response, FilterContext context) {
         Blackhole.consumeCPU(CONSUME_TOKENS);
         return null;
     }
