0e549d7b7affb5c6672475a62cc84e617ef429f9
Piotr Płaczek <piotrpdev@gmail.com>
2025-11-04 16:30:42 +0000

    add basic JwsBytebufValidator
    Signed-off-by: Piotr Płaczek <piotrpdev@gmail.com>

---
+++ kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/filter/validation/validators/bytebuf/JwsBytebufValidator.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.proxy.filter.validation.validators.bytebuf;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.kafka.common.record.Record;
+import org.jose4j.jwa.AlgorithmConstraints;
+import org.jose4j.jwk.JsonWebKey;
+import org.jose4j.jwk.JsonWebKeySet;
+import org.jose4j.jwk.VerificationJwkSelector;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.lang.JoseException;
+import org.jose4j.lang.UnresolvableKeyException;
+
+import io.kroxylicious.proxy.filter.validation.validators.Result;
+
+/**
+ * Checks if the {@link ByteBuffer} contains a valid {@link JsonWebSignature}.
+ * <p>
+ * The JWS Signature is validated using the provided {@link JsonWebKeySet} and {@link AlgorithmConstraints}.
+ * </p>
+ *
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc7515">RFC 7515 (JWS)</a>
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc7517">RFC 7517 (JWK)</a>
+ * @see <a href="https://datatracker.ietf.org/doc/html/rfc7518">RFC 7518 (JWA)</a>
+ */
+public class JwsBytebufValidator implements BytebufValidator {
+    private static final String DEFAULT_ERROR_MESSAGE = "Buffer could not be successfully verified using JWS signature";
+
+    private final String jwks;
+    private final AlgorithmConstraints algorithmConstraints;
+
+    /**
+     * Constructor for {@link JwsBytebufValidator}.
+     *
+     * @param jwks Result of {@link JsonWebKeySet#toJson()}.
+     * @param algorithmConstraintType Result of {@link AlgorithmConstraints.ConstraintType#toString()}.
+     * @param algorithms Array of {@link AlgorithmIdentifiers}.
+     *
+     * @see <a href="https://bitbucket.org/b_c/jose4j/wiki/JWS%20Examples">jose4j JWS examples</a>
+     */
+    public JwsBytebufValidator(String jwks, String algorithmConstraintType, String[] algorithms) {
+        this.jwks = jwks;
+        this.algorithmConstraints = new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.valueOf(algorithmConstraintType), algorithms);
+    }
+
+    @Override
+    public CompletionStage<Result> validate(ByteBuffer buffer, Record record, boolean isKey) {
+        Objects.requireNonNull(record);
+        if (buffer == null) {
+            throw new IllegalArgumentException("buffer is null");
+        }
+        if (buffer.remaining() < 1) {
+            throw new IllegalArgumentException("size is less than 1");
+        }
+
+        try {
+            String bufferString = new String(StandardCharsets.UTF_8.decode(buffer).array());
+
+            if (verifySignature(bufferString)) {
+                return Result.VALID_RESULT_STAGE;
+            }
+        }
+        catch (JoseException e) {
+            String message = DEFAULT_ERROR_MESSAGE + (e.getMessage() != null ? ": " + e.getMessage() : "");
+            return CompletableFuture.completedStage(new Result(false, message));
+        }
+
+        return CompletableFuture.completedStage(new Result(false, DEFAULT_ERROR_MESSAGE));
+    }
+
+    private boolean verifySignature(String bufferString) throws JoseException {
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setAlgorithmConstraints(algorithmConstraints);
+
+        jws.setCompactSerialization(bufferString);
+
+        JsonWebKeySet jsonWebKeySet = new JsonWebKeySet(jwks);
+
+        VerificationJwkSelector jwkSelector = new VerificationJwkSelector();
+        JsonWebKey jwk = jwkSelector.select(jws, jsonWebKeySet.getJsonWebKeys());
+
+        if (jwk == null) {
+            throw new UnresolvableKeyException("Could not select valid JWK that matches the algorithm constraints");
+        }
+
+        jws.setKey(jwk.getKey());
+
+        return jws.verifySignature();
+    }
+}
