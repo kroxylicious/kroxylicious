328f537df0614bc8e283b766713cb4f2a298113d
Robert Young <robertyoungnz@gmail.com>
2025-11-07 09:33:11 +1300

    name retriever short-circuit on empty names request
    If a client requests a mapping for the empty set, immediately supply
    them with an empty mapping, using the filter dispatch executor
    associated with the filter (until now we have leant on sendRequest
    to control which thread chained futures will execute on).
    
    Why:
    There is nothing to retrieve, so making a Metadata Request is a waste of
    time and resources.
    
    In some filters it may be convenient to always request topic names, to
    simplify async logic, even if the topic name set is empty.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/testplugins/TopicIdToNameResponseStamper.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/testplugins/TopicIdToNameResponseStamper.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.errors.InvalidRequestException;
 import org.apache.kafka.common.message.RequestHeaderData;
 import org.apache.kafka.common.message.ResponseHeaderData;
 import org.apache.kafka.common.protocol.ApiKeys;
@@ -26,6 +27,7 @@ import org.apache.kafka.common.protocol.types.RawTaggedField;
 import io.kroxylicious.UnknownTaggedFields;
 import io.kroxylicious.proxy.filter.Filter;
 import io.kroxylicious.proxy.filter.FilterContext;
+import io.kroxylicious.proxy.filter.FilterDispatchExecutor;
 import io.kroxylicious.proxy.filter.FilterFactory;
 import io.kroxylicious.proxy.filter.FilterFactoryContext;
 import io.kroxylicious.proxy.filter.RequestFilter;
@@ -57,22 +59,31 @@ public class TopicIdToNameResponseStamper implements FilterFactory<Void, Void> {
 
     @Override
     public Filter createFilter(FilterFactoryContext context, Void initializationData) {
-        return new TopicIdToNameResponseStamperFilter();
+        return new TopicIdToNameResponseStamperFilter(context.filterDispatchExecutor());
     }
 
     static class TopicIdToNameResponseStamperFilter implements RequestFilter, ResponseFilter {
 
+        private final FilterDispatchExecutor filterDispatchExecutor;
         Map<Integer, TopicNameMapping> correlated = new HashMap<>();
+
+        TopicIdToNameResponseStamperFilter(FilterDispatchExecutor filterDispatchExecutor) {
+            this.filterDispatchExecutor = filterDispatchExecutor;
+        }
 
         @Override
         public CompletionStage<RequestFilterResult> onRequest(ApiKeys apiKey, RequestHeaderData header, ApiMessage request, FilterContext context) {
             List<String> list = UnknownTaggedFields.unknownTaggedFieldsToStrings(request, TOPIC_ID_TAG).toList();
             if (list.isEmpty()) {
-                return context.forwardRequest(header, request);
+                return context.requestFilterResultBuilder().errorResponse(header, request, new InvalidRequestException("no topic id tag")).withCloseConnection()
+                        .completed();
             }
 
-            Set<Uuid> uuids = Arrays.stream(list.getFirst().split(",")).map(Uuid::fromString).collect(Collectors.toSet());
+            Set<Uuid> uuids = Arrays.stream(list.getFirst().split(",")).filter(s -> !s.isEmpty()).map(Uuid::fromString).collect(Collectors.toSet());
             return context.topicNames(uuids).thenCompose(topicNames -> {
+                if (!filterDispatchExecutor.isInFilterDispatchThread()) {
+                    throw new IllegalStateException("work chained to topicNames future should execute in filter dispatch thread");
+                }
                 correlated.put(header.correlationId(), topicNames);
                 return context.forwardRequest(header, request);
             });
