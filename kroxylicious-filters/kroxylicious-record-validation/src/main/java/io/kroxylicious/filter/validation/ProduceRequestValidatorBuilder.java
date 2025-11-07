26bd1a4b5f7986783ee7f0cc28b40e12c77ca2ba
Piotr Płaczek <piotrpdev@gmail.com>
2025-11-07 15:55:27 +0000

    add content detached config option
    Signed-off-by: Piotr Płaczek <piotrpdev@gmail.com>

--- kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/filter/validation/ProduceRequestValidatorBuilder.java
+++ kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/filter/validation/ProduceRequestValidatorBuilder.java
@@ -64,7 +64,7 @@ class ProduceRequestValidatorBuilder {
                         config.apicurioGlobalId())));
         valueRule.getJwsSignatureValidationConfig().ifPresent(
                 config -> validators
-                        .add(BytebufValidators.jwsSignatureValidator(config.getJsonWebKeySet(), config.getAlgorithmConstraints(), config.getJwsHeaderName())));
+                        .add(BytebufValidators.jwsSignatureValidator(config.getJsonWebKeySet(), config.getAlgorithmConstraints(), config.getJwsHeaderName(), config.getIsContentDetached())));
 
         return BytebufValidators.chainOf(validators);
     }
