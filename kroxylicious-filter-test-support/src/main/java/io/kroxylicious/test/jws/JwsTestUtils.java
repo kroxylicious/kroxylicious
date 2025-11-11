dcc5a1df4c56a44aed0eb62ade188c5da2a85968
Piotr Płaczek <piotrpdev@gmail.com>
2025-11-11 15:52:19 +0000

    refactor test supports to new JwsTestUtils
    Signed-off-by: Piotr Płaczek <piotrpdev@gmail.com>

---
+++ kroxylicious-filter-test-support/src/main/java/io/kroxylicious/test/jws/JwsTestUtils.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.test.jws;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.jose4j.jwk.JsonWebKeySet;
+import org.jose4j.jwk.PublicJsonWebKey;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwx.HeaderParameterNames;
+import org.jose4j.lang.JoseException;
+
+@SuppressWarnings("java:S2386") // Some constants are used in different packages
+public final class JwsTestUtils {
+    public static final String JWS_HEADER_NAME = "jws";
+
+    public static final String ECDSA_SIGN_JWK_JSON = "{" +
+            "\"kty\": \"EC\"," +
+            "\"d\": \"Tk7qzHNnSBMioAU7NwZ9JugFWmWbUCyzeBRjVcTp_so\"," +
+            "\"key_ops\": [ \"sign\" ]," +
+            "\"alg\": \"ES256\"," +
+            "\"crv\": \"P-256\"," +
+            "\"kid\": \"ECDSA JWK\"," +
+            "\"x\": \"qqeGjWmYZU5M5bBrRw1zqZcbPunoFVxsfaa9JdA0R5I\"," +
+            "\"y\": \"wnoj0YjheNP80XYh1SEvz1-wnKByEoHvb6KrDcjMuWc\"" +
+            "}";
+
+    public static final String MISSING_ECDSA_SIGN_JWK_JSON = "{" +
+            "\"kty\": \"EC\"," +
+            "\"d\": \"-vdgJSAZ-Uorygv-ertyEcVZNacnWLG5DMY6xIYdYIA\"," +
+            "\"key_ops\": [ \"sign\" ]," +
+            "\"alg\": \"ES256\"," +
+            "\"crv\": \"P-256\"," +
+            "\"kid\": \"ECDSA MISSING JWK\"," +
+            "\"x\": \"Y5fPO5Xy0TOVVfWeK3bAKjqcXT-RIiLnox29ACDmHsQ\"," +
+            "\"y\": \"Js0Bt1PlHUrp5wMZXxJc97f8VoCZnuC7Z5pDNO7wNuc\"" +
+            "}";
+
+    public static final String RSA_SIGN_JWK_JSON = "{" +
+            "\"kty\": \"RSA\"," +
+            "\"d\": \"jIHlmSPqAasFF8kfLa6cZ8RY3g-iddn_P2vHhCGR9sXC-zP69TaRoq3ISnrosunAGHe9051VsAx2nFQFQ8Xqpl8CGr2lVov1vLt-43foHlc3XZc4lZ6MsmdPhx_7cq4xMygju8xGZ6bSp6BGDXCQsRNjqZL73SGEAxBX7rZUSV0jdcKYPqy6oAbTjGmhgWEMK8BCd6EfpydRWmrVJmDrGcx5lF_p8DvKNjwv2wEy6Y68KrDlp_nLpWkLdoa9XYiZyYm6SVS2IdIVbAVLzHdOb6XvVbu7aYk_ZREy9gGRdnPqiVHLomVFVJdd6Nisbgb6uAs6-8zfdoX6mSJP2pvIxQ\","
+            +
+            "\"key_ops\": [ \"sign\" ]," +
+            "\"alg\": \"RS256\"," +
+            "\"p\": \"xyPNtl3_2Rx1K1qlXX85U9Q1k75Ml1KOz2VXaqKaazEQ7Rp9RMAwuu0JUyGitQXQ5YKQm30CNZGkT3dlsZLHJNSgaiYPoJv_lQysn_QpezAVB2irTKrRnJCWyhkG30P2_W1c3kjVXbYKIczvYA8WbcHRgFEFhbcsDPbxZ1nvn-c\","
+            +
+            "\"q\": \"wkYuFficFLvp6wsUuF5_OzGnCHwJ6WyrHSHYTIF3iK_dSm4Y99k_lZY9WrICcx_3JVudiDtks5NzoaoQJLhfNgHAK5fcEZMhqdZ_1FebT7a1tJsXdmAQxQTmIhzRIAqe3muiugEuUvwRtX_BpLG7JVRjYlOZyDEMIZWi8A3g4B8\","
+            +
+            "\"e\":\"AQAB\"," +
+            "\"kid\":\"RSA JWK\"," +
+            "\"qi\":\"Xd7v96nYNfZKR7wmMjMmc3QLCMj609NPsmYbeYJSwpUKux37G9KkMzW_K9VJ7XRPPJp00CbtTnkyoxzIgZbFh-U9LZhccaCEuVc_Kei-1oGjyCbTJKvveTzzEGC7UBSKld8SnUtuO8JsDimsZ7-DHJHeZi-tXfNTuwdIr5cbJZA\","
+            +
+            "\"dp\":\"IIcvpfdKwFsOpItE8bXDVncWXVC7UAhzPVtPYSK4WIQGQMSP67f8_buUR1j6K9mMWsDuAAf2YWutzDEzkkLodpKotU4MRW7V27HbTLFkSTP8a15khLxuSsWva8mUvslqQdEoV0LMX2dJ1mWUQDuWrUz4fJ4_aa0W6_M2UWx2YMc\","
+            +
+            "\"dq\":\"vSheCRCG0H1jNnMUmquP0E_5Jf64G-qt9XCVzXAlthYeLjFi6DhEe97MIHnAft2540r_6LyDwYGpjdgrXcWTFt-_f_Kd2RLcLSToVBV06Lmq1I5J2v2QdnTdqotKZ5tPsps010z9ENnUWFdrcXOIF8HB_uQNkOmIuU6cVoX81ds\","
+            +
+            "\"n\":\"lx-5h_lkVJGc8-NxgKGkfxobBlM7DDCjqAloEieCf8c9KbPJ12V0Bm8arOX-lm0wRSLxmWzQ3NTM6S9pDbSc7fQt77THlMD1zEDMwIAJxfoMt3U_VrMHVdiOvO6YpU3jWBp7BfQNYHJjCSiaxIHqDPuDCh2GTflkU32Nfim7W3iLBuH4_K8ADYHz3QdeX29vzl49PDbUiiJEsjnEconsfuYjrFaN3uGn41mGzjedc7oxlcID8fDxq5bvZvOFBAIqdrxs5qPYw1QlVupB3LT0AMU1qKWOB_vkM1n54eDIxP9Xd__WnvX4wagYqA1OZ9LPBenhBX7_Rbnrap2QNQ58-Q\""
+            +
+            "}";
+
+    public static final JsonWebKeySet ECDSA_SIGN_JWKS;
+    public static final JsonWebKeySet ECDSA_VERIFY_JWKS;
+
+    public static final JsonWebKeySet MISSING_ECDSA_SIGN_JWKS;
+
+    public static final JsonWebKeySet RSA_SIGN_JWKS;
+    public static final JsonWebKeySet RSA_VERIFY_JWKS;
+
+    public static final JsonWebKeySet RSA_AND_ECDSA_VERIFY_JWKS;
+
+    static {
+        try {
+            // ECDSA
+            PublicJsonWebKey ecdsaSignJwk = PublicJsonWebKey.Factory.newPublicJwk(ECDSA_SIGN_JWK_JSON);
+            ECDSA_SIGN_JWKS = new JsonWebKeySet(ecdsaSignJwk);
+
+            // Note: kid cannot change https://bitbucket.org/b_c/jose4j/src/jose4j-0.9.6/src/main/java/org/jose4j/jwk/SimpleJwkFilter.java#lines-103
+            PublicJsonWebKey ecdsaVerifyJwk = PublicJsonWebKey.Factory.newPublicJwk(ECDSA_SIGN_JWK_JSON);
+            ecdsaVerifyJwk.setKeyOps(List.of("verify"));
+            ecdsaVerifyJwk.setPrivateKey(null);
+            ECDSA_VERIFY_JWKS = new JsonWebKeySet(ecdsaVerifyJwk);
+
+            // Missing ECDSA
+            PublicJsonWebKey missingEcdsaJwk = PublicJsonWebKey.Factory.newPublicJwk(MISSING_ECDSA_SIGN_JWK_JSON);
+            MISSING_ECDSA_SIGN_JWKS = new JsonWebKeySet(missingEcdsaJwk);
+
+            // RSA
+            PublicJsonWebKey rsaSignJwk = PublicJsonWebKey.Factory.newPublicJwk(RSA_SIGN_JWK_JSON);
+            RSA_SIGN_JWKS = new JsonWebKeySet(rsaSignJwk);
+
+            // Note: kid cannot change https://bitbucket.org/b_c/jose4j/src/jose4j-0.9.6/src/main/java/org/jose4j/jwk/SimpleJwkFilter.java#lines-103
+            PublicJsonWebKey rsaVerifyJwk = PublicJsonWebKey.Factory.newPublicJwk(RSA_SIGN_JWK_JSON);
+            rsaVerifyJwk.setKeyOps(List.of("verify"));
+            rsaVerifyJwk.setPrivateKey(null);
+            RSA_VERIFY_JWKS = new JsonWebKeySet(rsaVerifyJwk);
+
+            // RSA and ECDSA
+            RSA_AND_ECDSA_VERIFY_JWKS = new JsonWebKeySet(rsaVerifyJwk, ecdsaVerifyJwk);
+        }
+        catch (JoseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private JwsTestUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String VALID_JWS_USING_ECDSA_JWK_PAYLOAD = "Message signed with JWK";
+    public static final byte[] VALID_JWS_USING_ECDSA_JWK = generateJws(ECDSA_SIGN_JWKS, VALID_JWS_USING_ECDSA_JWK_PAYLOAD, false, false)
+            .getBytes(StandardCharsets.UTF_8);
+
+    public static final String VALID_JWS_USING_MISSING_ECDSA_JWK_PAYLOAD = "Message signed with missing JWK";
+    public static final byte[] VALID_JWS_USING_MISSING_ECDSA_JWK = generateJws(MISSING_ECDSA_SIGN_JWKS, VALID_JWS_USING_MISSING_ECDSA_JWK_PAYLOAD, false, false)
+            .getBytes(StandardCharsets.UTF_8);
+
+    public static final String VALID_JWS_USING_RSA_JWK_PAYLOAD = "Message signed with RSA JWK";
+    public static final byte[] VALID_JWS_USING_RSA_JWK = generateJws(RSA_SIGN_JWKS, VALID_JWS_USING_RSA_JWK_PAYLOAD, false, false).getBytes(StandardCharsets.UTF_8);
+
+    public static final String VALID_JWS_USING_ECDSA_JWK_AND_CONTENT_DETACHED_PAYLOAD = "Message signed with JWK that will be content detached";
+    public static final byte[] VALID_JWS_USING_ECDSA_JWK_AND_CONTENT_DETACHED = generateJws(ECDSA_SIGN_JWKS, VALID_JWS_USING_ECDSA_JWK_AND_CONTENT_DETACHED_PAYLOAD,
+            true,
+            false)
+            .getBytes(StandardCharsets.UTF_8);
+
+    public static final String VALID_JWS_USING_ECDSA_JWK_AND_UNENCODED_CONTENT_DETACHED_PAYLOAD = "Message signed with JWK that will be content detached and unencoded";
+    public static final byte[] VALID_JWS_USING_ECDSA_JWK_AND_UNENCODED_CONTENT_DETACHED = generateJws(ECDSA_SIGN_JWKS,
+            VALID_JWS_USING_ECDSA_JWK_AND_UNENCODED_CONTENT_DETACHED_PAYLOAD, true, true)
+            .getBytes(StandardCharsets.UTF_8);
+
+    public static final byte[] INVALID_JWS = "This is a non JWS value".getBytes(StandardCharsets.UTF_8);
+
+    public static String generateJws(JsonWebKeySet jwks, String payload, boolean isContentDetached, boolean useUnencodedPayload) {
+        try {
+            PublicJsonWebKey jwk = (PublicJsonWebKey) jwks.getJsonWebKeys().get(0);
+
+            JsonWebSignature jws = new JsonWebSignature();
+            jws.setKeyIdHeaderValue(jwk.getKeyId());
+            jws.setAlgorithmHeaderValue(jwk.getAlgorithm());
+            jws.setKey(jwk.getPrivateKey());
+            jws.setPayload(payload);
+
+            if (useUnencodedPayload) {
+                jws.setHeader(HeaderParameterNames.BASE64URL_ENCODE_PAYLOAD, false);
+                jws.setCriticalHeaderNames(HeaderParameterNames.BASE64URL_ENCODE_PAYLOAD);
+            }
+
+            return isContentDetached ? jws.getDetachedContentCompactSerialization() : jws.getCompactSerialization();
+        }
+        catch (JoseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
