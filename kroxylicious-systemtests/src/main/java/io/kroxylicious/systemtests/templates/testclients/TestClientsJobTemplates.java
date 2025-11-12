c9185a55b1c96abeab93180bae3ea2fe3fe934e0
Francisco Vila <57452611+franvila@users.noreply.github.com>
2025-11-12 07:06:23 +0100

    Update kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/templates/testclients/TestClientsJobTemplates.java
    Co-authored-by: Sam Barker <sam@quadrocket.co.uk>
    Signed-off-by: Francisco Vila <57452611+franvila@users.noreply.github.com>

--- kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/templates/testclients/TestClientsJobTemplates.java
+++ kroxylicious-systemtests/src/main/java/io/kroxylicious/systemtests/templates/testclients/TestClientsJobTemplates.java
@@ -238,7 +238,7 @@ public class TestClientsJobTemplates {
             envVars.add(envVar(MESSAGE_KEY_VAR, messageKey));
         }
         List<String> additionalConfig = new ArrayList<>();
-        if (!compressionType.equals(CompressionType.NONE)) {
+        if (!CompressionType.NONE.equals(compressionTypex)) {
             additionalConfig.add(ProducerConfig.COMPRESSION_TYPE_CONFIG + "=" + compressionType.name);
         }
         envVars.add(envVar(ADDITIONAL_CONFIG_VAR, String.join("\n", additionalConfig)));
