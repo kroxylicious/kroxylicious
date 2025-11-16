cabf1b092f0c35e2538c555d52b842b5335d5b08
Sam Barker <sam@quadrocket.co.uk>
2025-11-17 10:45:26 +1300

    Drop exploratory code to trigger snappy loading eagerly.
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -5,7 +5,6 @@
  */
 package io.kroxylicious.proxy;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +20,6 @@ import java.util.stream.Stream;
 import org.apache.kafka.common.protocol.ApiKeys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xerial.snappy.Snappy;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBufAllocator;
@@ -146,12 +144,6 @@ public final class KafkaProxy implements AutoCloseable {
         this.virtualClusterModels = config.virtualClusterModel(pfr);
         this.managementConfiguration = config.management();
         this.micrometerConfig = config.getMicrometer();
-        try {
-            byte[] compressed = Snappy.compress("wibble");
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @VisibleForTesting
