239496208904ab9723f5f145cfd0faa29a9d559b
Sam Barker <sam@quadrocket.co.uk>
2025-11-18 17:27:30 +1300

    Allow users to override library path
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-app/src/assembly/kroxylicious-start.sh
+++ kroxylicious-app/src/assembly/kroxylicious-start.sh
@@ -30,7 +30,6 @@ classpath() {
 # Disable warnings about `local` variables
 # shellcheck disable=SC3043
 native_library_path() {
-  NATIVE_LIB_BASE_DIR="/opt/kroxylicious/libs/native/"
   local lib_path="${1}"
   arch="${TARGETARCH:-$(uname -m)}"
   native_lib="${NATIVE_LIB_BASE_DIR}${lib_path}/${TARGETOS:-linux}"
@@ -60,18 +59,16 @@ if [ "${KROXYLICIOUS_LOGGING_OPTIONS+set}" != set ]; then
   KROXYLICIOUS_LOGGING_OPTIONS="-Dlog4j2.configurationFile=$(script_dir)/../config/log4j2.yaml -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 fi
 
+NATIVE_LIB_BASE_DIR=${NATIVE_LIB_BASE_DIR:-"/opt/kroxylicious/libs/native/"}
 NETTY_NATIVE_LIB=$(native_library_path netty)
 LZ4_NATIVE_LIB=$(native_library_path lz4-java/net/jpountz/util)
 SNAPPY_NATIVE_LIB=$(native_library_path snappy/org/xerial/snappy/native)
 ZSTD_NATIVE_LIB=$(native_library_path zstd-jni)
-NATIVE_LIB_PATH="${NETTY_NATIVE_LIB}:${LZ4_NATIVE_LIB}:${SNAPPY_NATIVE_LIB}:${ZSTD_NATIVE_LIB}"
-
-ls -l "${NETTY_NATIVE_LIB}"
-
+NATIVE_LIB_PATH="${NATIVE_LIB_PATH:-${NETTY_NATIVE_LIB}:${LZ4_NATIVE_LIB}:${SNAPPY_NATIVE_LIB}:${ZSTD_NATIVE_LIB}}"
 echo "setting java.library.path=${NATIVE_LIB_PATH}"
-NATIVE_LIB_OPTIONS="-Djava.library.path=${NATIVE_LIB_PATH} -Dorg.xerial.snappy.disable.bundled.libs=true"
+NATIVE_LIB_OPTIONS=${NATIVE_LIB_OPTIONS:-"-Djava.library.path=${NATIVE_LIB_PATH} -Dorg.xerial.snappy.disable.bundled.libs=true"}
 
-export JAVA_OPTIONS="${KROXYLICIOUS_LOGGING_OPTIONS} ${NATIVE_LIB_OPTIONS} ${JAVA_OPTIONS:-}"
+export JAVA_OPTIONS="${KROXYLICIOUS_LOGGING_OPTIONS:-} ${NATIVE_LIB_OPTIONS:-} ${JAVA_OPTIONS:-}"
 JAVA_CLASSPATH="$(classpath)"
 export JAVA_CLASSPATH
 export JAVA_MAIN_CLASS=io.kroxylicious.app.Kroxylicious
