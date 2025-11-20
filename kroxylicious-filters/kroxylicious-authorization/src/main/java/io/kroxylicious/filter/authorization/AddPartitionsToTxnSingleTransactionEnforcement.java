8051ecd60bb3b8df4b5e9e5f39f7e8e95d903fd2
Tom Bentley <tbentley@redhat.com>
2025-11-21 10:33:16 +1300

    Add authorization for the Consumer APIs
    Co-authored-by: Robert Young <robertyoungnz@gmail.com>
    Signed-off-by: Tom Bentley <tbentley@redhat.com>
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

---
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AddPartitionsToTxnSingleTransactionEnforcement.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.filter.authorization;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
+
+import org.apache.kafka.common.message.AddPartitionsToTxnRequestData;
+import org.apache.kafka.common.message.AddPartitionsToTxnResponseData;
+import org.apache.kafka.common.message.AddPartitionsToTxnResponseData.AddPartitionsToTxnPartitionResult;
+import org.apache.kafka.common.message.AddPartitionsToTxnResponseData.AddPartitionsToTxnTopicResult;
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.protocol.Errors;
+
+import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.authorizer.service.AuthorizeResult;
+import io.kroxylicious.proxy.filter.FilterContext;
+import io.kroxylicious.proxy.filter.RequestFilterResult;
+
+public class AddPartitionsToTxnSingleTransactionEnforcement extends ApiEnforcement<AddPartitionsToTxnRequestData, AddPartitionsToTxnResponseData> {
+    @Override
+    short minSupportedVersion() {
+        return 0;
+    }
+
+    @Override
+    short maxSupportedVersion() {
+        return 3;
+    }
+
+    @Override
+    CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header,
+                                                   AddPartitionsToTxnRequestData request,
+                                                   FilterContext context,
+                                                   AuthorizationFilter authorizationFilter) {
+        List<Action> actions = request.v3AndBelowTopics().stream().map(s -> new Action(TopicResource.WRITE, s.name())).toList();
+        CompletionStage<AuthorizeResult> authorization = authorizationFilter.authorization(context, actions);
+        return authorization.thenCompose(result -> {
+            if (result.denied().isEmpty()) {
+                return context.forwardRequest(header, request);
+            }
+            else {
+                // add partitions succeeds or fails atomically for a transaction, allowed topics fail with OPERATION_NOT_ATTEMPTED
+                Set<String> deniedTopics = result.denied().stream().map(Action::resourceName).collect(Collectors.toSet());
+                AddPartitionsToTxnResponseData response = new AddPartitionsToTxnResponseData();
+                List<AddPartitionsToTxnTopicResult> topicResults = request.v3AndBelowTopics().stream().map(topic -> {
+                    AddPartitionsToTxnTopicResult txnTopicResult = new AddPartitionsToTxnTopicResult();
+                    txnTopicResult.setName(topic.name());
+                    topic.partitions().forEach(partition -> {
+                        AddPartitionsToTxnPartitionResult partitionResult = new AddPartitionsToTxnPartitionResult();
+                        partitionResult.setPartitionIndex(partition);
+                        Errors error = deniedTopics.contains(topic.name()) ? Errors.TOPIC_AUTHORIZATION_FAILED : Errors.OPERATION_NOT_ATTEMPTED;
+                        partitionResult.setPartitionErrorCode(error.code());
+                        txnTopicResult.resultsByPartition().add(partitionResult);
+                    });
+                    return txnTopicResult;
+                }).toList();
+                // results are reversed in the real implementation for some reason
+                for (int i = topicResults.size() - 1; i >= 0; i--) {
+                    response.resultsByTopicV3AndBelow().mustAdd(topicResults.get(i));
+                }
+
+                return context.requestFilterResultBuilder().shortCircuitResponse(response).completed();
+            }
+        });
+    }
+}
---
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/ListOffsetsEnforcement.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.filter.authorization;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.kafka.common.message.ListOffsetsRequestData;
+import org.apache.kafka.common.message.ListOffsetsRequestData.ListOffsetsTopic;
+import org.apache.kafka.common.message.ListOffsetsResponseData;
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.protocol.Errors;
+import org.apache.kafka.common.requests.ListOffsetsResponse;
+
+import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.authorizer.service.Decision;
+import io.kroxylicious.proxy.filter.FilterContext;
+import io.kroxylicious.proxy.filter.RequestFilterResult;
+
+class ListOffsetsEnforcement extends ApiEnforcement<ListOffsetsRequestData, ListOffsetsResponseData> {
+
+    short minSupportedVersion() {
+        return 1;
+    }
+
+    short maxSupportedVersion() {
+        return 10;
+    }
+
+    @Override
+    CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header,
+                                                   ListOffsetsRequestData request,
+                                                   FilterContext context,
+                                                   AuthorizationFilter authorizationFilter) {
+        List<Action> actions = request.topics().stream().map(ListOffsetsTopic::name)
+                .map(s -> new Action(TopicResource.DESCRIBE, s)).toList();
+        return authorizationFilter.authorization(context, actions)
+                .thenCompose(authorization -> {
+                    Map<Decision, List<ListOffsetsTopic>> topicDescribeDecisions = authorization.partition(request.topics(),
+                            TopicResource.DESCRIBE,
+                            ListOffsetsTopic::name);
+                    List<ListOffsetsTopic> allowedTopics = topicDescribeDecisions.getOrDefault(Decision.ALLOW, List.of());
+                    if (allowedTopics.isEmpty()) {
+                        // Shortcircuit if there's no allowed topics
+                        ListOffsetsResponseData response = new ListOffsetsResponseData();
+                        var topics = createDenyTopicResponses(topicDescribeDecisions);
+                        response.setTopics(topics);
+                        return context.requestFilterResultBuilder()
+                                .shortCircuitResponse(response)
+                                .completed();
+                    }
+                    else if (topicDescribeDecisions.getOrDefault(Decision.DENY, List.of()).isEmpty()) {
+                        // Just forward if there's no denied topics
+                        return context.forwardRequest(header, request);
+                    }
+                    else {
+                        var topics = createDenyTopicResponses(topicDescribeDecisions);
+                        request.setTopics(allowedTopics);
+                        authorizationFilter.pushInflightState(header,
+                                (ListOffsetsResponseData response) -> {
+                                    response.topics().addAll(topics);
+                                    return response;
+                                });
+                        return context.forwardRequest(header, request);
+                    }
+                });
+    }
+
+    private List<ListOffsetsResponseData.ListOffsetsTopicResponse> createDenyTopicResponses(Map<Decision, List<ListOffsetsTopic>> topicDescribeDecisions) {
+        List<ListOffsetsTopic> listOffsetsTopics = topicDescribeDecisions.get(Decision.DENY);
+        return listOffsetsTopics.stream().map(t -> {
+            List<ListOffsetsResponseData.ListOffsetsPartitionResponse> partitionResponses = t.partitions().stream()
+                    .map(listOffsetsPartition -> new ListOffsetsResponseData.ListOffsetsPartitionResponse()
+                            .setPartitionIndex(listOffsetsPartition.partitionIndex())
+                            .setErrorCode(Errors.TOPIC_AUTHORIZATION_FAILED.code())
+                            .setTimestamp(ListOffsetsResponse.UNKNOWN_TIMESTAMP)
+                            .setOffset(ListOffsetsResponse.UNKNOWN_OFFSET))
+                    .toList();
+            ListOffsetsResponseData.ListOffsetsTopicResponse responseTopic = new ListOffsetsResponseData.ListOffsetsTopicResponse();
+            responseTopic.setName(t.name());
+            responseTopic.setPartitions(partitionResponses);
+            return responseTopic;
+        }).toList();
+    }
+}
