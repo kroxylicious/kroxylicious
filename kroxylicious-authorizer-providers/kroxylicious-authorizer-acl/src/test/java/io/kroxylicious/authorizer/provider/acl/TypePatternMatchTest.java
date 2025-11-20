f5f00b9cd7ec11dfb7ee0d5a80c9b4ab351d417f
Tom Bentley <tbentley@redhat.com>
2025-11-20 17:09:02 +1300

    Rob's case
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-authorizer-providers/kroxylicious-authorizer-acl/src/test/java/io/kroxylicious/authorizer/provider/acl/TypePatternMatchTest.java
+++ kroxylicious-authorizer-providers/kroxylicious-authorizer-acl/src/test/java/io/kroxylicious/authorizer/provider/acl/TypePatternMatchTest.java
@@ -23,12 +23,13 @@ class TypePatternMatchTest {
         var m = new TypePatternMatch();
         m.add(new ResourceMatcherNameMatches<>(FakeTopicResource.class, Pattern.compile("baz*")), Set.of(FakeTopicResource.CREATE));
         m.add(new ResourceMatcherNameMatches<>(FakeTopicResource.class, Pattern.compile("baz*")), Set.of(FakeTopicResource.ALTER));
+        m.add(new ResourceMatcherNameMatches<>(FakeTopicResource.class, Pattern.compile("bazz")), Set.of(FakeTopicResource.DESCRIBE));
         m.add(new ResourceMatcherNameMatches<>(FakeClusterResource.class, Pattern.compile("foo*")), Set.of(FakeClusterResource.CONNECT));
 
         assertThat(m.matchingOperations(FakeTopicResource.class, "absent")).isNull();
         assertThat(m.matchingOperations(FakeTopicResource.class, "ba")).isEqualTo(Set.of(FakeTopicResource.CREATE, FakeTopicResource.ALTER));
         assertThat(m.matchingOperations(FakeTopicResource.class, "baz")).isEqualTo(Set.of(FakeTopicResource.CREATE, FakeTopicResource.ALTER));
-        assertThat(m.matchingOperations(FakeTopicResource.class, "bazz")).isEqualTo(Set.of(FakeTopicResource.CREATE, FakeTopicResource.ALTER));
+        assertThat(m.matchingOperations(FakeTopicResource.class, "bazz")).isEqualTo(Set.of(FakeTopicResource.CREATE, FakeTopicResource.ALTER, FakeTopicResource.DESCRIBE));
 
         assertThat(m.matchingOperations(FakeClusterResource.class, "absent")).isNull();
         assertThat(m.matchingOperations(FakeClusterResource.class, "fo")).isEqualTo(Set.of(FakeClusterResource.CONNECT));
