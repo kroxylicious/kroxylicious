91e2053422d642f20f468b4590e18396570ed279
Tom Bentley <tbentley@redhat.com>
2025-11-21 14:34:21 +1300

    Add authorization for the admin client apis

---
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AlterConfigsEnforcement.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.filter.authorization;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.kafka.common.message.AlterConfigsRequestData;
+import org.apache.kafka.common.message.AlterConfigsRequestData.AlterConfigsResource;
+import org.apache.kafka.common.message.AlterConfigsResponseData;
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.protocol.Errors;
+import org.apache.kafka.common.resource.ResourceType;
+
+import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.authorizer.service.Decision;
+import io.kroxylicious.proxy.filter.FilterContext;
+import io.kroxylicious.proxy.filter.RequestFilterResult;
+
+import static org.apache.kafka.common.resource.ResourceType.TOPIC;
+
+public class AlterConfigsEnforcement extends ApiEnforcement<AlterConfigsRequestData, AlterConfigsResponseData> {
+    @Override
+    short minSupportedVersion() {
+        return 0;
+    }
+
+    @Override
+    short maxSupportedVersion() {
+        return 2;
+    }
+
+    @Override
+    CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header, AlterConfigsRequestData request, FilterContext context,
+                                                   AuthorizationFilter authorizationFilter) {
+        List<AlterConfigsResource> topicRequests = request.resources().stream()
+                .filter(resource -> ResourceType.fromCode(resource.resourceType()) == TOPIC).toList();
+        if (topicRequests.isEmpty()) {
+            return context.forwardRequest(header, request);
+        }
+        else {
+            List<Action> actions = topicRequests.stream()
+                    .map(re -> new Action(TopicResource.ALTER_CONFIGS, re.resourceName())).toList();
+            return authorizationFilter.authorization(context, actions).thenCompose(result -> {
+                Map<Decision, List<AlterConfigsResource>> partitioned = result.partition(topicRequests, TopicResource.ALTER_CONFIGS,
+                        AlterConfigsResource::resourceName);
+                List<AlterConfigsResource> denied = partitioned.get(Decision.DENY);
+                if (denied.isEmpty()) {
+                    return context.forwardRequest(header, request);
+                }
+                else {
+                    request.resources().removeAll(denied);
+                    authorizationFilter.pushInflightState(header, (AlterConfigsResponseData d) -> {
+                        denied.forEach(describeConfigsResource -> {
+                            AlterConfigsResponseData.AlterConfigsResourceResponse configResult = new AlterConfigsResponseData.AlterConfigsResourceResponse();
+                            configResult.setErrorCode(Errors.TOPIC_AUTHORIZATION_FAILED.code());
+                            configResult.setErrorMessage(Errors.TOPIC_AUTHORIZATION_FAILED.message());
+                            configResult.setResourceName(describeConfigsResource.resourceName());
+                            configResult.setResourceType(describeConfigsResource.resourceType());
+                            d.responses().add(configResult);
+                        });
+                        return d;
+                    });
+                    return context.forwardRequest(header, request);
+                }
+            });
+        }
+    }
+}
---
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/CreatePartitionsEnforcement.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.filter.authorization;
+
+import java.util.concurrent.CompletionStage;
+
+import org.apache.kafka.common.message.CreatePartitionsRequestData;
+import org.apache.kafka.common.message.CreatePartitionsResponseData;
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.message.ResponseHeaderData;
+import org.apache.kafka.common.protocol.Errors;
+
+import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.authorizer.service.Decision;
+import io.kroxylicious.proxy.filter.FilterContext;
+import io.kroxylicious.proxy.filter.RequestFilterResult;
+
+public class CreatePartitionsEnforcement extends ApiEnforcement<CreatePartitionsRequestData, CreatePartitionsResponseData> {
+
+    @Override
+    short minSupportedVersion() {
+        return 0;
+    }
+
+    @Override
+    short maxSupportedVersion() {
+        return 3;
+    }
+
+    CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header,
+                                                   CreatePartitionsRequestData request,
+                                                   FilterContext context,
+                                                   AuthorizationFilter authorizationFilter) {
+        var actions = request.topics().stream()
+                .map(cpd -> new Action(TopicResource.ALTER, cpd.name()))
+                .toList();
+        return authorizationFilter.authorization(context, actions)
+                .thenCompose(authorization -> {
+                    var decisions = authorization.partition(request.topics(),
+                            TopicResource.ALTER,
+                            CreatePartitionsRequestData.CreatePartitionsTopic::name);
+                    if (decisions.get(Decision.ALLOW).isEmpty()) {
+                        // Shortcircuit if there's no allowed topics
+                        var creatableTopics = decisions.get(Decision.DENY).stream()
+                                .map(CreatePartitionsEnforcement::topicAuthzFailed)
+                                .toList();
+                        return context.requestFilterResultBuilder().shortCircuitResponse(
+                                new ResponseHeaderData().setCorrelationId(header.correlationId()),
+                                new CreatePartitionsResponseData().setResults(creatableTopics)).completed();
+                    }
+                    else if (decisions.get(Decision.DENY).isEmpty()) {
+                        // Just forward if there's no denied topics
+                        return context.forwardRequest(header, request);
+                    }
+                    else {
+                        var topicCollection = new CreatePartitionsRequestData.CreatePartitionsTopicCollection();
+                        for (var topic : decisions.get(Decision.ALLOW)) {
+                            topicCollection.mustAdd(topic.duplicate());
+                        }
+                        request.setTopics(topicCollection);
+                        var creatableTopicResults = decisions.get(Decision.DENY)
+                                .stream().map(CreatePartitionsEnforcement::topicAuthzFailed)
+                                .toList();
+                        authorizationFilter.pushInflightState(header, (CreatePartitionsResponseData response) -> {
+                            response.results().addAll(creatableTopicResults);
+                            return response;
+                        });
+                        return context.forwardRequest(header, request);
+                    }
+                });
+    }
+
+    static CreatePartitionsResponseData.CreatePartitionsTopicResult topicAuthzFailed(CreatePartitionsRequestData.CreatePartitionsTopic t) {
+        return new CreatePartitionsResponseData.CreatePartitionsTopicResult()
+                .setName(t.name())
+                .setErrorCode(Errors.TOPIC_AUTHORIZATION_FAILED.code());
+    }
+}
