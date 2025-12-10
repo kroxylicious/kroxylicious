df2c5a3ca93579e37387ed5972bbb216d5aba041
Keith Wall <kwall@apache.org>
2025-12-10 10:04:03 +0000

    Fix for #3035 and new ITs demonstrating the virtual cluster/sasl inspection subject builder working end-to-end (#3036)
    * fix(sasl inspection): add missing annotations that cause #3035
    * Add unit tests for sasl inspector config deserialization
    
    Signed-off-by: Keith Wall <kwall@apache.org>
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>
    Co-authored-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/BaseIT.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/proxy/BaseIT.java
@@ -6,25 +6,32 @@
 
 package io.kroxylicious.proxy;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.DeleteTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicCollection;
+import org.apache.kafka.common.serialization.Serdes;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import io.github.nettyplus.leakdetector.junit.NettyLeakDetectorExtension;
 
 import io.kroxylicious.test.tester.KroxyliciousTester;
 import io.kroxylicious.testing.kafka.junit5ext.KafkaClusterExtension;
+import io.kroxylicious.testing.kafka.junit5ext.Topic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -77,5 +84,29 @@ public abstract class BaseIT {
             return tester.producer(virtualCluster.get(), producerConfig);
         }
         return tester.producer(producerConfig);
+    }
+
+    public static void sendReceiveBatches(KroxyliciousTester tester,
+                                          Topic topic,
+                                          Map<String, Object> producerConfig,
+                                          Map<String, Object> consumerConfig,
+                                          int numBatches,
+                                          BiConsumer<Integer, ConsumerRecords<String, byte[]>> recordConsumer) {
+        try (var producer = tester.producer(producerConfig);
+                var consumer = tester
+                        .consumer(Serdes.String(), Serdes.ByteArray(), consumerConfig)) {
+            int batchNumOneBased = 1;
+            while (batchNumOneBased <= numBatches) {
+                assertThat(producer.send(new ProducerRecord<>(topic.name(), "my-key", "my-value")))
+                        .succeedsWithin(Duration.ofSeconds(5));
+
+                consumer.subscribe(Set.of(topic.name()));
+                var records = consumer.poll(Duration.ofSeconds(10));
+
+                assertThat(records).hasSize(1);
+                recordConsumer.accept(batchNumOneBased, records);
+                batchNumOneBased += 1;
+            }
+        }
     }
 }
