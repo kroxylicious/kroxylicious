7eeb7222b5a01d1372447a64d5a0eb166fa57de7
Sam Barker <sam@quadrocket.co.uk>
2025-12-12 11:01:16 +1300

    Richer status model.
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/KafkaSession.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/internal/KafkaSession.java
@@ -6,8 +6,28 @@
 
 package io.kroxylicious.proxy.internal;
 
-public record KafkaSession(String sessionId, KafkaSessionState currentState) {
+import java.util.Objects;
+import java.util.UUID;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+record KafkaSession(String sessionId, KafkaSessionState currentState) {
+
     public KafkaSession in(KafkaSessionState newState) {
         return new KafkaSession(this.sessionId, newState);
+    }
+
+    KafkaSession(KafkaSessionState currentState) {
+        this(null, currentState);
+    }
+
+    KafkaSession(@Nullable String sessionId, KafkaSessionState currentState) {
+        if (Objects.isNull(sessionId)) {
+            this.sessionId = UUID.randomUUID().toString();
+        }
+        else {
+            this.sessionId = sessionId;
+        }
+        this.currentState = currentState;
     }
 }
