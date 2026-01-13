f2cb18dc806e08bd8ae092dfc0a47eb2db570c7e
Robert Young <robertyoungnz@gmail.com>
2026-01-14 12:44:53 +1300

    Add missing @Override annotations
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-sasl-inspection/src/main/java/io/kroxylicious/filter/sasl/inspection/SaslInspection.java
+++ kroxylicious-filters/kroxylicious-sasl-inspection/src/main/java/io/kroxylicious/filter/sasl/inspection/SaslInspection.java
@@ -12,7 +12,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
