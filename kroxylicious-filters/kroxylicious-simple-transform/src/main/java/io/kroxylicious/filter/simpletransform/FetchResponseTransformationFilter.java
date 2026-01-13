255fc56ec53db1c12af73f760762ff7c260ec9e2
Tom Bentley <tbentley@redhat.com>
2026-01-14 11:43:01 +1300

    Change simple transformation filters' package using @DeprecatedPluginName
    From `io.kroxylicious.proxy.filter.simpletransform` to `io.kroxylicious.filter.simpletransform` (remove proxy).
    
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-filters/kroxylicious-simple-transform/src/main/java/io/kroxylicious/filter/simpletransform/FetchResponseTransformationFilter.java
+++ kroxylicious-filters/kroxylicious-simple-transform/src/main/java/io/kroxylicious/filter/simpletransform/FetchResponseTransformationFilter.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.kroxylicious.proxy.filter.simpletransform;
+package io.kroxylicious.filter.simpletransform;
 
 import java.util.List;
 import java.util.Map;
