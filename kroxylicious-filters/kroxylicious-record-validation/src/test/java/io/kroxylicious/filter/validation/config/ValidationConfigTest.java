15787c476e426699dcd222a781dc8da8066b3f7d
Robert Young <robertyoungnz@gmail.com>
2026-01-15 11:53:43 +1300

    Fix duplicated tests
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-record-validation/src/test/java/io/kroxylicious/filter/validation/config/ValidationConfigTest.java
+++ kroxylicious-filters/kroxylicious-record-validation/src/test/java/io/kroxylicious/filter/validation/config/ValidationConfigTest.java
@@ -10,8 +10,11 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,145 +24,87 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ValidationConfigTest {
 
-    @Test
-    void testDecodeDefaultValues() throws JsonProcessingException {
-        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
-        ValidationConfig deserialised = yamlMapper.readerFor(ValidationConfig.class).readValue("""
-                defaultRule:
-                  valueRule: {}
-                rules:
-                - topicNames:
-                  - one
-                  valueRule:
-                    syntacticallyCorrectJson: {}
-                - topicNames:
-                  - two
-                  keyRule: {}
-                """);
-
-        TopicMatchingRecordValidationRule ruleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
-                new BytebufValidation(new SyntacticallyCorrectJsonConfig(false), null, true, false));
-        TopicMatchingRecordValidationRule ruleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, true, false), null);
-        ValidationConfig expected = new ValidationConfig(List.of(ruleOne, ruleTwo),
-                new RecordValidationRule(null, new BytebufValidation(null, null, true, false)));
-        assertEquals(expected, deserialised);
-    }
-
-    @Test
-    void testDecodeNonDefaultValues() throws JsonProcessingException {
-        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
-        ValidationConfig deserialised = yamlMapper.readerFor(ValidationConfig.class).readValue("""
-                defaultRule:
-                  valueRule:
-                    allowNulls: false
-                    allowEmpty: true
-                rules:
-                - topicNames:
-                  - one
-                  valueRule:
-                    syntacticallyCorrectJson:
-                        validateObjectKeysUnique: true
-                    allowNulls: false
-                    allowEmpty: true
-                - topicNames:
-                  - two
-                  keyRule:
-                    allowNulls: false
-                    allowEmpty: true
-                """);
-
-        TopicMatchingRecordValidationRule ruleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
-                new BytebufValidation(new SyntacticallyCorrectJsonConfig(true), null, false, true));
-        TopicMatchingRecordValidationRule ruleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, false, true), null);
-        ValidationConfig expected = new ValidationConfig(List.of(ruleOne, ruleTwo), new RecordValidationRule(null, new BytebufValidation(null, null, false, true)));
-        assertEquals(expected, deserialised);
-    }
-
-    @Test
-    void testDecodeDefaultValuesSchemaValidation() throws JsonProcessingException {
-        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
-        ValidationConfig deserialised = yamlMapper.readerFor(ValidationConfig.class).readValue("""
-                defaultRule:
-                  valueRule: {}
-                rules:
-                - topicNames:
-                  - one
-                  valueRule:
-                    syntacticallyCorrectJson: {}
-                - topicNames:
-                  - two
-                  keyRule: {}
-                """);
-
-        TopicMatchingRecordValidationRule ruleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
-                new BytebufValidation(new SyntacticallyCorrectJsonConfig(false), null, true, false));
-        TopicMatchingRecordValidationRule ruleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, true, false), null);
-        ValidationConfig expected = new ValidationConfig(List.of(ruleOne, ruleTwo),
-                new RecordValidationRule(null, new BytebufValidation(null, null, true, false)));
-        assertEquals(expected, deserialised);
-    }
-
-    @Test
-    void testDecodeNonDefaultValuesSchemaValidation() throws JsonProcessingException, MalformedURLException {
-        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
-        ValidationConfig deserialised = yamlMapper.readerFor(ValidationConfig.class).readValue("""
-                defaultRule:
-                  valueRule:
-                    allowNulls: false
-                    allowEmpty: true
-                rules:
-                - topicNames:
-                  - one
-                  valueRule:
-                    syntacticallyCorrectJson:
-                        validateObjectKeysUnique: true
-                    schemaValidationConfig:
-                        apicurioGlobalId: 1
-                        apicurioRegistryUrl: http://localhost:8080
-                    allowNulls: false
-                    allowEmpty: true
-                - topicNames:
-                  - two
-                  keyRule:
-                    allowNulls: false
-                    allowEmpty: true
-                """);
-
+    private static ValidationConfig expectedApicurioConfig() throws MalformedURLException {
         TopicMatchingRecordValidationRule ruleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
                 new BytebufValidation(new SyntacticallyCorrectJsonConfig(true), new SchemaValidationConfig(URI.create("http://localhost:8080").toURL(), 1L), false,
                         true));
         TopicMatchingRecordValidationRule ruleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, false, true), null);
-        ValidationConfig expected = new ValidationConfig(List.of(ruleOne, ruleTwo), new RecordValidationRule(null, new BytebufValidation(null, null, false, true)));
-        assertEquals(expected, deserialised);
+        return new ValidationConfig(List.of(ruleOne, ruleTwo), new RecordValidationRule(null, new BytebufValidation(null, null, false, true)));
     }
 
-    @Test
-    void testDecodeInvalidValuesSchemaValidation() throws JsonProcessingException {
-        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
-        ValidationConfig deserialised = yamlMapper.readerFor(ValidationConfig.class).readValue("""
+    public static Stream<Arguments> deserialize() throws MalformedURLException {
+        TopicMatchingRecordValidationRule ruleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
+                new BytebufValidation(new SyntacticallyCorrectJsonConfig(false), null, true, false));
+        TopicMatchingRecordValidationRule ruleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, true, false), null);
+        ValidationConfig defaultConfig = new ValidationConfig(List.of(ruleOne, ruleTwo),
+                new RecordValidationRule(null, new BytebufValidation(null, null, true, false)));
+
+        TopicMatchingRecordValidationRule nonDefaultRuleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
+                new BytebufValidation(new SyntacticallyCorrectJsonConfig(true), null, false, true));
+        TopicMatchingRecordValidationRule nonDefaultRuleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, false, true), null);
+        ValidationConfig nonDefaultConfig = new ValidationConfig(List.of(nonDefaultRuleOne, nonDefaultRuleTwo),
+                new RecordValidationRule(null, new BytebufValidation(null, null, false, true)));
+        return Stream.of(Arguments.argumentSet("default values", """
                 defaultRule:
-                  valueRule:
-                    allowNulls: false
-                    allowEmpty: true
+                  valueRule: {}
                 rules:
                 - topicNames:
                   - one
                   valueRule:
-                    syntacticallyCorrectJson:
-                        validateObjectKeysUnique: true
-                    allowNulls: false
-                    allowEmpty: true
+                    syntacticallyCorrectJson: {}
                 - topicNames:
                   - two
-                  keyRule:
-                    allowNulls: false
-                    allowEmpty: true
-                """);
+                  keyRule: {}
+                """, defaultConfig),
+                Arguments.argumentSet("non default values", """
+                        defaultRule:
+                          valueRule:
+                            allowNulls: false
+                            allowEmpty: true
+                        rules:
+                        - topicNames:
+                          - one
+                          valueRule:
+                            syntacticallyCorrectJson:
+                                validateObjectKeysUnique: true
+                            allowNulls: false
+                            allowEmpty: true
+                        - topicNames:
+                          - two
+                          keyRule:
+                            allowNulls: false
+                            allowEmpty: true
+                        """, nonDefaultConfig),
+                Arguments.argumentSet("non default apicurio values", """
+                        defaultRule:
+                          valueRule:
+                            allowNulls: false
+                            allowEmpty: true
+                        rules:
+                        - topicNames:
+                          - one
+                          valueRule:
+                            syntacticallyCorrectJson:
+                                validateObjectKeysUnique: true
+                            schemaValidationConfig:
+                                apicurioGlobalId: 1
+                                apicurioRegistryUrl: http://localhost:8080
+                            allowNulls: false
+                            allowEmpty: true
+                        - topicNames:
+                          - two
+                          keyRule:
+                            allowNulls: false
+                            allowEmpty: true
+                        """, expectedApicurioConfig()));
+    }
 
-        TopicMatchingRecordValidationRule ruleOne = new TopicMatchingRecordValidationRule(Set.of("one"), null,
-                new BytebufValidation(new SyntacticallyCorrectJsonConfig(true), null, false, true));
-        TopicMatchingRecordValidationRule ruleTwo = new TopicMatchingRecordValidationRule(Set.of("two"), new BytebufValidation(null, null, false, true), null);
-        ValidationConfig expected = new ValidationConfig(List.of(ruleOne, ruleTwo), new RecordValidationRule(null, new BytebufValidation(null, null, false, true)));
+    @MethodSource
+    @ParameterizedTest
+    void deserialize(String content, ValidationConfig expected) throws JsonProcessingException {
+        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+        ValidationConfig deserialised = yamlMapper.readerFor(ValidationConfig.class).readValue(content);
         assertEquals(expected, deserialised);
     }
+
 }
