50e4c23222934cbed280038cd8ad2ac787470dc5
Robert Young <robertyoungnz@gmail.com>
2026-01-20 16:39:35 +1300

    Remove redundant methods from records
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/filter/validation/config/SchemaValidationConfig.java
+++ kroxylicious-filters/kroxylicious-record-validation/src/main/java/io/kroxylicious/filter/validation/config/SchemaValidationConfig.java
@@ -13,6 +13,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Configuration for validating a component ByteBuffer of a {@link org.apache.kafka.common.record.Record} is valid using the schema in Apicurio Registry.
+ * @param apicurioGlobalId globalId
+ * @param apicurioRegistryUrl apicurio registry url
  */
 public record SchemaValidationConfig(URL apicurioRegistryUrl, long apicurioGlobalId) {
     /**
@@ -25,30 +27,6 @@ public record SchemaValidationConfig(URL apicurioRegistryUrl, long apicurioGloba
                                   @JsonProperty(value = "apicurioGlobalId", required = true) long apicurioGlobalId) {
         this.apicurioGlobalId = apicurioGlobalId;
         this.apicurioRegistryUrl = apicurioRegistryUrl;
-    }
-
-    /**
-     * @return the configured globalId to be used
-     */
-    @Override
-    public long apicurioGlobalId() {
-        return apicurioGlobalId;
-    }
-
-    /**
-     * @return the apicurio registry url to be used for this validation
-     */
-    @Override
-    public URL apicurioRegistryUrl() {
-        return apicurioRegistryUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "SchemaValidationConfig{" +
-                "apicurioGlobalId=" + apicurioGlobalId +
-                ", apicurioRegistryUrl='" + apicurioRegistryUrl + '\'' +
-                '}';
     }
 
 }
