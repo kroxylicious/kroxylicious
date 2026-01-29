6692f8c2cdc4422474c9a9cd774edf50dc1fecff
Kroxylicious Robot <153509303+kroxylicious-robot@users.noreply.github.com>
2026-01-29 09:31:08 +0000

    chore(deps): update localstack/localstack docker tag to v4.13.0
    Signed-off-by: Kroxylicious Robot <153509303+kroxylicious-robot@users.noreply.github.com>

--- kroxylicious-kms-providers/kroxylicious-kms-provider-aws-kms-test-support/src/main/java/io/kroxylicious/kms/provider/aws/kms/AwsKmsTestKmsFacade.java
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-aws-kms-test-support/src/main/java/io/kroxylicious/kms/provider/aws/kms/AwsKmsTestKmsFacade.java
@@ -18,7 +18,7 @@ import edu.umd.cs.findbugs.annotations.Nullable;
 
 public class AwsKmsTestKmsFacade extends AbstractAwsKmsTestKmsFacade {
     private static final Logger LOG = LoggerFactory.getLogger(AwsKmsTestKmsFacade.class);
-    private static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack:4.12.0");
+    private static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack:4.13.0");
     private @Nullable LocalStackContainer localStackContainer;
 
     @Override
