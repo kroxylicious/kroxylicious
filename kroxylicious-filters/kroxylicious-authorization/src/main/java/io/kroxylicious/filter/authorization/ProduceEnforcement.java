6f615b5dd9cba26c16133a18cc3bfbe3bcf757d2
Robert Young <robertyoungnz@gmail.com>
2026-02-11 13:30:10 +1300

    Drop zero-ack requests which are not authorized for txnlId
    We should not attempt to response to zero-ack produce
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/ProduceEnforcement.java
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/ProduceEnforcement.java
@@ -18,11 +18,10 @@ import org.apache.kafka.common.requests.ProduceRequest;
 import org.apache.kafka.common.requests.RequestUtils;
 
 import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.authorizer.service.AuthorizeResult;
 import io.kroxylicious.authorizer.service.Decision;
 import io.kroxylicious.proxy.filter.FilterContext;
 import io.kroxylicious.proxy.filter.RequestFilterResult;
-
-import edu.umd.cs.findbugs.annotations.NonNull;
 
 import static org.apache.kafka.common.protocol.Errors.TRANSACTIONAL_ID_AUTHORIZATION_FAILED;
 
@@ -43,71 +42,83 @@ class ProduceEnforcement extends ApiEnforcement<ProduceRequestData, ProduceRespo
                                                    FilterContext context,
                                                    AuthorizationFilter authorizationFilter) {
         ProduceRequest produceRequest = new ProduceRequest(request, header.requestApiVersion());
+        boolean requiresResponse = request.acks() != 0;
         boolean hasTransactionalRecords = RequestUtils.hasTransactionalRecords(produceRequest);
         String transactionalId = request.transactionalId();
         // fail fast if records flagged as transactional but request has no transactionalId
         if (hasTransactionalRecords && transactionalId == null) {
-            return transactionalIdErrorResponse(context, produceRequest);
+            return transactionalIdErrorResponse(context, produceRequest, requiresResponse);
         }
-        boolean requiresResponse = request.acks() != 0;
-        var topicWriteActions = request.topicData().stream()
-                .map(t -> new Action(TopicResource.WRITE, t.name()));
-        var transactionalIdActions = hasTransactionalRecords ? Stream.of(new Action(TransactionalIdResource.WRITE, transactionalId)) : Stream.<Action> empty();
-        return authorizationFilter.authorization(context, Stream.concat(topicWriteActions, transactionalIdActions).toList()).thenCompose(authorization -> {
-            if (hasTransactionalRecords && authorization.denied(TransactionalIdResource.WRITE).contains(transactionalId)) {
-                return transactionalIdErrorResponse(context, produceRequest);
-            }
-            var topicWriteDecisions = authorization.partition(request.topicData(),
-                    TopicResource.WRITE, ProduceRequestData.TopicProduceData::name);
-
-            var allowedTopicWrites = topicWriteDecisions.get(Decision.ALLOW);
-            if (allowedTopicWrites.isEmpty()) {
-                if (requiresResponse) {
-                    return context.requestFilterResultBuilder()
-                            .errorResponse(header, request, Errors.TOPIC_AUTHORIZATION_FAILED.exception())
-                            .completed();
+        else {
+            var topicWriteActions = request.topicData().stream()
+                    .map(t -> new Action(TopicResource.WRITE, t.name()));
+            var transactionalIdActions = hasTransactionalRecords ? Stream.of(new Action(TransactionalIdResource.WRITE, transactionalId)) : Stream.<Action> empty();
+            return authorizationFilter.authorization(context, Stream.concat(topicWriteActions, transactionalIdActions).toList()).thenCompose(authorization -> {
+                if (hasTransactionalRecords && authorization.denied(TransactionalIdResource.WRITE).contains(transactionalId)) {
+                    return transactionalIdErrorResponse(context, produceRequest, requiresResponse);
                 }
-                else {
-                    return context.requestFilterResultBuilder().drop().completed();
-                }
-            }
-
-            var deniedTopicWrites = topicWriteDecisions.get(Decision.DENY);
-
-            if (deniedTopicWrites.isEmpty()) {
-                // nothing denied, forward whole request on
-                return context.forwardRequest(header, request);
-            }
-
-            for (var topic : deniedTopicWrites) {
-                request.topicData().remove(topic);
-            }
-
-            var topicProduceResponses = deniedTopicWrites.stream()
-                    .map(topicProduceData -> new ProduceResponseData.TopicProduceResponse()
-                            .setName(topicProduceData.name())
-                            .setPartitionResponses(topicProduceData.partitionData().stream()
-                                    .map(partitionProduceData -> new ProduceResponseData.PartitionProduceResponse()
-                                            .setIndex(partitionProduceData.index())
-                                            .setErrorMessage(Errors.TOPIC_AUTHORIZATION_FAILED.message())
-                                            .setErrorCode(Errors.TOPIC_AUTHORIZATION_FAILED.code()))
-                                    .toList()))
-                    .toList();
-
-            if (requiresResponse) {
-                authorizationFilter.pushInflightState(header, (ProduceResponseData response) -> {
-                    response.responses().addAll(topicProduceResponses);
-                    return response;
-                });
-            }
-            return context.forwardRequest(header, request);
-        });
+                return authorizeTopics(header, request, context, authorizationFilter, authorization, requiresResponse);
+            });
+        }
     }
 
-    @NonNull
-    private static CompletionStage<RequestFilterResult> transactionalIdErrorResponse(FilterContext context, ProduceRequest produceRequest) {
-        ApiMessage response = produceRequest.getErrorResponse(TRANSACTIONAL_ID_AUTHORIZATION_FAILED.exception()).data();
-        return context.requestFilterResultBuilder().shortCircuitResponse(response).completed();
+    private static CompletionStage<RequestFilterResult> authorizeTopics(RequestHeaderData header, ProduceRequestData request, FilterContext context,
+                                                                        AuthorizationFilter authorizationFilter, AuthorizeResult authorization,
+                                                                        boolean requiresResponse) {
+        var topicWriteDecisions = authorization.partition(request.topicData(),
+                TopicResource.WRITE, ProduceRequestData.TopicProduceData::name);
+
+        var allowedTopicWrites = topicWriteDecisions.get(Decision.ALLOW);
+        if (allowedTopicWrites.isEmpty()) {
+            if (requiresResponse) {
+                return context.requestFilterResultBuilder()
+                        .errorResponse(header, request, Errors.TOPIC_AUTHORIZATION_FAILED.exception())
+                        .completed();
+            }
+            else {
+                return context.requestFilterResultBuilder().drop().completed();
+            }
+        }
+
+        var deniedTopicWrites = topicWriteDecisions.get(Decision.DENY);
+
+        if (deniedTopicWrites.isEmpty()) {
+            // nothing denied, forward whole request on
+            return context.forwardRequest(header, request);
+        }
+
+        for (var topic : deniedTopicWrites) {
+            request.topicData().remove(topic);
+        }
+
+        var topicProduceResponses = deniedTopicWrites.stream()
+                .map(topicProduceData -> new ProduceResponseData.TopicProduceResponse()
+                        .setName(topicProduceData.name())
+                        .setPartitionResponses(topicProduceData.partitionData().stream()
+                                .map(partitionProduceData -> new ProduceResponseData.PartitionProduceResponse()
+                                        .setIndex(partitionProduceData.index())
+                                        .setErrorMessage(Errors.TOPIC_AUTHORIZATION_FAILED.message())
+                                        .setErrorCode(Errors.TOPIC_AUTHORIZATION_FAILED.code()))
+                                .toList()))
+                .toList();
+
+        if (requiresResponse) {
+            authorizationFilter.pushInflightState(header, (ProduceResponseData response) -> {
+                response.responses().addAll(topicProduceResponses);
+                return response;
+            });
+        }
+        return context.forwardRequest(header, request);
+    }
+
+    private static CompletionStage<RequestFilterResult> transactionalIdErrorResponse(FilterContext context, ProduceRequest produceRequest, boolean requiresResponse) {
+        if (requiresResponse) {
+            ApiMessage response = produceRequest.getErrorResponse(TRANSACTIONAL_ID_AUTHORIZATION_FAILED.exception()).data();
+            return context.requestFilterResultBuilder().shortCircuitResponse(response).completed();
+        }
+        else {
+            return context.requestFilterResultBuilder().drop().completed();
+        }
     }
 
 }
