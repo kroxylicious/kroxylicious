4a5f8dad87d4b668d3ca7a1ee288e46d50737e15
Shubham Rawat <40595489+ShubhamRwt@users.noreply.github.com>
2026-02-18 22:44:30 +0530

    refactoring(operator): extract KafkaServiceReconciler mappers to top-level classes (#3309)
    * Refactor mappers from KafkaServiceReconciler
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Addes suggestions and renamed classes
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    ---------
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>

---
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/reconciler/kafkaservice/SecretSecondaryJoinedOnTlsCertificateRefMapperToKafkaServicePrimaryMapperTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kubernetes.operator.reconciler.kafkaservice;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import io.fabric8.kubernetes.api.model.KubernetesResourceList;
+import io.fabric8.kubernetes.api.model.SecretBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.api.reconciler.EventSourceContext;
+
+import io.kroxylicious.kubernetes.api.v1alpha1.KafkaService;
+import io.kroxylicious.kubernetes.api.v1alpha1.KafkaServiceBuilder;
+
+import static io.kroxylicious.kubernetes.operator.reconciler.kafkaservice.MapperTestSupport.SERVICE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class SecretSecondaryJoinedOnTlsCertificateRefMapperToKafkaServicePrimaryMapperTest {
+
+    static Stream<Arguments> mappingToSecretToleratesKafkaServicesWithoutTls() {
+        return Stream.of(
+                Arguments.argumentSet("without tls", new KafkaServiceBuilder(SERVICE).editSpec().withTls(null).endSpec().build()),
+                Arguments.argumentSet("with tls but without client cert",
+                        new KafkaServiceBuilder(SERVICE).editSpec().editTls().withCertificateRef(null).endTls().endSpec().build()));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void mappingToSecretToleratesKafkaServicesWithoutTls(KafkaService service) {
+        // Given
+        EventSourceContext<KafkaService> eventSourceContext = mock();
+        KubernetesClient client = mock();
+        when(eventSourceContext.getClient()).thenReturn(client);
+
+        KubernetesResourceList<KafkaService> mockList = MapperTestSupport.mockKafkaServiceListOperation(client);
+        when(mockList.getItems()).thenReturn(List.of(service));
+
+        // When
+        var mapper = new SecretSecondaryJoinedOnTlsCertificateRefMapperToKafkaServicePrimaryMapper(eventSourceContext);
+
+        // Then
+        var primaryResourceIDs = mapper.toPrimaryResourceIDs(new SecretBuilder().withNewMetadata().withName("secret").endMetadata().build());
+        assertThat(primaryResourceIDs).isEmpty();
+    }
+}
