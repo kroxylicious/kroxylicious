9952497343b1d3a48888bf1aeb9e38c65230a65a
Shubham Rawat <40595489+ShubhamRwt@users.noreply.github.com>
2026-02-18 17:08:09 +0530

    refactoring(operator): extract VirtualKafkaCluster mappers to top-level classes (#3304)
    * Refactor VirtualKafkaClusterReconciler
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Updated names with Mapper at last
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Added suggestions by Keith
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * fix naming
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Fix Sonar Issue
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    ---------
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>

---
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/operator/reconciler/virtualkafkacluster/SecretSecondaryJoinedOnIngressCertificateRefToVirtualKafkaClusterPrimaryMapperTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.kubernetes.operator.reconciler.virtualkafkacluster;
+
+import org.junit.jupiter.api.Test;
+
+import io.javaoperatorsdk.operator.api.reconciler.EventSourceContext;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
+
+import io.kroxylicious.kubernetes.api.v1alpha1.VirtualKafkaCluster;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SecretSecondaryJoinedOnIngressCertificateRefToVirtualKafkaClusterPrimaryMapperTest {
+    @Test
+    void canMapFromSecretToVirtualKafkaClusterWithTls() {
+        // Given
+        EventSourceContext<VirtualKafkaCluster> eventSourceContext = MapperTestSupport.mockContextContaining(MapperTestSupport.CLUSTER_TLS_NO_FILTERS);
+
+        // When
+        var mapper = new SecretSecondaryJoinedOnIngressCertificateRefToVirtualKafkaClusterPrimaryMapper(eventSourceContext);
+
+        // Then
+        var primaryResourceIDs = mapper.toPrimaryResourceIDs(MapperTestSupport.KUBE_TLS_CERT_SECRET);
+        assertThat(primaryResourceIDs).containsExactly(ResourceID.fromResource(MapperTestSupport.CLUSTER_TLS_NO_FILTERS));
+    }
+
+    @Test
+    void canMapFromSecretToVirtualKafkaClusterToleratesVirtualKafkaClusterWithoutTls() {
+        // Given
+        EventSourceContext<VirtualKafkaCluster> eventSourceContext = MapperTestSupport.mockContextContaining(MapperTestSupport.CLUSTER_NO_FILTERS);
+
+        // When
+        var mapper = new SecretSecondaryJoinedOnIngressCertificateRefToVirtualKafkaClusterPrimaryMapper(eventSourceContext);
+
+        // Then
+        var primaryResourceIDs = mapper.toPrimaryResourceIDs(MapperTestSupport.KUBE_TLS_CERT_SECRET);
+        assertThat(primaryResourceIDs).isEmpty();
+    }
+
+}
