8204809d3e754106f418087d702336f202ecb968
Robert Young <robertyoungnz@gmail.com>
2026-02-23 15:14:58 +1300

    Require READ on Group for AddOffsetsToTxn
    Note that transactional id authorization takes precedence, so group
    authorization failures will only be visible if the transactional id is
    allowed to be written to.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AddOffsetsToTxnEnforcement.java
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AddOffsetsToTxnEnforcement.java
@@ -15,6 +15,7 @@ import org.apache.kafka.common.message.RequestHeaderData;
 import org.apache.kafka.common.protocol.Errors;
 
 import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.authorizer.service.AuthorizeResult;
 import io.kroxylicious.proxy.filter.FilterContext;
 import io.kroxylicious.proxy.filter.RequestFilterResult;
 
@@ -34,16 +35,30 @@ public class AddOffsetsToTxnEnforcement extends ApiEnforcement<AddOffsetsToTxnRe
     CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header, AddOffsetsToTxnRequestData request, FilterContext context,
                                                    AuthorizationFilter authorizationFilter) {
         Action writeTxnAction = new Action(TransactionalIdResource.WRITE, request.transactionalId());
-        return authorizationFilter.authorization(context, List.of(writeTxnAction)).thenCompose(authorizeResult -> {
+        Action groupReadAction = new Action(GroupResource.READ, request.groupId());
+        return authorizationFilter.authorization(context, List.of(writeTxnAction, groupReadAction)).thenCompose(authorizeResult -> {
             // only authorizing a single action, so can check if denied list is empty
             if (!authorizeResult.denied().isEmpty()) {
                 AddOffsetsToTxnResponseData responseData = new AddOffsetsToTxnResponseData();
-                responseData.setErrorCode(Errors.TRANSACTIONAL_ID_AUTHORIZATION_FAILED.code());
+                Errors error = getError(authorizeResult, writeTxnAction, groupReadAction);
+                responseData.setErrorCode(error.code());
                 return context.requestFilterResultBuilder().shortCircuitResponse(responseData).completed();
             }
             else {
                 return context.forwardRequest(header, request);
             }
         });
+    }
+
+    private static Errors getError(AuthorizeResult authorizeResult, Action writeTxnAction, Action groupReadAction) {
+        if (authorizeResult.denied().contains(writeTxnAction)) {
+            return Errors.TRANSACTIONAL_ID_AUTHORIZATION_FAILED;
+        }
+        else if (authorizeResult.denied().contains(groupReadAction)) {
+            return Errors.GROUP_AUTHORIZATION_FAILED;
+        }
+        else {
+            throw new IllegalStateException("unexpected action in denied set" + authorizeResult);
+        }
     }
 }
