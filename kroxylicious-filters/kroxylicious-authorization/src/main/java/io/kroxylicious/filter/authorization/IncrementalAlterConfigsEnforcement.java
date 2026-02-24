332e7107272cbbe395b43895f82b90ccd533abab
Robert Young <robertyoungnz@gmail.com>
2026-02-24 13:24:17 +1300

    Require ALTER_CONFIGS on Group for INCREMENTAL_ALTER_CONFIGS
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/IncrementalAlterConfigsEnforcement.java
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/IncrementalAlterConfigsEnforcement.java
@@ -9,6 +9,7 @@ package io.kroxylicious.filter.authorization;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Stream;
 
 import org.apache.kafka.common.message.IncrementalAlterConfigsRequestData;
 import org.apache.kafka.common.message.IncrementalAlterConfigsRequestData.AlterConfigsResource;
@@ -21,6 +22,7 @@ import io.kroxylicious.authorizer.service.Decision;
 import io.kroxylicious.proxy.filter.FilterContext;
 import io.kroxylicious.proxy.filter.RequestFilterResult;
 
+import static org.apache.kafka.common.config.ConfigResource.Type.GROUP;
 import static org.apache.kafka.common.config.ConfigResource.Type.TOPIC;
 
 class IncrementalAlterConfigsEnforcement extends ApiEnforcement<IncrementalAlterConfigsRequestData, IncrementalAlterConfigsResponseData> {
@@ -38,35 +40,50 @@ class IncrementalAlterConfigsEnforcement extends ApiEnforcement<IncrementalAlter
     CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header, IncrementalAlterConfigsRequestData request, FilterContext context,
                                                    AuthorizationFilter authorizationFilter) {
         List<AlterConfigsResource> topicRequests = ConfigResources.filter(request.resources().stream(), AlterConfigsResource::resourceType, TOPIC).toList();
-        if (topicRequests.isEmpty()) {
+        List<AlterConfigsResource> groupRequests = ConfigResources.filter(request.resources().stream(), AlterConfigsResource::resourceType, GROUP).toList();
+        if (topicRequests.isEmpty() && groupRequests.isEmpty()) {
             return context.forwardRequest(header, request);
         }
         else {
-            List<Action> actions = topicRequests.stream()
-                    .map(re -> new Action(TopicResource.ALTER_CONFIGS, re.resourceName())).toList();
+            Stream<Action> topicActions = topicRequests.stream()
+                    .map(re -> new Action(TopicResource.ALTER_CONFIGS, re.resourceName()));
+            Stream<Action> groupActions = groupRequests.stream()
+                    .map(re -> new Action(GroupResource.ALTER_CONFIGS, re.resourceName()));
+            List<Action> actions = Stream.concat(groupActions, topicActions).toList();
             return authorizationFilter.authorization(context, actions).thenCompose(result -> {
-                Map<Decision, List<AlterConfigsResource>> partitioned = result.partition(topicRequests, TopicResource.ALTER_CONFIGS,
+                Map<Decision, List<AlterConfigsResource>> partitionedTopicResources = result.partition(topicRequests, TopicResource.ALTER_CONFIGS,
                         AlterConfigsResource::resourceName);
-                List<AlterConfigsResource> denied = partitioned.get(Decision.DENY);
-                if (denied.isEmpty()) {
+                Map<Decision, List<AlterConfigsResource>> partitionedGroupResources = result.partition(groupRequests, GroupResource.ALTER_CONFIGS,
+                        AlterConfigsResource::resourceName);
+                List<AlterConfigsResource> deniedTopicResources = partitionedTopicResources.get(Decision.DENY);
+                List<AlterConfigsResource> deniedGroupResources = partitionedGroupResources.get(Decision.DENY);
+                if (deniedTopicResources.isEmpty() && deniedGroupResources.isEmpty()) {
                     return context.forwardRequest(header, request);
                 }
                 else {
-                    request.resources().removeAll(denied);
-                    authorizationFilter.pushInflightState(header, (IncrementalAlterConfigsResponseData d) -> {
-                        denied.forEach(describeConfigsResource -> {
-                            IncrementalAlterConfigsResponseData.AlterConfigsResourceResponse configResult = new IncrementalAlterConfigsResponseData.AlterConfigsResourceResponse();
-                            configResult.setErrorCode(Errors.TOPIC_AUTHORIZATION_FAILED.code());
-                            configResult.setErrorMessage(Errors.TOPIC_AUTHORIZATION_FAILED.message());
-                            configResult.setResourceName(describeConfigsResource.resourceName());
-                            configResult.setResourceType(describeConfigsResource.resourceType());
-                            d.responses().add(configResult);
+                    request.resources().removeAll(deniedTopicResources);
+                    request.resources().removeAll(deniedGroupResources);
+                    authorizationFilter.pushInflightState(header, (IncrementalAlterConfigsResponseData response) -> {
+                        deniedTopicResources.forEach(deniedTopicResource -> {
+                            addResourceFailure(response, deniedTopicResource, Errors.TOPIC_AUTHORIZATION_FAILED);
                         });
-                        return d;
+                        deniedGroupResources.forEach(deniedGroupResource -> {
+                            addResourceFailure(response, deniedGroupResource, Errors.GROUP_AUTHORIZATION_FAILED);
+                        });
+                        return response;
                     });
                     return context.forwardRequest(header, request);
                 }
             });
         }
+    }
+
+    private static void addResourceFailure(IncrementalAlterConfigsResponseData response, AlterConfigsResource describeConfigsResource, Errors error) {
+        IncrementalAlterConfigsResponseData.AlterConfigsResourceResponse configResult = new IncrementalAlterConfigsResponseData.AlterConfigsResourceResponse();
+        configResult.setErrorCode(error.code());
+        configResult.setErrorMessage(error.message());
+        configResult.setResourceName(describeConfigsResource.resourceName());
+        configResult.setResourceType(describeConfigsResource.resourceType());
+        response.responses().add(configResult);
     }
 }
