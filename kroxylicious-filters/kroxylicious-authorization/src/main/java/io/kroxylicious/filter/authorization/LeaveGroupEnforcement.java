5d150874d829ce8c93bd40093bc7013a6483d5cf
Robert Young <robertyoungnz@gmail.com>
2026-02-24 15:50:00 +1300

    Require READ on Group for LEAVE_GROUP
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

---
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/LeaveGroupEnforcement.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Kroxylicious Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.kroxylicious.filter.authorization;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.kafka.common.message.LeaveGroupRequestData;
+import org.apache.kafka.common.message.LeaveGroupResponseData;
+import org.apache.kafka.common.message.RequestHeaderData;
+import org.apache.kafka.common.protocol.Errors;
+
+import io.kroxylicious.authorizer.service.Action;
+import io.kroxylicious.proxy.filter.FilterContext;
+import io.kroxylicious.proxy.filter.RequestFilterResult;
+
+public class LeaveGroupEnforcement extends ApiEnforcement<LeaveGroupRequestData, LeaveGroupResponseData> {
+    @Override
+    short minSupportedVersion() {
+        return 0;
+    }
+
+    @Override
+    short maxSupportedVersion() {
+        return 5;
+    }
+
+    @Override
+    CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header, LeaveGroupRequestData request, FilterContext context,
+                                                   AuthorizationFilter authorizationFilter) {
+        Action readGroup = new Action(GroupResource.READ, request.groupId());
+        return authorizationFilter.authorization(context, List.of(readGroup)).thenCompose(authorizeResult -> {
+            if (authorizeResult.denied().contains(readGroup)) {
+                return context.requestFilterResultBuilder().errorResponse(header, request, Errors.GROUP_AUTHORIZATION_FAILED.exception()).completed();
+            }
+            else {
+                return context.forwardRequest(header, request);
+            }
+        });
+    }
+}
