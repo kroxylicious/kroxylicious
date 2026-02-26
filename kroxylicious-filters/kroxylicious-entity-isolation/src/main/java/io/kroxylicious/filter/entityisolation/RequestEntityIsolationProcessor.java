49a576ca4308e9f78983d6f8b685f266b1633285
Keith Wall <kwall@apache.org>
2026-02-26 12:46:37 +0000

    adapting generator so that it can generate a template based off a request/response pair. WIP
    Signed-off-by: Keith Wall <kwall@apache.org>

--- kroxylicious-filters/kroxylicious-entity-isolation/src/main/java/io/kroxylicious/filter/entityisolation/RequestEntityIsolationProcessor.java
+++ kroxylicious-filters/kroxylicious-entity-isolation/src/main/java/io/kroxylicious/filter/entityisolation/RequestEntityIsolationProcessor.java
@@ -9,18 +9,14 @@ package io.kroxylicious.filter.entityisolation;
 import java.util.concurrent.CompletionStage;
 
 import org.apache.kafka.common.message.RequestHeaderData;
-import org.apache.kafka.common.message.ResponseHeaderData;
 import org.apache.kafka.common.protocol.ApiMessage;
 
 import io.kroxylicious.proxy.filter.FilterContext;
 import io.kroxylicious.proxy.filter.RequestFilterResult;
-import io.kroxylicious.proxy.filter.ResponseFilterResult;
 
-interface EntityIsolationProcessor<Q extends ApiMessage, S extends ApiMessage> {
+interface RequestEntityIsolationProcessor<Q extends ApiMessage> {
 
     boolean shouldHandleRequest(short apiVersion);
-
-    boolean shouldHandleResponse(short apiVersion);
 
     default CompletionStage<RequestFilterResult> onRequest(RequestHeaderData header,
                                                            short apiVersion,
@@ -29,14 +25,6 @@ interface EntityIsolationProcessor<Q extends ApiMessage, S extends ApiMessage> {
                                                            EntityIsolationFilter authorizationFilter) {
 
         return context.forwardRequest(header, request);
-    }
-
-    default CompletionStage<ResponseFilterResult> onResponse(ResponseHeaderData header,
-                                                             short apiVersion,
-                                                             S response,
-                                                             FilterContext context,
-                                                             EntityIsolationFilter entityIsolationFilter) {
-        return context.forwardResponse(header, response);
     }
 
 }
