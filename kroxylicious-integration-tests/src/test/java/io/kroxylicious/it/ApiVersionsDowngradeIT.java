137c02ab939c4de44a5a8aa48ec72cf2751d4b06
dahyvuun <dahyvuun@gmail.com>
2026-04-09 00:12:26 +0200

    Fix ErrorProne BadImport warnings
    Signed-off-by: dahyvuun <dahyvuun@gmail.com>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/it/ApiVersionsDowngradeIT.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/it/ApiVersionsDowngradeIT.java
@@ -53,8 +53,7 @@ import io.kroxylicious.testing.kafka.junit5ext.KafkaClusterExtension;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 import static io.kroxylicious.test.tester.KroxyliciousConfigUtils.proxy;
-import static io.kroxylicious.test.tester.KroxyliciousTesters.mockKafkaKroxyliciousTester;
-import static io.kroxylicious.test.tester.KroxyliciousTesters.newBuilder;
+import io.kroxylicious.test.tester.KroxyliciousTesters;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -85,7 +84,7 @@ public class ApiVersionsDowngradeIT {
     @Test
     void clientAheadOfProxy() {
         // Given
-        try (var tester = mockKafkaKroxyliciousTester(KroxyliciousConfigUtils::proxy);
+        try (var tester = KroxyliciousTesters.mockKafkaKroxyliciousTester(KroxyliciousConfigUtils::proxy);
                 var client = tester.simpleTestClient()) {
             OpaqueRequestFrame frame = createHypotheticalFutureRequest();
             ApiVersionsResponseData.ApiVersion expected = new ApiVersionsResponseData.ApiVersion();
@@ -136,7 +135,7 @@ public class ApiVersionsDowngradeIT {
         var testConfigEnabled = Features.builder().enable(Feature.TEST_ONLY_CONFIGURATION).build();
         var proxy = proxy(cluster)
                 .withDevelopment(Map.of("apiKeyIdMaxVersionOverride", Map.of(ApiKeys.API_VERSIONS.name(), apiVersion)));
-        try (var tester = newBuilder(proxy).setFeatures(testConfigEnabled).createDefaultKroxyliciousTester();
+        try (var tester = KroxyliciousTesters.newBuilder(proxy).setFeatures(testConfigEnabled).createDefaultKroxyliciousTester();
                 var admin = tester.admin(clientSecurityProtocolConfig)) {
             // We've got no way to observe the actual version of the API versions request that is used during _negotiation_
             // so we make do with asserting the connection is usable.
@@ -188,7 +187,7 @@ public class ApiVersionsDowngradeIT {
             var testConfigEnabled = Features.builder().enable(Feature.TEST_ONLY_CONFIGURATION).build();
             var proxy = proxy("localhost:9092")
                     .withDevelopment(Map.of("apiKeyIdMaxVersionOverride", Map.of(ApiKeys.API_VERSIONS.name(), proxyApiVersion)));
-            try (var tester = newBuilder(proxy).setFeatures(testConfigEnabled).createDefaultKroxyliciousTester();
+            try (var tester = KroxyliciousTesters.newBuilder(proxy).setFeatures(testConfigEnabled).createDefaultKroxyliciousTester();
                     var admin = tester.admin()) {
                 // We've got no way to observe the actual version of the API versions request that is used during _negotiation_
                 // so we make do with asserting the connection is usable.
