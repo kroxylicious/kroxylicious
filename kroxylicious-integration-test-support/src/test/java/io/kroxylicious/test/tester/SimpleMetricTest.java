f36a6e4575c5643664254eadd37bd6411ebfc5ea
Dan Vulpe <dvulpe@users.noreply.github.com>
2026-04-13 17:07:01 +0100

    fix (integration-test-support): SimpleMetric replace regex with grammar (#3607)
    * spotbugs reported the regular expression was vulnerable to ReDOS even with possessive quantifiers
    * increase test coverage with additional cases supported now (including NaN,+Inf,-Inf and escaped label values)
    
    Signed-off-by: Dan Vulpe <dvulpe@gmail.com>
    Co-authored-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-integration-test-support/src/test/java/io/kroxylicious/test/tester/SimpleMetricTest.java
+++ kroxylicious-integration-test-support/src/test/java/io/kroxylicious/test/tester/SimpleMetricTest.java
@@ -13,7 +13,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -57,12 +56,54 @@ class SimpleMetricTest {
 
                                 """,
                         List.of()),
+                Arguments.of("metric value in scientific format with negative exponent uppercase E",
+                        """
+                                metric_with_scientific_format_negative_exponent{} -1.23456789E-10""",
+                        List.of(new SimpleMetric("metric_with_scientific_format_negative_exponent", Map.of(), -1.23456789E-10))),
+                Arguments.of("metric value in scientific format with negative exponent lowercase e",
+                        """
+                                metric_with_scientific_format_negative_exponent{} -1.23456789e-10""",
+                        List.of(new SimpleMetric("metric_with_scientific_format_negative_exponent", Map.of(), -1.23456789E-10))),
+                Arguments.of("metric label with braces",
+                        """
+                                metric_label_with_braces{version="${git.version}"} 1.0""",
+                        List.of(new SimpleMetric("metric_label_with_braces", Map.of("version", "${git.version}"), 1.0))),
+                Arguments.of("escaping in label values",
+                        """
+                                msdos_file_access_time_seconds{path="C:\\\\DIR\\\\FILE.TXT",error="Cannot find file:\\n\\"FILE.TXT\\""} 1.458255915e9""",
+                        List.of(new SimpleMetric("msdos_file_access_time_seconds",
+                                Map.of("path", "C:\\\\DIR\\\\FILE.TXT",
+                                        "error", "Cannot find file:\\n\\\"FILE.TXT\\\""),
+                                1.458255915E9))),
+                Arguments.of("negative infinite value",
+                        """
+                                metric_value_inf{} -Inf""",
+                        List.of(new SimpleMetric("metric_value_inf", Map.of(), Double.NEGATIVE_INFINITY))),
+                Arguments.of("infinite value",
+                        """
+                                metric_value_inf{} +Inf""",
+                        List.of(new SimpleMetric("metric_value_inf", Map.of(), Double.POSITIVE_INFINITY))),
+                Arguments.of("not-a-number value",
+                        """
+                                metric_value_nan{} NaN""",
+                        List.of(new SimpleMetric("metric_value_nan", Map.of(), Double.NaN))),
                 Arguments.of("no metrics - comments only",
                         """
                                 #Mary had a little lamb
                                 #His fleece was white as snow
                                 """,
                         List.of()));
+    }
+
+    static Stream<Arguments> knownBad() {
+        return Stream.of(
+                Arguments.of("(bad)"),
+                Arguments.of("no_value"),
+                Arguments.of("invalid_value three"),
+                Arguments.of("invalid_labels{foo=\"bar\",} 1.0"),
+                Arguments.of("labels_missing_end_quote{foo=\"bar} 1.0"),
+                Arguments.of("labels_missing_start_quote{foo=bar\"} 1.0"),
+                Arguments.of("missing_end_brace{foo=\"bar\" 1.0"));
     }
 
     @ParameterizedTest(name = "{0}")
@@ -74,7 +115,7 @@ class SimpleMetricTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "(bad)", "no_value", "invalid_value three" })
+    @MethodSource("knownBad")
     void parseError(String malformed) {
         assertThatThrownBy(() -> SimpleMetric.parse(malformed))
                 .isInstanceOf(IllegalArgumentException.class)
