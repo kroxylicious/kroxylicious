6ce0f300335b26009174100dd5c131354ef18dc6
Robert Young <robertyoungnz@gmail.com>
2026-06-12 15:30:20 +1200

    refactor(integration-tests): update for ClusterDefinition rename
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/it/RoutingPassThroughIT.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/it/RoutingPassThroughIT.java
@@ -23,7 +23,7 @@ import io.kroxylicious.proxy.config.ConfigurationBuilder;
 import io.kroxylicious.proxy.config.RouteDefinition;
 import io.kroxylicious.proxy.config.RouteTarget;
 import io.kroxylicious.proxy.config.RouterDefinition;
-import io.kroxylicious.proxy.config.TargetClusterDefinition;
+import io.kroxylicious.proxy.config.ClusterDefinition;
 import io.kroxylicious.proxy.config.VirtualClusterBuilder;
 import io.kroxylicious.testing.kafka.api.KafkaCluster;
 import io.kroxylicious.testing.kafka.junit5ext.KafkaClusterExtension;
@@ -47,7 +47,7 @@ class RoutingPassThroughIT {
     private static final String TARGET_CLUSTER_NAME = "backing";
 
     private ConfigurationBuilder routingConfig(KafkaCluster cluster) {
-        var targetCluster = new TargetClusterDefinition(TARGET_CLUSTER_NAME,
+        var targetCluster = new ClusterDefinition(TARGET_CLUSTER_NAME,
                 cluster.getBootstrapServers(), null);
 
         var route = new RouteDefinition(ROUTE_NAME, null, new RouteTarget(TARGET_CLUSTER_NAME, null));
