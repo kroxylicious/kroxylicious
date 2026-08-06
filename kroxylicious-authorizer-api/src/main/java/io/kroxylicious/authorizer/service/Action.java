f38814fa9536619c863c1a563333692156051879
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2026-08-05 21:14:00 -0300

    fix: remove javadoc warnings suppression and enhance exception handling in AuthorizerException (#4587)
    Signed-off-by: Matheus André <matheusandr2@gmail.com>

--- kroxylicious-authorizer-api/src/main/java/io/kroxylicious/authorizer/service/Action.java
+++ kroxylicious-authorizer-api/src/main/java/io/kroxylicious/authorizer/service/Action.java
@@ -17,11 +17,21 @@ public record Action(
                      ResourceType<?> operation,
                      String resourceName) {
 
+    /**
+     * Creates an action.
+     * @param operation The operation (and the resource type)
+     * @param resourceName The resource name
+     * @throws NullPointerException if the {@code operation} or {@code resourceName} is null
+     */
     public Action {
         Objects.requireNonNull(operation);
         Objects.requireNonNull(resourceName);
     }
 
+    /**
+     * The class of the {@linkplain #operation() operation}, which identifies the type of resource being acted upon.
+     * @return The class of the operation.
+     */
     @SuppressWarnings({ "rawtypes", "unchecked", "java:S1452" })
     public Class<? extends ResourceType<?>> resourceTypeClass() {
         return (Class) operation.getClass();
