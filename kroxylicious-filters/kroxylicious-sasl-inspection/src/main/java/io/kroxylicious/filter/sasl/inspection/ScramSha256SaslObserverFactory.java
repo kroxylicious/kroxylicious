8ee93b5d594b812023723f47f9e179aaef3b4571
Matheus André <matheusandr2@gmail.com>
2026-08-06 10:58:50 -0300

    fix(sasl-inspection): update documentation and remove javadoc warnings
    Signed-off-by: Matheus André <matheusandr2@gmail.com>

--- kroxylicious-filters/kroxylicious-sasl-inspection/src/main/java/io/kroxylicious/filter/sasl/inspection/ScramSha256SaslObserverFactory.java
+++ kroxylicious-filters/kroxylicious-sasl-inspection/src/main/java/io/kroxylicious/filter/sasl/inspection/ScramSha256SaslObserverFactory.java
@@ -15,6 +15,14 @@ import io.kroxylicious.proxy.plugin.Plugin;
 @Plugin(configType = Void.class)
 @DeprecatedPluginName(oldName = "io.kroxylicious.filters.sasl.inspection.ScramSha256SaslObserverFactory", since = "0.19.0")
 public class ScramSha256SaslObserverFactory implements SaslObserverFactory {
+
+    /**
+     * Constructs a ScramSha256SaslObserverFactory.
+     */
+    public ScramSha256SaslObserverFactory() {
+        // explicit ctor needed for javadoc
+    }
+
     @Override
     public SaslObserver createObserver() {
         return new ScramSaslObserver(this.mechanismName());
