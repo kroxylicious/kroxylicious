8c36eb3f3314395a46ae78a932b9c06dac54dfd8
Kroxylicious Robot <153509303+kroxylicious-robot@users.noreply.github.com>
2026-08-08 04:23:10 +0000

    chore(deps): update hashicorp/vault docker tag to v2.0.4
    Signed-off-by: Kroxylicious Robot <153509303+kroxylicious-robot@users.noreply.github.com>
    Signed-off-by: Kroxylicious Robot <153509303+kroxylicious-robot@users.noreply.github.com>

--- kroxylicious-kms-providers/kroxylicious-kms-provider-hashicorp-vault-test-support/src/main/java/io/kroxylicious/testing/kms/vault/VaultTestKmsFacade.java
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-hashicorp-vault-test-support/src/main/java/io/kroxylicious/testing/kms/vault/VaultTestKmsFacade.java
@@ -15,7 +15,7 @@ import org.testcontainers.vault.VaultContainer;
 import edu.umd.cs.findbugs.annotations.Nullable;
 
 public class VaultTestKmsFacade extends AbstractVaultTestKmsFacade {
-    private static final String IMAGE = "hashicorp/vault:2.0.3@sha256:a296a888b118615dc01d5f1a6846e6d4a7277946caaed5b447008fff5fe06b54";
+    private static final String IMAGE = "hashicorp/vault:2.0.4@sha256:5be49781ecf78bfe775c5309c6a4d9f4e9e040b6c885c99eb2b12fb69855e1a2";
     private static final DockerImageName HASHICORP_VAULT = DockerImageName.parse(IMAGE)
             .asCompatibleSubstituteFor(DockerImageName.parse(IMAGE.substring(0, IMAGE.indexOf("@"))));
 
