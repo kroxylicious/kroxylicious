d28c2c907171f008bd32b311e2133f33aef7ec8d
Matheus André <matheusandr2@gmail.com>
2026-08-08 12:24:24 -0300

    docs(kroxylicious-kms-provider-azure-key-vault-kms)  enchance, update documentation and remove warnings
    Signed-off-by: Matheus André <matheusandr2@gmail.com>

--- kroxylicious-kms-providers/kroxylicious-kms-provider-azure-key-vault-kms/src/main/java/io/kroxylicious/kms/provider/azure/config/package-info.java
+++ kroxylicious-kms-providers/kroxylicious-kms-provider-azure-key-vault-kms/src/main/java/io/kroxylicious/kms/provider/azure/config/package-info.java
@@ -4,6 +4,9 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
+/**
+ * Configuration model for the Azure Key Vault KMS provider.
+ */
 @ReturnValuesAreNonnullByDefault
 @DefaultAnnotationForParameters(NonNull.class)
 @DefaultAnnotation(NonNull.class)
