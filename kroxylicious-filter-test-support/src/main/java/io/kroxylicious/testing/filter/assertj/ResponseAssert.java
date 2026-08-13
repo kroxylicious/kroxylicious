84dc939e3820c4b9b4337c1a10af388e79a55902
Matheus André <matheusandr2@gmail.com>
2026-08-13 17:24:35 -0300

    docs(kroxylicious-filter-test-support) enhance documentations and remove warnigs
    Signed-off-by: Matheus André <matheusandr2@gmail.com>

--- kroxylicious-filter-test-support/src/main/java/io/kroxylicious/testing/filter/assertj/ResponseAssert.java
+++ kroxylicious-filter-test-support/src/main/java/io/kroxylicious/testing/filter/assertj/ResponseAssert.java
@@ -15,15 +15,38 @@ import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.Condition;
 import org.assertj.core.api.MapAssert;
 
+/**
+ * AssertJ assertions for {@link AbstractResponse}.
+ *
+ * @param <T> the type of response the assertion applies to
+ */
 public class ResponseAssert<T extends AbstractResponse> extends AbstractAssert<ResponseAssert<T>, T> {
+    /**
+     * Constructs an assertion for the given response.
+     *
+     * @param t the actual response
+     */
     protected ResponseAssert(T t) {
         super(t, ResponseAssert.class);
     }
 
+    /**
+     * Creates an assertion for the given response.
+     *
+     * @param <X> the type of response the assertion applies to
+     * @param actual the actual response
+     * @return the assertion
+     */
     public static <X extends AbstractResponse> ResponseAssert<X> assertThat(X actual) {
         return new ResponseAssert<>(actual);
     }
 
+    /**
+     * Verifies that the response has the expected API key.
+     *
+     * @param apiKey the expected API key
+     * @return this assertion
+     */
     public ResponseAssert<T> hasApiKey(ApiKeys apiKey) {
         if (!Objects.equals(actual.apiKey(), apiKey)) {
             failWithMessage("Expected message with apiKey <%s> but was <%s>", apiKey, actual.apiKey());
@@ -31,6 +54,13 @@ public class ResponseAssert<T extends AbstractResponse> extends AbstractAssert<R
         return this;
     }
 
+    /**
+     * Verifies that the response has at least the given number of errors of the given type.
+     *
+     * @param errorType the expected error type
+     * @param errorCount the minimum expected number of errors of that type
+     * @return this assertion
+     */
     public ResponseAssert<T> hasErrorCount(Errors errorType, int errorCount) {
         isNotNull();
         MapAssert.assertThatMap(this.actual.errorCounts())
