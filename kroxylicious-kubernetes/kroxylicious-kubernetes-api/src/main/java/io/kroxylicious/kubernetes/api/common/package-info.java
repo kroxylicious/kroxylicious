4caecb19244a247e5ac69bee8ce59bac52149be5
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2026-08-17 00:14:22 -0300

    docs(kubernetes-api): enhance Javadoc comments across various classes for better clarity (#4681)
    Signed-off-by: Matheus André <matheusandr2@gmail.com>

--- kroxylicious-kubernetes/kroxylicious-kubernetes-api/src/main/java/io/kroxylicious/kubernetes/api/common/package-info.java
+++ kroxylicious-kubernetes/kroxylicious-kubernetes-api/src/main/java/io/kroxylicious/kubernetes/api/common/package-info.java
@@ -4,6 +4,14 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
+/**
+ * Types shared between the Kroxylicious custom resource APIs, such as
+ * {@linkplain io.kroxylicious.kubernetes.api.common.LocalRef references between resources},
+ * {@linkplain io.kroxylicious.kubernetes.api.common.Condition status conditions} and
+ * TLS-related types. The CRD-to-Java code generator is configured to map structurally
+ * identical types occurring in several CRD schemas onto the hand-written classes in
+ * this package, so that they share a single Java type.
+ */
 @ReturnValuesAreNonnullByDefault
 @DefaultAnnotationForParameters(NonNull.class)
 @DefaultAnnotation(NonNull.class)
