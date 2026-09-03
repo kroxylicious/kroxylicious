e81c3263a8dfb582d74cf70ac292efbb224cbda6
Tom Bentley <tbentley@redhat.com>
2026-09-04 10:17:11 +1200

    Imports
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- kroxylicious-integration-tests/src/test/java/io/kroxylicious/it/RouteFilterCorrectnessIT.java
+++ kroxylicious-integration-tests/src/test/java/io/kroxylicious/it/RouteFilterCorrectnessIT.java
@@ -9,7 +9,6 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.message.ApiVersionsResponseData;
